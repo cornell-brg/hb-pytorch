8430e46f3208f6a6333fc7c3e81e9a965118cc3b
lc873 <lc873@cornell.edu>
2020-02-12 14:58:55 -0500

    [Offloading] read kernel path from ENV at compile time

--- c10/hammerblade/HammerBladeDevice.h
+++ c10/hammerblade/HammerBladeDevice.h
@@ -11,11 +11,16 @@
 
 #include <bsg_manycore_cuda.h>
 
+#define PATH(x) pstr(x)
+#define pstr(x) #x
+
 namespace c10 {
 namespace hammerblade {
 
 extern hb_mc_dimension_t _hb_tg_dim;
 extern hb_mc_dimension_t _hb_grid_dim;
 extern hb_mc_device_t _hb_device;
+
+static char _bin_path[] = PATH(HB_KERNEL_DIR); // path to device kernel binary
 
 }} // namespace c10::hammerblade
--- c10/hammerblade/HammerBladeFunctions.cpp
+++ c10/hammerblade/HammerBladeFunctions.cpp
@@ -10,10 +10,7 @@ static std::once_flag init_flag;
 static void initHammerBladeDevice() {
   C10_HB_CHECK(hb_mc_device_init(&_hb_device, "HB_PYTORCH_PORT", 0));
 
-  // XXX: apparently you need to load a binary file to setup allocator ...
-  char bin_path[] = "/work/global/lc873/work/sdh/cosim/"
-    "brg_bsg_bladerunner/bsg_manycore/software/spmd/bsg_cuda_lite_runtime/empty/main.riscv";
-  C10_HB_CHECK(hb_mc_device_program_init(&_hb_device, bin_path, "default_allocator", 0));
+  C10_HB_CHECK(hb_mc_device_program_init(&_hb_device, _bin_path, "default_allocator", 0));
 
   AT_WARN("HammerBlade Device Initialized\n");
   return;
