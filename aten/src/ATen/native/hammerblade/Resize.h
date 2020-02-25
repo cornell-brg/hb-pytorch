8b5e5aedb6649fade7ad2cc32d15780f8eb872c3
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-02-24 22:43:58 -0800

    view tests

---
+++ aten/src/ATen/native/hammerblade/Resize.h
@@ -0,0 +1,84 @@
+#pragma once
+
+#include <ATen/ATen.h>
+#include <TH/THTensor.hpp>
+#include <ATen/hammerblade/HammerBladeContext.h>
+
+namespace at { namespace native {
+
+static inline void HBStorage_resize(THStorage* storage, ptrdiff_t size) {
+  if (storage->resizable()) {
+    /* case when the allocator does not have a realloc defined */
+    at::DataPtr new_data;
+    if (size != 0) {
+      new_data = storage->allocator()->allocate(storage->itemsize() * size);
+    }
+    at::DataPtr old_data = storage->set_data_ptr(std::move(new_data));
+    ptrdiff_t old_size = storage->numel();
+    storage->set_numel(size);
+    if (old_data != nullptr) {
+      ptrdiff_t copy_size = old_size;
+      if (storage->numel() < copy_size) {
+        copy_size = storage->numel();
+      }
+      if (copy_size > 0) {
+        c10::hammerblade::memcpy_host_to_device(
+            storage->data(), 
+            old_data.get(), 
+            storage->itemsize() * copy_size);
+      }
+    }
+  } else {
+    AT_ERROR("Trying to resize storage that is not resizable");
+  }
+}
+
+static inline void maybe_resize_storage_hb(TensorImpl* self, int64_t new_size) {
+  // It does not make sense to try to resize a storage
+  // to hold 0 elements, and this can break
+  // if storage_offset is positive but
+  // new_size is 0, so just bail in that case
+  // (same comment is in Resize.cuh)
+  if (new_size > 0) {
+    if (!THTensor_getStoragePtr(self)) {
+      AT_ERROR("Tensor: invalid null storage");
+    }
+    if (new_size + self->storage_offset() > self->storage().numel()) {
+      HBStorage_resize(
+          THTensor_getStoragePtr(self),
+          new_size + self->storage_offset());
+    }
+  }
+}
+
+inline TensorImpl* resize_impl_hb_(
+    TensorImpl* self,
+    IntArrayRef size,
+    c10::optional<IntArrayRef> stride) {
+  if (self->sizes() == size && (!stride || self->strides() == stride)) {
+    return self;
+  }
+
+  int64_t storage_size = 1;
+  if (stride) {
+    self->set_sizes_and_strides(size, *stride);
+    // NB: storage size can be different from numel.
+    for (size_t dim = 0; dim < size.size(); ++dim) {
+      // FIXME: Don't rely on storage_size being negative because this
+      // may not be true for some edge cases.
+      if (size[dim] == 0) {
+        storage_size = 0;
+        break;
+      }
+      storage_size += (size[dim] - 1) * stride.value()[dim];
+    }
+  } else {
+    self->set_sizes_contiguous(size);
+    storage_size = self->numel();
+  }
+  maybe_resize_storage_hb(self, storage_size);
+
+  return self;
+}
+
+}} // namepsace at::native
--- hammerblade/torch/tests/test_hammerblade_kernels.py
+++ hammerblade/torch/tests/test_hammerblade_kernels.py
@@ -13,7 +13,7 @@ def test_elementwise_div_1():
   z = x * y
   z_h = x.hammerblade() / y.hammerblade()
   assert z_h.device == torch.device("hammerblade")
-  assert torch.equal(z_h.cpu(), z)
+  assert torch.allclose(z_h.cpu(), z)
 
 def test_elementwise_div_2():
   x = torch.ones(4,5)
@@ -21,7 +21,7 @@ def test_elementwise_div_2():
   z = x / y
   z_h = x.hammerblade() / y.hammerblade()
   assert z_h.device == torch.device("hammerblade")
-  assert torch.equal(z_h.cpu(), z)
+  assert torch.allclose(z_h.cpu(), z)
 
 def test_elementwise_div_3():
   x = torch.rand(1,128)
@@ -29,7 +29,7 @@ def test_elementwise_div_3():
   z = x / y
   z_h = x.hammerblade() / y.hammerblade()
   assert z_h.device == torch.device("hammerblade")
-  assert torch.equal(z_h.cpu(), z)
+  assert torch.allclose(z_h.cpu(), z)
 
 def test_elementwise_div_4():
   x = torch.rand(16,32)
@@ -37,7 +37,7 @@ def test_elementwise_div_4():
   z = x / y
   z_h = x.hammerblade() / y.hammerblade()
   assert z_h.device == torch.device("hammerblade")
-  assert torch.equal(z_h.cpu(), z)
+  assert torch.allclose(z_h.cpu(), z)
 
 def test_elementwise_in_place_div():
   x1 = torch.rand(16,32)
@@ -48,5 +48,45 @@ def test_elementwise_in_place_div():
   x1_h.div_(x2_h)
   assert x1_h.device == torch.device("hammerblade")
   x1_h_c = x1_h.cpu()
-  assert torch.equal(x1_h_c, x1)
+  assert torch.allclose(x1_h_c, x1)
+
+def test_view_1():
+  x1 = torch.rand(2,3)
+  x1_h = x1.hammerblade()
+
+  x1 = x1.view(2*3)
+  x1_h = x1_h.view(2*3)
+
+  assert x1_h.device == torch.device("hammerblade")
+  assert torch.equal(x1_h.cpu(), x1)
+
+def test_view_2():
+  x1 = torch.rand(2,3)
+  x1_h = x1.hammerblade()
+
+  x1 = x1.view(1, 2*3)
+  x1_h = x1_h.view(1, 2*3)
+
+  assert x1_h.device == torch.device("hammerblade")
+  assert torch.equal(x1_h.cpu(), x1)
+
+def test_view_3():
+  x1 = torch.rand(4,6)
+  x1_h = x1.hammerblade()
+
+  x1 = x1.view(2, 3, 4)
+  x1_h = x1_h.view(2, 3, 4)
+
+  assert x1_h.device == torch.device("hammerblade")
+  assert torch.equal(x1_h.cpu(), x1)
+
+def test_view_4():
+  x1 = torch.rand(2,3)
+  x1_h = x1.hammerblade()
+
+  x1 = x1.view(3,2)
+  x1_h = x1_h.view(3,2)
+
+  assert x1_h.device == torch.device("hammerblade")
+  assert torch.equal(x1_h.cpu(), x1)
 
