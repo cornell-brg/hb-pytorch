f6b533be50f5b22331ff26a6a3dab34339bbd6a7
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-03-03 16:04:15 -0800

    Separated tests

--- hammerblade/torch/tests/targets.py
+++ hammerblade/torch/tests/targets.py
@@ -7,4 +7,7 @@
 
 pytest_targets = ["test_hammerblade_device",
                   "test_hammerblade_add_sub",
-                  "test_hammerblade_mul"]
+                  "test_hammerblade_mul",
+                  "test_hammerblade_div",
+                  "test_hammerblade_tensor_manip",
+                  ]
