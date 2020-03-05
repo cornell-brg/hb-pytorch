d5ec77984b5d51a2e433a6768ba059c5e3d4431f
lc873 <lc873@cornell.edu>
2020-03-05 00:05:13 -0500

    [kernel] pack up bsg_manycore headers to common.h

--- hammerblade/torch/kernel/kernel.cpp
+++ hammerblade/torch/kernel/kernel.cpp
@@ -1,58 +1,9 @@
-#include <cstring>
-#include <cstdint>
-#include <math.h>
-
-// BSG_TILE_GROUP_X_DIM and BSG_TILE_GROUP_Y_DIM must be defined
-// before bsg_manycore.h and bsg_tile_group_barrier.h are
-// included. bsg_tiles_X and bsg_tiles_Y must also be defined for
-// legacy reasons, but they are deprecated.
-#define BSG_TILE_GROUP_X_DIM 1
-#define BSG_TILE_GROUP_Y_DIM 1
-#define bsg_tiles_X BSG_TILE_GROUP_X_DIM
-#define bsg_tiles_Y BSG_TILE_GROUP_Y_DIM
-#include "bsg_manycore.h"
-#include "bsg_set_tile_x_y.h"
-#include "bsg_tile_group_barrier.h"
-#include "bsg_tensor.hpp"
-
 //====================================================================
-// HammerBlade kernel emulation
-// 03/02/2020, Lin Cheng (lc873@cornell.edu)
+// Legacy kernel entrance
 //====================================================================
-// When emulation layer is enabled, macro HB_EMUL is defined
-// In such case, we need to include kernel.h from c10/hammerblade/emul
-// and we have to define init_kernel_starters
-//
-// Note: when emulation layer is enabled, this file is included when
-// building c10/hammerblade/emul
 
-#include <hammerblade_emul.hpp>
+#include <common.h>
 
-INIT_TILE_GROUP_BARRIER(r_barrier, c_barrier, 0, bsg_tiles_X-1,
-    0, bsg_tiles_Y-1);
-
-template
-<typename TA, typename TB, typename TC, typename TD, typename Func>
-int __attribute__ ((noinline)) vector_op(TA *A, TB *B, TC *C, TD alpha,
-    uint32_t N, Func op) {
-
-  uint32_t len_per_tile = ceil((float) N /
-      (float) (bsg_tiles_X * bsg_tiles_Y));
-
-  for (int i = 0; i < len_per_tile; ++i) {
-    int index = len_per_tile * __bsg_id + i;
-
-    if(index > N)
-      break;
-
-    C[index] = op(A[index], alpha * B[index]);
-  }
-
-  // Tile group size in PyTorch is set to 1,1 as of now.
-  // bsg_tile_group_barrier(&r_barrier, &c_barrier);
-
-  return 0;
-}
 
 /* We wrap all external-facing C++ kernels with `extern "C"` to
  * prevent name mangling
