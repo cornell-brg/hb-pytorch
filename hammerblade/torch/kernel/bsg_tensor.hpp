ade04e196cadd1e87d4290880042fa1dca35b391
lc873 <lc873@cornell.edu>
2020-03-06 01:15:06 -0500

    [kernel emul] define eva_t to be uint64_t if we are using emulation

--- hammerblade/torch/kernel/bsg_tensor.hpp
+++ hammerblade/torch/kernel/bsg_tensor.hpp
@@ -4,8 +4,13 @@
 typedef struct {
   uint32_t N;
   uint32_t dims;
+#ifdef HB_EMUL
+  uint64_t strides;
+  uint64_t data;
+#else
   uint32_t strides;
   uint32_t data;
+#endif
 } bsg_tensor_t;
 
 #endif // _BSG_TENSOR_HPP
