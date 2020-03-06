e2ec9d31b06f34511a3ce89a6b339801fd26ccaf
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-03-06 10:56:09 -0800

    small conv2d program

---
+++ hammerblade/torch/tests/test_hb_conv_op.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn.functional as F
+
+def test_conv2d_1():
+    kernel = torch.rand(8, 4, 3, 3)
+    inputs = torch.rand(1, 4, 5, 5)
+
+    kernel_hb = kernel.hammerblade()
+    inputs_hb = inputs.hammerblade()
+
+    conv_result = F.conv2d(inputs, kernel, padding=1)
+    conv_result_hb = F.conv2d(inputs_hb, kernel_hb, padding=1)
+
+    assert conv_result == conv_result_hb.cpu()
