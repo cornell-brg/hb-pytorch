891b6c4dfe3bf69945b9f6f89243cc98d0bf1dcf
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-03-09 11:16:54 -0700

    More conv tests

--- hammerblade/torch/tests/test_hb_conv_op.py
+++ hammerblade/torch/tests/test_hb_conv_op.py
@@ -2,8 +2,8 @@ import torch
 import torch.nn.functional as F
 
 def test_conv2d_1():
-    kernel = torch.rand(8, 4, 3, 3)
-    inputs = torch.rand(1, 4, 5, 5)
+    kernel = torch.rand(1, 1, 3, 3)
+    inputs = torch.rand(1, 1, 5, 5)
 
     kernel_hb = kernel.hammerblade()
     inputs_hb = inputs.hammerblade()
@@ -11,4 +11,40 @@ def test_conv2d_1():
     conv_result_hb = F.conv2d(inputs_hb, kernel_hb, padding=1)
     conv_result = F.conv2d(inputs, kernel, padding=1)
 
-    assert conv_result == conv_result_hb.cpu()
+    assert torch.allclose(conv_result, conv_result_hb.cpu())
+
+def test_conv2d_2():
+    kernel = torch.rand(1, 2, 3, 3)
+    inputs = torch.rand(1, 2, 5, 5)
+
+    kernel_hb = kernel.hammerblade()
+    inputs_hb = inputs.hammerblade()
+
+    conv_result_hb = F.conv2d(inputs_hb, kernel_hb, padding=1)
+    conv_result = F.conv2d(inputs, kernel, padding=1)
+
+    assert torch.allclose(conv_result, conv_result_hb.cpu())
+
+def test_conv2d_3():
+    kernel = torch.rand(2, 1, 3, 3)
+    inputs = torch.rand(1, 1, 5, 5)
+
+    kernel_hb = kernel.hammerblade()
+    inputs_hb = inputs.hammerblade()
+
+    conv_result_hb = F.conv2d(inputs_hb, kernel_hb, padding=1)
+    conv_result = F.conv2d(inputs, kernel, padding=1)
+
+    assert torch.allclose(conv_result, conv_result_hb.cpu())
+
+def test_conv2d_4():
+    kernel = torch.rand(5, 3, 3, 3)
+    inputs = torch.rand(2, 3, 5, 5)
+
+    kernel_hb = kernel.hammerblade()
+    inputs_hb = inputs.hammerblade()
+
+    conv_result_hb = F.conv2d(inputs_hb, kernel_hb, padding=(1,2), stride=(1,2))
+    conv_result = F.conv2d(inputs, kernel, padding=(1,2), stride=(1,2))
+
+    assert torch.allclose(conv_result, conv_result_hb.cpu())
