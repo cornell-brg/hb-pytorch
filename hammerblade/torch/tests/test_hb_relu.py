c309b0356df042b6a7b7ef59d0f37230049968d9
lc873 <lc873@cornell.edu>
2020-03-09 11:37:37 -0400

    [ReLU] added tests for relu, turned relu on for hammerblade; Known to fail due to missing threshold kernel

---
+++ hammerblade/torch/tests/test_hb_relu.py
@@ -0,0 +1,33 @@
+"""
+Tests on torch.nn.relu (threshold kernel)
+03/09/2020 Lin Cheng (lc873@cornell.edu)
+"""
+import torch
+import torch.nn as nn
+
+def test_torch_nn_relu_1():
+  x = torch.ones(10)
+  x_h = x.hammerblade()
+  relu = nn.ReLU()
+  x_relu = relu(x)
+  x_h_relu = relu(x_h)
+  assert x_h_relu.device == torch.device("hammerblade")
+  assert torch.equal(x_h_relu.cpu(), x_relu)
+
+def test_torch_nn_relu_2():
+  x = torch.randn(10)
+  x_h = x.hammerblade()
+  relu = nn.ReLU()
+  x_relu = relu(x)
+  x_h_relu = relu(x_h)
+  assert x_h_relu.device == torch.device("hammerblade")
+  assert torch.equal(x_h_relu.cpu(), x_relu)
+
+def test_torch_nn_relu_3():
+  x = torch.randn(3,4)
+  x_h = x.hammerblade()
+  relu = nn.ReLU()
+  x_relu = relu(x)
+  x_h_relu = relu(x_h)
+  assert x_h_relu.device == torch.device("hammerblade")
+  assert torch.equal(x_h_relu.cpu(), x_relu)
