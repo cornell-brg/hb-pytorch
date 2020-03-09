10c17cf7ed8569fe779b8a74879733aa12b8f821
lc873 <lc873@cornell.edu>
2020-03-09 12:30:53 -0400

    [addmm] added host code definiton and unit tests for addmm. It WILL fail due to not implemented

--- aten/src/ATen/native/hammerblade/Math.cpp
+++ aten/src/ATen/native/hammerblade/Math.cpp
@@ -24,4 +24,18 @@ Tensor dot_hb(const Tensor& self, const Tensor& other) {
   return sum;
 }
 
+
+Tensor addmm_hb(
+    const Tensor& self,
+    const Tensor& mat1,
+    const Tensor& mat2,
+    Scalar beta,
+    Scalar alpha
+) {
+
+  //TODO: implmement the correct addmm_hb host code
+  return self;
+
+}
+
 }} // namespace at::native
