5b92672cc6e7e3fd3ab42d9a5162f8f85f8b1c3d
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-03-12 17:27:22 -0700

    Updated run-hb-pytest command

--- run-hb-pytest.sh
+++ run-hb-pytest.sh
@@ -9,4 +9,4 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
-make -C $DIR/hammerblade/torch clean test_loader.cosim.log
+make -C $DIR/hammerblade/torch clean regression
