d85439cf475e64df76d2e55777862e77ffd4f0f0
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-03-16 14:46:57 -0700

    cnn forward hb testscase

--- hammerblade/torch/tests/test_lenet5.py
+++ hammerblade/torch/tests/test_lenet5.py
@@ -61,7 +61,7 @@ def train(net, loader, epochs, optimizer, loss_func):
 
       if (batch_idx % 1000) == 0:
         print('epoch {} : [{}/{} ({:.0f}%)]\tLoss={:.6f}'.format(
-          epoch, batch_idx*batch_size, len(loader.dataset), 
+          epoch, batch_idx*batch_size, len(loader.dataset),
           100. * (batch_idx/len(loader)), loss.item()
         ))
 
@@ -89,6 +89,18 @@ def test(net, loader, loss_func):
             test_loss, num_correct, len(loader.dataset), test_accuracy
   ))
 
+def test_lenet5_forward_1():
+    net = LeNet5()
+    net_hb = net.to(device='hammerblade')
+
+    data = torch.rand(1, 1, 32, 32)
+    data_hb = data.hammerblade()
+
+    output = net.forward(data)
+    output_hb = net_hb.forward(data_hb)
+
+    assert torch.allclose(output, output_hb.cpu())
+
 @pytest.mark.skip(reason = "Not implented for HB yet.")
 def test_lenet5_mnist():
     """
@@ -104,29 +116,29 @@ def test_lenet5_mnist():
       net.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM
     )
     loss_func = nn.CrossEntropyLoss()
-    
+
     # Data
     transforms = torchvision.transforms.Compose([
       torchvision.transforms.Resize((32,32)),
       torchvision.transforms.ToTensor(),
     ])
-    
+
     trainset = torchvision.datasets.MNIST(
       root='./data/mnist', train=True, download=True, transform=transforms
     )
     trainloader = torch.utils.data.DataLoader(
       trainset, batch_size=BATCH_SIZE, shuffle=True, num_workers=2
     )
-    
+
     testset = torchvision.datasets.MNIST(
       root='./data/mnist', train=False, download=True, transform=transforms
     )
     testloader = torch.utils.data.DataLoader(
       testset, batch_size=BATCH_SIZE, shuffle=False, num_workers=2
     )
-    
+
     # Train
     train(net, trainloader, EPOCHS, optimizer, loss_func)
-    
+
     # Test
     test(net, testloader, loss_func)
