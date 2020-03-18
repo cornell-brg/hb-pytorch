e9a40b26420cdb94c8e4fe4c20dd1a9367c892ce
lc873 <lc873@cornell.edu>
2020-03-18 16:35:41 -0400

    [contiguous] add the tests for torch.contiguous

---
+++ hammerblade/torch/tests/test_contiguous.py
@@ -0,0 +1,36 @@
+"""
+Tests on torch.contiguous
+03/18/2020 Lin Cheng (lc873@cornell.edu)
+"""
+import torch
+
+# These tensors are already contiguous
+def test_torch_contiguous_1():
+    x = torch.ones(10).hammerblade()
+    y = x.contiguous()
+    assert y.device == torch.device("hammerblade")
+    assert y.is_contiguous()
+    assert torch.equal(x.cpu(), y.cpu())
+
+def test_torch_contiguous_2():
+    x = torch.randn(10).hammerblade()
+    y = x.contiguous()
+    assert y.device == torch.device("hammerblade")
+    assert y.is_contiguous()
+    assert torch.equal(x.cpu(), y.cpu())
+
+def test_torch_contiguous_3():
+    x = torch.randn(3, 4).hammerblade()
+    y = x.contiguous()
+    assert y.device == torch.device("hammerblade")
+    assert y.is_contiguous()
+    assert torch.equal(x.cpu(), y.cpu())
+
+# transpose makes a tensor non contiguous
+def test_torch_contiguous_4():
+    x = torch.randn(3, 4).hammerblade().t()
+    assert not x.is_contiguous()
+    y = x.contiguous()
+    assert y.device == torch.device("hammerblade")
+    assert y.is_contiguous()
+    assert torch.equal(x.cpu(), y.cpu())
