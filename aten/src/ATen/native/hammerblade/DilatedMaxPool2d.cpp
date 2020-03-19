840e31c40ab0c9f5e1d32a2c861cef691b5bf9fc
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-03-18 20:27:19 -0700

    Argument check for maxpool2d

--- aten/src/ATen/native/hammerblade/DilatedMaxPool2d.cpp
+++ aten/src/ATen/native/hammerblade/DilatedMaxPool2d.cpp
@@ -1,10 +1,98 @@
 #include <ATen/ATen.h>
 #include <ATen/native/hammerblade/HammerBladeTensor.h>
 #include <ATen/native/hammerblade/Offload.h>
+#include <ATen/native/Pool.h>
 
 namespace at {
 namespace native {
 namespace { // anonumous
+
+void max_pool2d_with_indices_out_hb_template(
+           Tensor& output,
+           Tensor& indices,
+           const Tensor& input_,
+           IntArrayRef kernel_size,
+           IntArrayRef stride,
+           IntArrayRef padding,
+           IntArrayRef dilation,
+           bool ceil_mode)
+{
+  TensorArg output_arg{ output, "output", 1 };
+  TensorArg indices_arg{ indices, "indices", 2 };
+  TensorArg input_arg{ input_, "input_", 3 };
+
+  checkAllSameHB("max_pool2d_with_indices_out_hb",
+                  {output_arg, indices_arg, input_arg});
+
+  // #20866, #22032: Guarantee this for the official C++ API?
+  TORCH_CHECK(kernel_size.size() == 1 || kernel_size.size() == 2,
+    "max_pool2d: kernel_size must either be a single int, or a tuple of two ints")
+  const int kH = safe_downcast<int, int64_t>(kernel_size[0]);
+  const int kW = kernel_size.size() == 1 ? kH : safe_downcast<int, int64_t>(kernel_size[1]);
+
+  // NB: stride default is not expressible as an integer constant, so we accept
+  // empty stride for this case
+  TORCH_CHECK(stride.size() == 0 || stride.size() == 1 || stride.size() == 2,
+    "max_pool2d: stride must either be omitted, a single int, or a tuple of two ints")
+  const int dH = stride.empty() ? kH : safe_downcast<int, int64_t>(stride[0]);
+  const int dW = stride.empty() ? kW :
+                 stride.size() == 1 ? dH : safe_downcast<int, int64_t>(stride[1]);
+
+  TORCH_CHECK(padding.size() == 1 || padding.size() == 2,
+    "max_pool2d: padding must be either be a single int, or a tuple of two ints");
+  const int padH = safe_downcast<int, int64_t>(padding[0]);
+  const int padW = padding.size() == 1 ? padH : safe_downcast<int, int64_t>(padding[1]);
+
+  TORCH_CHECK(dilation.size() == 1 || dilation.size() == 2,
+    "max_pool2d: dilation must be either a single int, or a tuple of two ints");
+  const int dilationH = safe_downcast<int, int64_t>(dilation[0]);
+  const int dilationW = dilation.size() == 1 ? dilationH : safe_downcast<int, int64_t>(dilation[1]);
+
+  TORCH_CHECK((input_.ndimension() == 3 || input_.ndimension() == 4),
+    "non-empty 3D or 4D (batch mode) tensor expected for input");
+
+  const int64_t nbatch = input_.ndimension() == 4 ? input_.size(-4) : 1;
+  const int64_t nInputPlane = input_.size(-3);
+  const int64_t inputHeight = input_.size(-2);
+  const int64_t inputWidth = input_.size(-1);
+
+  const int64_t outputWidth = pooling_output_shape<int64_t>(inputWidth, kW, padW, dW, dilationW, ceil_mode);
+  const int64_t outputHeight = pooling_output_shape<int64_t>(inputHeight, kH, padH, dH, dilationH, ceil_mode);
+
+  pool2d_shape_check(
+    input_,
+    kH, kW, dH, dW, padH, padW, dilationH, dilationW,
+    nInputPlane,
+    inputHeight, inputWidth,
+    outputHeight, outputWidth);
+
+  Tensor input = input_.contiguous();
+
+  const int64_t in_stride_c = input.stride(-3);
+  const int64_t in_stride_h = input.stride(-2);
+  const int64_t in_stride_w = input.stride(-1);
+
+  output.resize_({nbatch, nInputPlane, outputHeight, outputWidth});
+  indices.resize_({nbatch, nInputPlane, outputHeight, outputWidth});
+
+  const int count = safe_downcast<int, int64_t>(output.numel());
+
+  AT_DISPATCH_FLOAT_TYPE_ONLY(input.scalar_type(),
+    "max_pool2d_with_indices_out_cuda_frame",
+    [&] {
+      scalar_t *output_data = output.data_ptr<scalar_t>();
+      scalar_t *input_data = input.data_ptr<scalar_t>();
+      int64_t *indices_data = indices.data_ptr<int64_t>();
+
+      TORCH_CHECK(false, "Offload maxpool2d.");
+    }
+  );
+
+  if(input.ndimension() == 3) {
+    output.resize_({nInputPlane, outputHeight, outputWidth});
+  }
+}
+
 } // anonymous
 
 std::tuple<Tensor, Tensor> max_pool2d_with_indices_hb(
@@ -17,15 +105,15 @@ std::tuple<Tensor, Tensor> max_pool2d_with_indices_hb(
 {
   Tensor output = at::empty({0}, input.options());
   Tensor indices = at::empty({0}, input.options().dtype(kLong));
-  //max_pool2d_with_indices_out_cpu_template(
-  //  output,
-  //  indices,
-  //  input,
-  //  kernel_size,
-  //  stride,
-  //  padding,
-  //  dilation,
-  //  ceil_mode);
+  max_pool2d_with_indices_out_hb_template(
+    output,
+    indices,
+    input,
+    kernel_size,
+    stride,
+    padding,
+    dilation,
+    ceil_mode);
   TORCH_CHECK(false, "Implement HB maxpool2d.");
   return std::tuple<Tensor, Tensor>(output, indices);
 }
