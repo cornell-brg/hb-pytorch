5e2f2b55299270cea70f05f204987b1c531eecbc
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-03-19 01:01:34 -0700

    Offload max_pool2d

---
+++ hammerblade/torch/kernel/kernel_pooling.cpp
@@ -0,0 +1,39 @@
+//====================================================================
+// Pooling kernel
+// 03/19/2020 Bandhav Veluri
+//====================================================================
+
+#include <kernel_common.hpp>
+
+// We wrap all external-facing C++ kernels with `extern "C"` to
+// prevent name mangling
+
+extern "C" {
+
+  __attribute__ ((noinline))  int tensorlib_max_pool2d(
+          bsg_tensor_t* output,
+          bsg_tensor_t* input,
+          bsg_tensor_t* indices,
+          int* kH, int* kW,
+          int* dH, int* dW,
+          int* padH, int* padW,
+          int* dilationH, int* dilationW) {
+    // Start profiling
+    bsg_cuda_print_stat_kernel_start();
+
+    exit(1);
+
+    // End profiling
+    bsg_cuda_print_stat_kernel_end();
+    return 0;
+  }
+
+  HB_EMUL_REG_KERNEL(tensorlib_max_pool2d,
+    output,
+    input,
+    indices,
+    kH, kW,
+    dH, dW,
+    padH, padW,
+    dilationH, dilationW);
+}
