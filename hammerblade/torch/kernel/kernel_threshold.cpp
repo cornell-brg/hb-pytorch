c4f38df47b7d11e41abadceb00c8984c7c927841
lc873 <lc873@cornell.edu>
2020-03-19 08:51:33 -0400

    [threshold] make the interface cleaner

--- hammerblade/torch/kernel/kernel_threshold.cpp
+++ hammerblade/torch/kernel/kernel_threshold.cpp
@@ -1,6 +1,6 @@
 //====================================================================
-// addmm kernel
-// 03/09/2020 YOUR NAME HERE Lin Cheng (YOU EMAIL HERE, lc873@cornell.edu)
+// threshold kernel
+// 03/19/2020 YOUR NAME HERE Lin Cheng (YOU EMAIL HERE, lc873@cornell.edu)
 //====================================================================
 
 #include <kernel_common.hpp>
@@ -8,11 +8,11 @@
 extern "C" {
 
   __attribute__ ((noinline))  int tensorlib_threshold(
-          bsg_tensor_t* res,
-          bsg_tensor_t* self,
-          bsg_tensor_t* other,
-          float* threshold_scalar,
-          float* value_scalar) {
+          bsg_tensor_t* t0_p,
+          bsg_tensor_t* t1_p,
+          bsg_tensor_t* t2_p,
+          float* _threshold_scalar_p,
+          float* _value_scalar_p) {
     // TODO: Convert uint32_t pointers to correct types
     // Start profiling
     bsg_cuda_print_stat_kernel_start();
@@ -21,7 +21,7 @@ extern "C" {
     bsg_cuda_print_stat_kernel_end();
     return 0;
   }
-  
+
   HB_EMUL_REG_KERNEL(tensorlib_threshold, bsg_tensor_t*, bsg_tensor_t*,
                      bsg_tensor_t*, float*, float*)
 
