2b7a83fc660e95d72231323626f8cf5a7de30170
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-03-19 09:33:14 -0700

    Revert threshold implementation

--- hammerblade/torch/kernel/kernel_threshold.cpp
+++ hammerblade/torch/kernel/kernel_threshold.cpp
@@ -1,6 +1,6 @@
 //====================================================================
-// threshold kernel
-// 03/09/2020 Bandhav Veluri Lin Cheng (YOU EMAIL HERE, lc873@cornell.edu)
+// addmm kernel
+// 03/09/2020 YOUR NAME HERE Lin Cheng (YOU EMAIL HERE, lc873@cornell.edu)
 //====================================================================
 
 #include <kernel_common.hpp>
@@ -13,34 +13,15 @@ extern "C" {
           bsg_tensor_t* other,
           float* threshold_scalar,
           float* value_scalar) {
-    auto N = self->N; // number of elements
-    auto out = (float*) ((intptr_t) res->data);
-    auto in = (float*) ((intptr_t) self->data);
-    auto threshold = *threshold_scalar;
-    auto value = *value_scalar;
-
-    // Calculate elements per tile
-    uint32_t len_per_tile = N / (bsg_tiles_X * bsg_tiles_Y) + 1;
-    uint32_t start = len_per_tile * __bsg_id;
-    uint32_t end = start + len_per_tile;
-    end = (end > N)  ? N : end;
-
+    // TODO: Convert uint32_t pointers to correct types
     // Start profiling
     bsg_cuda_print_stat_kernel_start();
-
-    for(int i = start; i < end; ++i) {
-      if(in[i] > threshold) {
-        out[i] = in[i];
-      } else {
-        out[i] = value;
-      }
-    }
-
+    // TODO: Implement threshold
     //   End profiling
     bsg_cuda_print_stat_kernel_end();
     return 0;
   }
-
+  
   HB_EMUL_REG_KERNEL(tensorlib_threshold, bsg_tensor_t*, bsg_tensor_t*,
                      bsg_tensor_t*, float*, float*)
 
