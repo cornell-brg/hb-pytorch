542fc1c4b9cef0925253ba72ed89bbb9bd11f012
lc873 <lc873@cornell.edu>
2020-03-19 08:48:20 -0400

    Revert "[relu] implemented threshold kernel"
    This reverts commit b5bb7215dc4c6a4dd00824d07fcfbf1f7bf12c42.

--- aten/src/ATen/native/hammerblade/Threshold.cpp
+++ aten/src/ATen/native/hammerblade/Threshold.cpp
@@ -2,7 +2,6 @@
 #include <ATen/hammerblade/HammerBladeContext.h>
 #include <ATen/native/TensorIterator.h>
 #include <ATen/native/Activation.h>
-#include <ATen/native/hammerblade/Offload.h>
 
 namespace at { namespace native {
 namespace {
@@ -12,10 +11,13 @@ static void threshold_kernel_hb(
     TensorIterator& iter,
     Scalar threshold_scalar,
     Scalar value_scalar) {
-  AT_DISPATCH_FLOAT_TYPE_ONLY(iter.dtype(), "threshold_hb", [&]() {
-      offload_op_binary(iter, threshold_scalar, value_scalar,
-                        "tensorlib_threshold");
-      });
+
+  TORCH_CHECK(false, "threshold_kernel_hb not implemented");
+    //TODO: implement the host code for threshold kernel
+    //      you may refer to add_kernel_hb in
+    //      aten/src/ATen/native/hammerblade/AddSub.cpp
+    //      as an example
+
 }
 
 } // anonymous namespace
--- hammerblade/torch/kernel/kernel_threshold.cpp
+++ hammerblade/torch/kernel/kernel_threshold.cpp
@@ -1,6 +1,6 @@
 //====================================================================
-// Threshold kernel
-// 03/17/2020 Lin Cheng (lc873@cornell.edu)
+// addmm kernel
+// 03/09/2020 YOUR NAME HERE Lin Cheng (YOU EMAIL HERE, lc873@cornell.edu)
 //====================================================================
 
 #include <kernel_common.hpp>
@@ -8,19 +8,15 @@
 extern "C" {
 
   __attribute__ ((noinline))  int tensorlib_threshold(
-          bsg_tensor_t* t0_p,
-          bsg_tensor_t* t1_p,
-          bsg_tensor_t* t2_p,
-          float* _threshold_scalar_p,
-          float* _value_scalar_p) {
-    float threshold = *_threshold_scalar_p;
-    float value = *_value_scalar_p;
+          bsg_tensor_t* res,
+          bsg_tensor_t* self,
+          bsg_tensor_t* other,
+          float* threshold_scalar,
+          float* value_scalar) {
+    // TODO: Convert uint32_t pointers to correct types
     // Start profiling
     bsg_cuda_print_stat_kernel_start();
-    brg_tile_element_wise_for(t0_p, t1_p, t2_p,
-      [&](float x, float other) {
-        return  x <= threshold ? value : other;
-    });
+    // TODO: Implement threshold
     //   End profiling
     bsg_cuda_print_stat_kernel_end();
     return 0;
