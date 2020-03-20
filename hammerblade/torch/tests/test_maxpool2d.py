64ef34496eadecb14dd9c6f7da1b1e23020fab1a
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-03-19 22:00:45 -0700

    max_pool2d refactored testcases

--- hammerblade/torch/tests/test_maxpool2d.py
+++ hammerblade/torch/tests/test_maxpool2d.py
@@ -5,16 +5,32 @@ Unit tests for maxpool2d operator
 
 import torch
 import torch.nn.functional as F
+import os
+import pytest
 
-def test_max_pool2d_1():
-    x = torch.rand(1, 1, 5, 5)
+def _test_max_pool2d(x, kernel_size, stride=None, padding=1):
     x_hb = x.hammerblade()
 
-    y, r = F.max_pool2d(x, 3, stride=2, return_indices=True)
-    y_hb, r_hb = F.max_pool2d(x_hb, 3, stride=2, return_indices=True)
+    y, r = F.max_pool2d(x, kernel_size, stride, padding, return_indices=True)
+    y_hb, r_hb =  F.max_pool2d(x_hb, kernel_size, stride, padding,
+                               return_indices=True)
 
     assert torch.allclose(y, y_hb.cpu())
     assert torch.equal(r, r_hb.cpu())
+
+def test_max_pool2d_1():
+    x = torch.rand(1, 1, 5, 5)
+    kernel_size = 3
+    stride = 2
+    padding = 0
+    _test_max_pool2d(x, kernel_size, stride, padding)
+
+def test_max_pool2d_2():
+    x = torch.ones(1, 1, 5, 5)
+    kernel_size = 3
+    stride = 2
+    padding = 0
+    _test_max_pool2d(x, kernel_size, stride, padding)
 
 if __name__ == "__main__":
     test_max_pool2d_1()
