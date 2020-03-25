6b80dccdf26ccc017bc7a828251d11d9101b1230
lc873 <lc873@cornell.edu>
2020-03-25 14:55:26 -0400

    [test type casting] on deivce type casting works for long -> int only

---
+++ hammerblade/torch/tests/test_type_casting.py
@@ -0,0 +1,50 @@
+"""
+Tests on torch.to (copy_hb_to_hb kernel)
+03/18/2020 Lin Cheng (lc873@cornell.edu)
+"""
+
+import torch
+import hypothesis.strategies as st
+from hypothesis import given, settings
+from .hypothesis_test_util import HypothesisUtil as hu
+
+def test_torch_to_long_to_int_1():
+    x = torch.ones(10, dtype=torch.long)
+    assert x.type() == 'torch.LongTensor'
+    h = x.hammerblade()
+    int_x = x.to(torch.int)
+    int_h = h.to(torch.int)
+    assert int_h.type() == 'torch.hammerblade.IntTensor'
+    assert int_h.device == torch.device("hammerblade")
+    assert torch.equal(int_x, int_h.cpu())
+
+def test_torch_to_long_to_int_2():
+    x = torch.LongTensor(10).random_(0, 10)
+    assert x.type() == 'torch.LongTensor'
+    h = x.hammerblade()
+    int_x = x.to(torch.int)
+    int_h = h.to(torch.int)
+    assert int_h.type() == 'torch.hammerblade.IntTensor'
+    assert int_h.device == torch.device("hammerblade")
+    assert torch.equal(int_x, int_h.cpu())
+
+def test_torch_to_long_to_int_3():
+    x = torch.LongTensor(64).random_(-2147483648, 2147483647)
+    assert x.type() == 'torch.LongTensor'
+    h = x.hammerblade()
+    int_x = x.to(torch.int)
+    int_h = h.to(torch.int)
+    assert int_h.type() == 'torch.hammerblade.IntTensor'
+    assert int_h.device == torch.device("hammerblade")
+    assert torch.equal(int_x, int_h.cpu())
+
+@pytest.mark.skip(reason="known bug")
+def test_torch_to_int_to_long_1():
+    x = torch.ones(10, dtype=torch.int)
+    assert x.type() == 'torch.IntTensor'
+    h = x.hammerblade()
+    long_x = x.to(torch.long)
+    long_h = h.to(torch.long)
+    assert long_h.type() == 'torch.hammerblade.LongTensor'
+    assert long_h.device == torch.device("hammerblade")
+    assert torch.equal(long_x, long_h.cpu())
