3285bdb1db52d104faa494b4edc7fb70d084b902
lc873 <lc873@cornell.edu>
2020-03-26 12:52:12 -0400

    [tests] python style fixes

--- hammerblade/torch/tests/test_LossNLL_backward.py
+++ hammerblade/torch/tests/test_LossNLL_backward.py
@@ -11,11 +11,13 @@ def test_torch_nn_NLLLoss_mean_back_1():
     m = nn.LogSoftmax(dim=1)
     loss = nn.NLLLoss(reduction='mean')
     input = torch.tensor([[-3.5164, -1.9673, -4.7514, -0.2075, -4.6912],
-        [-2.9321, -1.5085, -0.8699, -1.4016, -2.8089],
-        [-2.6460, -1.9800, -1.4818, -1.8358, -0.9057]], requires_grad=True)
+                         [-2.9321, -1.5085, -0.8699, -1.4016, -2.8089],
+                         [-2.6460, -1.9800, -1.4818, -1.8358, -0.9057]],
+                         requires_grad=True)
     input_h = torch.tensor([[-3.5164, -1.9673, -4.7514, -0.2075, -4.6912],
-        [-2.9321, -1.5085, -0.8699, -1.4016, -2.8089],
-        [-2.6460, -1.9800, -1.4818, -1.8358, -0.9057]], requires_grad=True)
+                           [-2.9321, -1.5085, -0.8699, -1.4016, -2.8089],
+                           [-2.6460, -1.9800, -1.4818, -1.8358, -0.9057]],
+                           requires_grad=True)
     target = torch.tensor([1, 0, 4])
     output = loss(m(input), target)
     output_h = loss(m(input_h).hammerblade(), target.hammerblade())
@@ -44,11 +46,13 @@ def test_torch_nn_NLLLoss_sum_back_1():
     m = nn.LogSoftmax(dim=1)
     loss = nn.NLLLoss(reduction='sum')
     input = torch.tensor([[-3.5164, -1.9673, -4.7514, -0.2075, -4.6912],
-        [-2.9321, -1.5085, -0.8699, -1.4016, -2.8089],
-        [-2.6460, -1.9800, -1.4818, -1.8358, -0.9057]], requires_grad=True)
+                         [-2.9321, -1.5085, -0.8699, -1.4016, -2.8089],
+                         [-2.6460, -1.9800, -1.4818, -1.8358, -0.9057]],
+                         requires_grad=True)
     input_h = torch.tensor([[-3.5164, -1.9673, -4.7514, -0.2075, -4.6912],
-        [-2.9321, -1.5085, -0.8699, -1.4016, -2.8089],
-        [-2.6460, -1.9800, -1.4818, -1.8358, -0.9057]], requires_grad=True)
+                           [-2.9321, -1.5085, -0.8699, -1.4016, -2.8089],
+                           [-2.6460, -1.9800, -1.4818, -1.8358, -0.9057]],
+                           requires_grad=True)
     target = torch.tensor([1, 0, 4])
     output = loss(m(input), target)
     output_h = loss(m(input_h).hammerblade(), target.hammerblade())
@@ -77,11 +81,13 @@ def test_torch_nn_NLLLoss_none_back_1():
     m = nn.LogSoftmax(dim=1)
     loss = nn.NLLLoss(reduction='none')
     input = torch.tensor([[-3.5164, -1.9673, -4.7514, -0.2075, -4.6912],
-        [-2.9321, -1.5085, -0.8699, -1.4016, -2.8089],
-        [-2.6460, -1.9800, -1.4818, -1.8358, -0.9057]], requires_grad=True)
+                         [-2.9321, -1.5085, -0.8699, -1.4016, -2.8089],
+                         [-2.6460, -1.9800, -1.4818, -1.8358, -0.9057]],
+                         requires_grad=True)
     input_h = torch.tensor([[-3.5164, -1.9673, -4.7514, -0.2075, -4.6912],
-        [-2.9321, -1.5085, -0.8699, -1.4016, -2.8089],
-        [-2.6460, -1.9800, -1.4818, -1.8358, -0.9057]], requires_grad=True)
+                           [-2.9321, -1.5085, -0.8699, -1.4016, -2.8089],
+                           [-2.6460, -1.9800, -1.4818, -1.8358, -0.9057]],
+                           requires_grad=True)
     target = torch.tensor([1, 0, 4])
     grad = torch.tensor([1., 2., 3.])
     output = loss(m(input), target)
