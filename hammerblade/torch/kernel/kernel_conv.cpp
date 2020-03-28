1ac84177f91b2a581b0d6b13733c9787418f2fee
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-03-28 14:25:12 -0700

    Conv2d input gradients

--- hammerblade/torch/kernel/kernel_conv.cpp
+++ hammerblade/torch/kernel/kernel_conv.cpp
@@ -107,8 +107,55 @@ extern "C" {
           bsg_tensor_t* weight,
           bsg_vector_t* padding,
           bsg_vector_t* strides) {
+    auto x = BSGTensor<float>(grad_input);
+    auto y = BSGTensor<float>(grad_output);
+    auto w = BSGTensor<float>(weight);
+    auto p = BSGVector<uint32_t>(padding);
+    auto s = BSGVector<uint32_t>(strides);
+
+    // Conv2d backward parameters
+    auto N = y.dim(0); // number of minibatches
+    auto Cout = y.dim(1); // number of output channels
+    auto Hout = y.dim(2);
+    auto Wout = y.dim(3);
+    auto Cin = x.dim(1); // number of input channels
+    auto Hin = x.dim(2);
+    auto Win = x.dim(3);
+    auto Kh = w.dim(2);
+    auto Kw = w.dim(3);
+    auto Sh = s[0];
+    auto Sw = s[1];
+    auto Ph = p[0];
+    auto Pw = p[1];
+
     // Start profiling
     bsg_cuda_print_stat_kernel_start();
+
+    // Preliminary single tile implementation
+    //
+    // Grows O(^5) with image size:
+    //   N x Cout x Cin x H x W
+    //   Kernel loops are constant-time
+    if(__bsg_id == 0) {
+      for(uint32_t n = 0; n < N; ++n)
+        for(uint32_t co = 0; co < Cout; ++co)
+          for(uint32_t yh = 0; yh < Hout; ++yh)
+            for(uint32_t yw = 0; yw < Wout; ++yw)
+              for(uint32_t ci = 0; ci < Cin; ++ci)
+                for(uint32_t kh = 0; kh < Kh; ++kh)
+                  for(uint32_t kw = 0; kw < Kw; ++kw) {
+                    int32_t xh = Sh * yh - Ph + kh;
+                    int32_t xw = Sw * yw - Pw + kw;
+
+                    if((ci + kh + kw) == 0) {
+                      x(n, ci, xh, xw) = 0.0;
+                    }
+
+                    if(xh >= 0 && xh < Hin && xw >= 0 && xw < Win) {
+                      x(n, ci, xh, xw) += y(n, co, yh, yw) * w(co, ci, kh, kw);
+                    } // else 0
+                  }
+    }
 
     // End profiling
     bsg_cuda_print_stat_kernel_end();
