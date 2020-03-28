293b0722b1745145c823a9ebaaaf1eb59eed8226
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-03-28 16:46:21 -0700

    Con2d backward weight grandients

--- hammerblade/torch/kernel/kernel_conv.cpp
+++ hammerblade/torch/kernel/kernel_conv.cpp
@@ -113,7 +113,7 @@ extern "C" {
     auto p = BSGVector<uint32_t>(padding);
     auto s = BSGVector<uint32_t>(strides);
 
-    // Conv2d backward parameters
+    // Conv2d parameters
     auto N = y.dim(0); // number of minibatches
     auto Cout = y.dim(1); // number of output channels
     auto Hout = y.dim(2);
@@ -171,8 +171,58 @@ extern "C" {
           bsg_tensor_t* input,
           bsg_vector_t* padding,
           bsg_vector_t* strides) {
+    auto x = BSGTensor<float>(input);
+    auto y = BSGTensor<float>(grad_output);
+    auto w = BSGTensor<float>(grad_weight);
+    auto p = BSGVector<uint32_t>(padding);
+    auto s = BSGVector<uint32_t>(strides);
+
+    // Conv2d parameters
+    auto N = y.dim(0); // number of minibatches
+    auto Cout = y.dim(1); // number of output channels
+    auto Hout = y.dim(2);
+    auto Wout = y.dim(3);
+    auto Cin = x.dim(1); // number of input channels
+    auto Hin = x.dim(2);
+    auto Win = x.dim(3);
+    auto Kh = w.dim(2);
+    auto Kw = w.dim(3);
+    auto Sh = s[0];
+    auto Sw = s[1];
+    auto Ph = p[0];
+    auto Pw = p[1];
+
     // Start profiling
     bsg_cuda_print_stat_kernel_start();
+
+    // Preliminary single tile implementation
+    //
+    // Grows O(^5) with image size:
+    //   N x Cout x Cin x H x W
+    //   Kernel loops are constant-time
+    if(__bsg_id == 0) {
+      // init weight grad
+      for(uint32_t ci = 0; ci < Cin; ++ci)
+        for(uint32_t co = 0; co < Cout; ++co)
+          for(uint32_t kh = 0; kh < Kh; ++kh)
+            for(uint32_t kw = 0; kw < Kw; ++kw)
+              w(ci, co, kh, kw) = 0.0f;
+
+      for(uint32_t n = 0; n < N; ++n)
+        for(uint32_t co = 0; co < Cout; ++co)
+          for(uint32_t yh = 0; yh < Hout; ++yh)
+            for(uint32_t yw = 0; yw < Wout; ++yw)
+              for(uint32_t ci = 0; ci < Cin; ++ci)
+                for(uint32_t kh = 0; kh < Kh; ++kh)
+                  for(uint32_t kw = 0; kw < Kw; ++kw) {
+                    int32_t xh = Sh * yh - Ph + kh;
+                    int32_t xw = Sw * yw - Pw + kw;
+
+                    if(xh >= 0 && xh < Hin && xw >= 0 && xw < Win) {
+                      w(ci, co, kh, kw) += y(n, co, yh, yw) * x(n, ci, xh, xw);
+                    } // else 0
+                  }
+    }
 
     // End profiling
     bsg_cuda_print_stat_kernel_end();
