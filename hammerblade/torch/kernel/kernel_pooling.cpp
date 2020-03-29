46bbfa02e5257fedfbeedcd2a5184d1b45b1bf1a
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-03-29 00:34:46 -0700

    MaxPool2d backward kernel

--- hammerblade/torch/kernel/kernel_pooling.cpp
+++ hammerblade/torch/kernel/kernel_pooling.cpp
@@ -78,11 +78,47 @@ extern "C" {
           bsg_tensor_t* gradOutput,
           bsg_tensor_t* indices,
           bsg_tensor_t* input,
-          bsg_vector_t* kernel_size,
-          bsg_vector_t* padding,
-          bsg_vector_t* stride) {
+          int* kH, int* kW,
+          int* sH, int* sW,
+          int* padH, int* padW) {
+    auto y = BSGTensor<float>(gradOutput);
+    auto x = BSGTensor<float>(gradInput);
+    auto ind = BSGTensor<int>(indices);
+
+    // max_pool2d parameters
+    auto N = y.dim(0); // number of minibatches
+    auto C = y.dim(1); // number of channels
+    auto Hout = y.dim(2);
+    auto Wout = y.dim(3);
+    auto Hin = x.dim(2);
+    auto Win = x.dim(3);
+    auto Kh = *kH;
+    auto Kw = *kW;
+    auto Sh = *sH;
+    auto Sw = *sW;
+    auto Ph = *padH;
+    auto Pw = *padW;
+
     // Start profiling
     bsg_cuda_print_stat_kernel_start();
+
+    if(__bsg_id == 0) {
+      for(uint32_t n = 0; n < N; ++n)
+        for(uint32_t c = 0; c < C; ++c)
+          for(uint32_t xh = 0; xh < Hin; ++xh)
+            for(uint32_t xw = 0; xw < Win; ++xw)
+              x(n, c, xh, xw) = 0.0f;
+
+      for(uint32_t n = 0; n < N; ++n)
+        for(uint32_t c = 0; c < C; ++c)
+          for(uint32_t yh = 0; yh < Hout; ++yh)
+            for(uint32_t yw = 0; yw < Wout; ++yw) {
+              int32_t xh = ind(n, c, yh, yw) / Win;
+              int32_t xw = ind(n, c, yh, yw) % Win;
+
+              x(n, c, xh, xw) += y(n, c, yh, yw);
+            }
+    }
 
     // End profiling
     bsg_cuda_print_stat_kernel_end();
@@ -99,7 +135,10 @@ extern "C" {
     int*, int*);
 
   HB_EMUL_REG_KERNEL(tensorlib_max_pool2d_backward,
-    bsg_tensor_t*, bsg_tensor_t*, bsg_tensor_t*, bsg_tensor_t*,
-    bsg_vector_t*, bsg_vector_t*, bsg_vector_t*);
+    bsg_tensor_t*, bsg_tensor_t*,
+    bsg_tensor_t*, bsg_tensor_t*,
+    int*, int*,
+    int*, int*,
+    int*, int*);
 
 }
