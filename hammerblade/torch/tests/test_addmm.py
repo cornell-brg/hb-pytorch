afc53e24e367c515b438c75dd420146ab5131d8b
Kexin Zheng <kz73@brg-vip.ece.cornell.edu>
2020-03-30 13:53:28 -0400

    fix python file format

--- hammerblade/torch/tests/test_addmm.py
+++ hammerblade/torch/tests/test_addmm.py
@@ -5,7 +5,7 @@ Unit tests for torch.addmm kernel
 import torch
 import pytest
 
-#@pytest.mark.skip(reason="not yet implemented")
+# @pytest.mark.skip(reason="not yet implemented")
 def test_torch_addmm_1():
     M = torch.ones(2, 3)
     M_h = M.hammerblade()
@@ -18,7 +18,7 @@ def test_torch_addmm_1():
     assert out_h.device == torch.device("hammerblade")
     assert torch.allclose(out_h.cpu(), out)
 
-#@pytest.mark.skip(reason="not yet implemented")
+# @pytest.mark.skip(reason="not yet implemented")
 # 1x1 matrices
 def test_torch_addmm_2():
     M = torch.randn(1, 1)
@@ -32,7 +32,7 @@ def test_torch_addmm_2():
     assert out_h.device == torch.device("hammerblade")
     assert torch.allclose(out_h.cpu(), out)
 
-#@pytest.mark.skip(reason="not yet implemented")
+# @pytest.mark.skip(reason="not yet implemented")
 # broadcast self to result
 def test_torch_addmm_3():
     M = torch.randn(1, 3)
@@ -50,7 +50,7 @@ def test_torch_addmm_3():
     assert out_h.device == torch.device("hammerblade")
     assert torch.allclose(out_h.cpu(), out)
 
-#@pytest.mark.skip(reason="not yet implemented")
+# @pytest.mark.skip(reason="not yet implemented")
 # bigger matrix
 def test_torch_addmm_4():
     M = torch.randn(1, 1)
@@ -66,16 +66,17 @@ def test_torch_addmm_4():
     print('actual')
     print(out_h.cpu())
     assert out_h.device == torch.device("hammerblade")
-    assert torch.allclose(out_h.cpu(), out, 1e-07, 1e-04) # doesn't pass unless tune up tolerance
+    assert torch.allclose(out_h.cpu(), out, 1e-07, 1e-04)
+    # doesn't pass unless tune up tolerance
 
-#@pytest.mark.skip(reason="not yet implemented")
+# @pytest.mark.skip(reason="not yet implemented")
 # directed test with given numbers
 def test_torch_addmm_5():
-    M = torch.tensor([[1.,1.,1.]])
+    M = torch.tensor([[1., 1., 1.]])
     M_h = M.hammerblade()
-    mat1 = torch.tensor([[1.,2.,3.,4.,5.],[6.,7.,8.,9.,10.],[11.,12.,13.,14.,15.]])
+    mat1 = torch.tensor([[1., 2., 3., 4., 5.],[6., 7., 8., 9., 10.],[11., 12., 13., 14., 15.]])
     mat1_h = mat1.hammerblade()
-    mat2 = torch.tensor([[16.,17.,18.],[19.,20.,21.],[22.,23.,24.],[25.,26.,27],[28.,29.,30.]])
+    mat2 = torch.tensor([[16., 17., 18.],[19., 20., 21.],[22., 23., 24.],[25., 26., 27],[28., 29., 30.]])
     mat2_h = mat2.hammerblade()
     out = torch.addmm(M, mat1, mat2)
     out_h = torch.addmm(M_h, mat1_h, mat2_h)
