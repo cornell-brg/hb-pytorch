94208088aed75587faad08d2418ceb669b6273f4
AngelaZou2000 <az292@cornell.edu>
2020-03-29 20:47:44 -0400

    hypothesis

--- hammerblade/torch/tests/test_relu.py
+++ hammerblade/torch/tests/test_relu.py
@@ -45,12 +45,18 @@ def test_torch_nn_relu_4():
     assert x_h_relu.device == torch.device("hammerblade")
     assert torch.equal(x_h_relu.cpu(), x_relu)
 
-# @given(inputs=hu.tensors(n=2))
-# def test_elementwise_torch_nn_relu_hypothesis(inputs):
-#     def elementwise_torch_nn_relu(inputs):
-#         assert len(inputs) == 2
-#         return inputs[0] - inputs[1]
-#     hu.assert_hb_checks(elementwise_torch_nn_relu, inputs)
+@given(tensors=hu.tensors(n=2), scalars=st.floats(n=2, min_value=-1024.0, max_value=1024.0, width=32))
+def test_elementwise_torch_nn_relu_hypothesis(tensors, scalars):
+    def elementwise_torch_nn_relu(tensors, scalars):
+        assert len(tensors) == 2
+        assert len(scalars) == 2
+        tensor_self, tensor_others = tensors
+        threshold, value = scalars
+        if (tensor_self <= threshold)
+        return value
+        else 
+        return others
+    hu.assert_hb_checks(elementwise_torch_nn_relu, [tensors, scalars])
 
 
 
