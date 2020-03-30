541517c617dd614df264133a85a1835833194637
AngelaZou2000 <az292@cornell.edu>
2020-03-30 13:57:49 -0400

    debug

--- hammerblade/torch/tests/test_relu.py
+++ hammerblade/torch/tests/test_relu.py
@@ -51,18 +51,17 @@ def test_torch_nn_relu_4():
     assert x_h_relu.device == torch.device("hammerblade")
     assert torch.equal(x_h_relu.cpu(), x_relu)
 
-@given(tensors=hu.tensors(n=2), threshold=st.floats(min_value=-1024.0, max_value=1024.0, width=32), value=st.floats(min_value=-1024.0, max_value=1024.0, width=32))
-# @given(tensors=hu.tensors(n=2), scalars=st.floats(n=2, width=32))
-def test_elementwise_torch_nn_relu_hypothesis(tensors, scalars):
-    def elementwise_torch_nn_relu(tensors, scalars):
-        assert len(tensors) == 2
-        assert len(scalars) == 2
-        tensor_self, tensor_others = tensors
-        if (tensor_self <= threshold):
+@given(tensors=hu.tensors(n=2), 
+    threshold=st.floats(width=32), 
+    value=st.floats(width=32))
+def test_elementwise_torch_nn_relu_hypothesis(tensors, threshold, value):
+    def elementwise_torch_nn_relu(inputs):
+        tensors, threshold, value = inputs
+        if (tensor[0] <= threshold):
             return value
         else:
-            return others
-    hu.assert_hb_checks(elementwise_torch_nn_relu, [tensors, scalars])
+            return tensor[1]
+    hu.assert_hb_checks(elementwise_torch_nn_relu, [tensors, threshold, value])
 
 
 
