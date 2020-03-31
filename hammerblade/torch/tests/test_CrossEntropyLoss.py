2a1704a0af41f9c87876e1997798db4e7b0513f3
lc873 <lc873@cornell.edu>
2020-03-31 12:51:37 -0400

    [test CrossEntropyLoss] refactor

--- hammerblade/torch/tests/test_CrossEntropyLoss.py
+++ hammerblade/torch/tests/test_CrossEntropyLoss.py
@@ -6,29 +6,26 @@ Tests on torch.nn.CrossEntropyLoss
 import torch
 import torch.nn as nn
 
-def test_torch_nn_CrossEntropyLoss_mean():
-    loss = nn.CrossEntropyLoss(reduction='mean')
-    input = torch.randn(3, 5, requires_grad=True)
-    target = torch.empty(3, dtype=torch.long).random_(5)
+def _test_torch_nn_CrossEntropyLoss(loss, input, target):
     output = loss(input, target)
     output_h = loss(input.hammerblade(), target.hammerblade())
     assert output_h.device == torch.device("hammerblade")
     assert torch.allclose(output, output_h.cpu())
+
+def test_torch_nn_CrossEntropyLoss_mean():
+    loss = nn.CrossEntropyLoss(reduction='mean')
+    input = torch.randn(3, 5, requires_grad=True)
+    target = torch.empty(3, dtype=torch.long).random_(5)
+    _test_torch_nn_CrossEntropyLoss(loss, input, target)
 
 def test_torch_nn_CrossEntropyLoss_none():
     loss = nn.CrossEntropyLoss(reduction='none')
     input = torch.randn(3, 5, requires_grad=True)
     target = torch.empty(3, dtype=torch.long).random_(5)
-    output = loss(input, target)
-    output_h = loss(input.hammerblade(), target.hammerblade())
-    assert output_h.device == torch.device("hammerblade")
-    assert torch.allclose(output, output_h.cpu())
+    _test_torch_nn_CrossEntropyLoss(loss, input, target)
 
 def test_torch_nn_CrossEntropyLoss_sum():
     loss = nn.CrossEntropyLoss(reduction='sum')
     input = torch.randn(3, 5, requires_grad=True)
     target = torch.empty(3, dtype=torch.long).random_(5)
-    output = loss(input, target)
-    output_h = loss(input.hammerblade(), target.hammerblade())
-    assert output_h.device == torch.device("hammerblade")
-    assert torch.allclose(output, output_h.cpu())
+    _test_torch_nn_CrossEntropyLoss(loss, input, target)
