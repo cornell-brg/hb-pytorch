2baebe9a225ef32ddd00ba16bb02f0677951d309
lc873 <lc873@cornell.edu>
2020-03-31 13:30:34 -0400

    [test CrossEntropyLoss] fix none backward

--- hammerblade/torch/tests/test_CrossEntropyLoss_backward.py
+++ hammerblade/torch/tests/test_CrossEntropyLoss_backward.py
@@ -39,4 +39,16 @@ def test_torch_nn_CrossEntropyLoss_none_back():
     loss = nn.CrossEntropyLoss(reduction='none')
     input = torch.randn(3, 5, requires_grad=True)
     target = torch.tensor([1, 0, 4])
-    _test_torch_nn_CrossEntropyLoss_back(loss, input, target)
+    grad = torch.tensor([1., 2., 3.])
+    input_h = hbutils.init_hb_tensor(input)
+    assert input_h is not input
+    output = loss(input, target)
+    output_h = loss(input_h, target.hammerblade())
+    assert output_h.device == torch.device("hammerblade")
+    assert torch.allclose(output, output_h.cpu())
+    output.backward(grad)
+    output_h.backward(grad.hammerblade())
+    assert input.grad is not None
+    assert input_h.grad is not None
+    assert input.grad is not input_h.grad
+    assert torch.allclose(input.grad, input_h.grad.cpu())
