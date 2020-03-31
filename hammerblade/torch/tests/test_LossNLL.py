2ae34e18c5d7eca7eea190f632fcd65104736129
lc873 <lc873@cornell.edu>
2020-03-31 14:12:09 -0400

    [test LossNLL] refactor

--- hammerblade/torch/tests/test_LossNLL.py
+++ hammerblade/torch/tests/test_LossNLL.py
@@ -6,33 +6,38 @@ Tests on torch.nn.NLLLoss
 import torch
 import torch.nn as nn
 
+torch.manual_seed(42)
+
+def _test_torch_nn_NLLLoss(loss, m, input, target):
+    output = loss(m(input), target)
+    output_h = loss(m(input.hammerblade()), target.hammerblade())
+    assert output_h.device == torch.device("hammerblade")
+    assert torch.allclose(output, output_h.cpu())
+
 def test_torch_nn_NLLLoss_mean():
     m = nn.LogSoftmax(dim=1)
     loss = nn.NLLLoss(reduction='mean')
     input = torch.randn(3, 5, requires_grad=True)
     target = torch.tensor([1, 0, 4])
-    output = loss(m(input), target)
-    output_h = loss(m(input.hammerblade()), target.hammerblade())
-    assert output_h.device == torch.device("hammerblade")
-    assert torch.allclose(output, output_h.cpu())
+    _test_torch_nn_NLLLoss(loss, m, input, target)
 
 def test_torch_nn_NLLLoss_none():
     m = nn.LogSoftmax(dim=1)
     loss = nn.NLLLoss(reduction='none')
     input = torch.randn(3, 5, requires_grad=True)
     target = torch.tensor([1, 0, 4])
-    output = loss(m(input), target)
-    output_h = loss(m(input.hammerblade()), target.hammerblade())
-    assert output_h.device == torch.device("hammerblade")
-    assert torch.allclose(output, output_h.cpu())
+    _test_torch_nn_NLLLoss(loss, m, input, target)
 
 def test_torch_nn_NLLLoss_sum():
     m = nn.LogSoftmax(dim=1)
     loss = nn.NLLLoss(reduction='sum')
     input = torch.randn(3, 5, requires_grad=True)
     target = torch.tensor([1, 0, 4])
+    _test_torch_nn_NLLLoss(loss, m, input, target)
+
+def _test_torch_nn_NLLLoss_weight(loss, loss_h, m, input, target):
     output = loss(m(input), target)
-    output_h = loss(m(input.hammerblade()), target.hammerblade())
+    output_h = loss_h(m(input.hammerblade()), target.hammerblade())
     assert output_h.device == torch.device("hammerblade")
     assert torch.allclose(output, output_h.cpu())
 
@@ -42,10 +47,7 @@ def test_torch_nn_NLLLoss_mean_weight():
     loss_h = nn.NLLLoss(reduction='mean', weight=torch.tensor([1., 2., 3., 4., 5.]).hammerblade())
     input = torch.randn(3, 5, requires_grad=True)
     target = torch.tensor([1, 0, 4])
-    output = loss(m(input), target)
-    output_h = loss_h(m(input.hammerblade()), target.hammerblade())
-    assert output_h.device == torch.device("hammerblade")
-    assert torch.allclose(output, output_h.cpu())
+    _test_torch_nn_NLLLoss_weight(loss, loss_h, m, input, target)
 
 def test_torch_nn_NLLLoss_none_weight():
     m = nn.LogSoftmax(dim=1)
@@ -53,10 +55,7 @@ def test_torch_nn_NLLLoss_none_weight():
     loss_h = nn.NLLLoss(reduction='none', weight=torch.tensor([1., 2., 3., 4., 5.]).hammerblade())
     input = torch.randn(3, 5, requires_grad=True)
     target = torch.tensor([1, 0, 4])
-    output = loss(m(input), target)
-    output_h = loss_h(m(input.hammerblade()), target.hammerblade())
-    assert output_h.device == torch.device("hammerblade")
-    assert torch.allclose(output, output_h.cpu())
+    _test_torch_nn_NLLLoss_weight(loss, loss_h, m, input, target)
 
 def test_torch_nn_NLLLoss_sum_weight():
     m = nn.LogSoftmax(dim=1)
@@ -64,7 +63,4 @@ def test_torch_nn_NLLLoss_sum_weight():
     loss_h = nn.NLLLoss(reduction='sum', weight=torch.tensor([1., 2., 3., 4., 5.]).hammerblade())
     input = torch.randn(3, 5, requires_grad=True)
     target = torch.tensor([1, 0, 4])
-    output = loss(m(input), target)
-    output_h = loss_h(m(input.hammerblade()), target.hammerblade())
-    assert output_h.device == torch.device("hammerblade")
-    assert torch.allclose(output, output_h.cpu())
+    _test_torch_nn_NLLLoss_weight(loss, loss_h, m, input, target)
