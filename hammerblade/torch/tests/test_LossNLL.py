f2963c226d6dc04ab5b532f11fb4a716dec206f1
lc873 <lc873@cornell.edu>
2020-04-01 10:43:43 -0400

    [test LossNLL] refactor fwd & back

--- hammerblade/torch/tests/test_LossNLL.py
+++ hammerblade/torch/tests/test_LossNLL.py
@@ -8,59 +8,55 @@ import torch.nn as nn
 
 torch.manual_seed(42)
 
-def _test_torch_nn_NLLLoss(loss, m, input, target):
+def _test_torch_nn_NLLLoss(loss, input, target):
+    m = nn.LogSoftmax(dim=1)
     output = loss(m(input), target)
     output_h = loss(m(input.hammerblade()), target.hammerblade())
     assert output_h.device == torch.device("hammerblade")
     assert torch.allclose(output, output_h.cpu())
 
 def test_torch_nn_NLLLoss_mean():
-    m = nn.LogSoftmax(dim=1)
     loss = nn.NLLLoss(reduction='mean')
     input = torch.randn(3, 5, requires_grad=True)
     target = torch.tensor([1, 0, 4])
-    _test_torch_nn_NLLLoss(loss, m, input, target)
+    _test_torch_nn_NLLLoss(loss, input, target)
 
 def test_torch_nn_NLLLoss_none():
-    m = nn.LogSoftmax(dim=1)
     loss = nn.NLLLoss(reduction='none')
     input = torch.randn(3, 5, requires_grad=True)
     target = torch.tensor([1, 0, 4])
-    _test_torch_nn_NLLLoss(loss, m, input, target)
+    _test_torch_nn_NLLLoss(loss, input, target)
 
 def test_torch_nn_NLLLoss_sum():
-    m = nn.LogSoftmax(dim=1)
     loss = nn.NLLLoss(reduction='sum')
     input = torch.randn(3, 5, requires_grad=True)
     target = torch.tensor([1, 0, 4])
-    _test_torch_nn_NLLLoss(loss, m, input, target)
+    _test_torch_nn_NLLLoss(loss, input, target)
 
-def _test_torch_nn_NLLLoss_weight(loss, loss_h, m, input, target):
+def _test_torch_nn_NLLLoss_weight(loss, loss_h, input, target):
+    m = nn.LogSoftmax(dim=1)
     output = loss(m(input), target)
     output_h = loss_h(m(input.hammerblade()), target.hammerblade())
     assert output_h.device == torch.device("hammerblade")
     assert torch.allclose(output, output_h.cpu())
 
 def test_torch_nn_NLLLoss_mean_weight():
-    m = nn.LogSoftmax(dim=1)
     loss = nn.NLLLoss(reduction='mean', weight=torch.tensor([1., 2., 3., 4., 5.]))
     loss_h = nn.NLLLoss(reduction='mean', weight=torch.tensor([1., 2., 3., 4., 5.]).hammerblade())
     input = torch.randn(3, 5, requires_grad=True)
     target = torch.tensor([1, 0, 4])
-    _test_torch_nn_NLLLoss_weight(loss, loss_h, m, input, target)
+    _test_torch_nn_NLLLoss_weight(loss, loss_h, input, target)
 
 def test_torch_nn_NLLLoss_none_weight():
-    m = nn.LogSoftmax(dim=1)
     loss = nn.NLLLoss(reduction='none', weight=torch.tensor([1., 2., 3., 4., 5.]))
     loss_h = nn.NLLLoss(reduction='none', weight=torch.tensor([1., 2., 3., 4., 5.]).hammerblade())
     input = torch.randn(3, 5, requires_grad=True)
     target = torch.tensor([1, 0, 4])
-    _test_torch_nn_NLLLoss_weight(loss, loss_h, m, input, target)
+    _test_torch_nn_NLLLoss_weight(loss, loss_h, input, target)
 
 def test_torch_nn_NLLLoss_sum_weight():
-    m = nn.LogSoftmax(dim=1)
     loss = nn.NLLLoss(reduction='sum', weight=torch.tensor([1., 2., 3., 4., 5.]))
     loss_h = nn.NLLLoss(reduction='sum', weight=torch.tensor([1., 2., 3., 4., 5.]).hammerblade())
     input = torch.randn(3, 5, requires_grad=True)
     target = torch.tensor([1, 0, 4])
-    _test_torch_nn_NLLLoss_weight(loss, loss_h, m, input, target)
+    _test_torch_nn_NLLLoss_weight(loss, loss_h, input, target)
