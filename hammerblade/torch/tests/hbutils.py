50cd11692c3b7d7f13e9ab359f5f9a80e11fdace
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-04-02 20:05:17 -0700

    CheckLayer backward

--- hammerblade/torch/tests/hbutils.py
+++ hammerblade/torch/tests/hbutils.py
@@ -8,36 +8,44 @@ def init_hb_tensor(input_t):
     return input_t.hammerblade().clone().detach().requires_grad_(
         input_t.requires_grad)
 
-class CheckLayerFunction(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, x, cpu_fwd):
-        if x.is_hammerblade:
-            print("Running HB")
-            assert cpu_fwd is not None, \
-                    "Forward must be called on CPU model first"
-            assert torch.allclose(cpu_fwd, x.cpu(), atol=1e-7) is True, \
-                    "hbutils.CheckLayer failed:\n" + \
-                    "CPU output:\n" + str(cpu_fwd) + "\n" \
-                    "HB output:\n" + str(x) + "\n"
-        else:
-            print("Running CPU")
-            cpu_fwd = x.clone()
-
-        return x, cpu_fwd
-
-    @staticmethod
-    def backward(ctx, x, cpu_fwd):
-        return x, cpu_fwd
-
 class CheckLayer(nn.Module):
-    cpu_fwd = None
-    cpu_bkwd = None
-
     def __init__(self):
         super(CheckLayer, self).__init__()
 
     def forward(self, x):
-        x, CheckLayer.cpu_fwd = CheckLayerFunction.apply(x, CheckLayer.cpu_fwd)
+        return CheckLayerFunction.apply(x)
+
+class CheckLayerFunction(torch.autograd.Function):
+    cpu_fwd = None
+    cpu_bkwd = None
+
+    @staticmethod
+    def forward(ctx, x):
         if x.is_hammerblade:
-            CheckLayer.cpu_fwd = None
+            assert CheckLayerFunction.cpu_fwd is not None, \
+                    "Forward must be called on CPU model first"
+            assert torch.allclose(CheckLayerFunction.cpu_fwd, x.cpu(), atol=1e-7) is True, \
+                    "hbutils.CheckLayer failed:\n" + \
+                    "CPU output:\n" + str(CheckLayerFunction.cpu_fwd) + "\n" \
+                    "HB output:\n" + str(x) + "\n"
+            CheckLayerFunction.cpu_fwd = None
+        else:
+            CheckLayerFunction.cpu_fwd = x.clone()
+
         return x
+
+    @staticmethod
+    def backward(ctx, x):
+        if x.is_hammerblade:
+            assert CheckLayerFunction.cpu_bkwd is not None, \
+                    "Forward must be called on CPU model first"
+            assert torch.allclose(CheckLayerFunction.cpu_bkwd, x.cpu(), atol=1e-7) is True, \
+                    "hbutils.CheckLayer failed:\n" + \
+                    "CPU output:\n" + str(CheckLayerFunction.cpu_bkwd) + "\n" \
+                    "HB output:\n" + str(x) + "\n"
+            CheckLayerFunction.cpu_bkwd = None
+        else:
+            CheckLayerFunction.cpu_bkwd = x.clone()
+
+        return x
+
