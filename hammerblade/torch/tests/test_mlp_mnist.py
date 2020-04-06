ba7b2f62d9b26033c35f5bd2638a8046a088a2c2
lc873 <lc873@cornell.edu>
2020-04-06 11:47:33 -0400

    [python style] more fixes

--- hammerblade/torch/tests/test_mlp_mnist.py
+++ hammerblade/torch/tests/test_mlp_mnist.py
@@ -20,21 +20,18 @@ torch.manual_seed(42)
 class MLPModel(nn.Module):
 
     def __init__(self):
-      super(MLPModel, self).__init__()
+        super(MLPModel, self).__init__()
 
-      self.mnist = nn.Sequential \
-      (
-        nn.Linear(784, 128),
-        nn.ReLU(),
-        nn.Dropout(0.0),
-        nn.Linear(128, 64),
-        nn.ReLU(),
-        nn.Dropout(0.0),
-        nn.Linear(64, 10),
-      )
+        self.mnist = nn.Sequential(nn.Linear(784, 128),
+                                   nn.ReLU(),
+                                   nn.Dropout(0.0),
+                                   nn.Linear(128, 64),
+                                   nn.ReLU(),
+                                   nn.Dropout(0.0),
+                                   nn.Linear(64, 10))
 
     def forward(self, x):
-      return self.mnist(x)
+        return self.mnist(x)
 
 # -------------------------------------------------------------------------
 # Forward pass
