5ca82eaece6bc3952e0abe3e4850424b283dd344
jlw422 <jlw422@cornell.edu>
2020-04-09 15:33:35 -0400

    fixed tabs

--- hammerblade/torch/tests/test_sigmoid.py
+++ hammerblade/torch/tests/test_sigmoid.py
@@ -9,31 +9,31 @@ from hypothesis import given, settings
 from .hypothesis_test_util import HypothesisUtil as hu
 
 def test_torch_nn_sigmoid_1():
-    sigmoid = nn.Sigmoid()
-    x = torch.ones(10)
-    x_h = x.hammerblade()
-    x_sig = sigmoid(x)
-    x_h_sig = sigmoid(x_h)
-    assert x_h_sig.device == torch.device("hammerblade")
-    assert torch.allclose(x_h_sig.cpu(), x_sig)
+	sigmoid = nn.Sigmoid()
+	x = torch.ones(10)
+	x_h = x.hammerblade()
+	x_sig = sigmoid(x)
+	x_h_sig = sigmoid(x_h)
+	assert x_h_sig.device == torch.device("hammerblade")
+	assert torch.allclose(x_h_sig.cpu(), x_sig)
 
 def test_torch_nn_sigmoid_2():
-    sigmoid = nn.Sigmoid()
-    x = torch.randn(10)
-    x_h = x.hammerblade()
-    x_sig = sigmoid(x)
-    x_h_sig = sigmoid(x_h)
-    assert x_h_sig.device == torch.device("hammerblade")
-    assert torch.allclose(x_h_sig.cpu(), x_sig)
+	sigmoid = nn.Sigmoid()
+	x = torch.randn(10)
+	x_h = x.hammerblade()
+	x_sig = sigmoid(x)
+	x_h_sig = sigmoid(x_h)
+	assert x_h_sig.device == torch.device("hammerblade")
+	assert torch.allclose(x_h_sig.cpu(), x_sig)
 
 def test_torch_nn_sigmoid_3():
-    sigmoid = nn.Sigmoid()
-    x = torch.randn(3, 4)
-    x_h = x.hammerblade()
-    x_sig = sigmoid(x)
-    x_h_sig = sigmoid(x_h)
-    assert x_h_sig.device == torch.device("hammerblade")
-    assert torch.allclose(x_h_sig.cpu(), x_sig)
+	sigmoid = nn.Sigmoid()
+	x = torch.randn(3, 4)
+	x_h = x.hammerblade()
+	x_sig = sigmoid(x)
+	x_h_sig = sigmoid(x_h)
+	assert x_h_sig.device == torch.device("hammerblade")
+	assert torch.allclose(x_h_sig.cpu(), x_sig)
 
 def _test_torch_sigmoid_check(tensor_self):
 	tensor_self_hb = torch.tensor(tensor_self).hammerblade()
