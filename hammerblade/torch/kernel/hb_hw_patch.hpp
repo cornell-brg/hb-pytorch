42ae86390522481680dd84f74e303e7e931f5c93
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-04-11 19:39:03 -0700

    Header for HB HW patches

---
+++ hammerblade/torch/kernel/hb_hw_patch.hpp
@@ -0,0 +1,17 @@
+#ifndef _HB_HW_PATCH_HPP
+#define _HB_HW_PATCH_HPP
+
+#ifndef HB_EMUL
+#define HB_RAW_HAZARD(var) \
+  do {                     \
+    asm volatile (         \
+        "mv %0, %1;"       \
+        : "=r" ((var))     \
+        : "r" ((var))      \
+        );                 \
+  } while(0)
+#else
+#define HB_RAW_HAZARD(var)
+#endif // ifndef HB_EMUL
+
+#endif // _HB_HW_PATCH_HPP
