cbfa1fcf796f272ddfd1e889f42a800b6d6fbcd2
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-04-11 21:54:52 -0700

    Added comment describing error message

--- hammerblade/torch/kernel/hb_hw_patch.hpp
+++ hammerblade/torch/kernel/hb_hw_patch.hpp
@@ -2,6 +2,11 @@
 #define _HB_HW_PATCH_HPP
 
 #ifndef HB_EMUL
+
+// Fixes WAW violations in HW
+//
+// WAW violations are seen in cosimulation as errors starting with:
+// [ERROR][VCORE] STALL_FORCE_WB WAW HAZARD
 #define HB_FIX_WAW_HAZARD(var) \
   do {                         \
     asm volatile (             \
@@ -10,8 +15,11 @@
         : "r" ((var))          \
         );                     \
   } while(0)
+
 #else
+
 #define HB_FIX_WAW_HAZARD(var)
+
 #endif // ifndef HB_EMUL
 
 #endif // _HB_HW_PATCH_HPP
