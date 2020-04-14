1769fe9d1d7c0d938a6af8f82e4c7a73467c895b
Michelle Chao <mc2244@cornell.edu>
2020-04-13 23:46:34 -0400

    add floor kernel

---
+++ hammerblade/torch/tests/test_floor.py
@@ -0,0 +1,33 @@
+"""
+Tests on torch.floor (floor kernel)
+04/10/2020 Michelle Chao (mc2244@cornell.edu)
+"""
+import torch
+
+torch.manual_seed(42)
+
+def _test_torch_floor_check(x):
+    h = x.hammerblade()
+    floor_x = x.floor()
+    floor_h = h.floor()
+    assert floor_h.device == torch.device("hammerblade")
+    assert torch.allclose(floor_h.cpu(), floor_x)
+
+def test_torch_floor_1():
+    x = torch.ones(10)
+    _test_torch_floor_check(x)
+
+def test_torch_floor_2():
+    x = torch.randn(16, 16)
+    _test_torch_floor_check(x)
+
+def test_torch_floor_3():
+    x = torch.randn(1)
+    _test_torch_floor_check(x)
+
+def test_torch_floor_4():
+    x = torch.randn(10)
+    _test_torch_floor_check(x)
+
+
+
