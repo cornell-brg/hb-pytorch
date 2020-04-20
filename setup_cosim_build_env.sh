8ae851db9cf4634036b131d86076c618c3302a8c
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-04-19 18:47:06 -0700

    Updated cosim cmd line interface

--- setup_cosim_build_env.sh
+++ setup_cosim_build_env.sh
@@ -44,6 +44,10 @@ else
 fi
 
 # Build COSIM runtime library and simulation executable
+make -C $BRG_BSG_BLADERUNNER_DIR/bsg_replicant/testbenches/pytorch test_loader
+
+# For backward compatibility.
+# Remove this with bsg_bladerunner's next version. Current is v4.0.0.
 make -C $BRG_BSG_BLADERUNNER_DIR/bsg_replicant/testbenches/python test_loader
 
 export HB_KERNEL_DIR=$DIR/hammerblade/torch
