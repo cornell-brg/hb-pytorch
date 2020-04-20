1010496792795529f98ba654b4e716982e9574d7
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-04-19 18:47:06 -0700

    Updated cosim cmd line interface

--- hammerblade/torch/python_cosim.sh
+++ hammerblade/torch/python_cosim.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash -x
 
 TOPLEVEL=$(git rev-parse --show-toplevel)
-COSIM_PYTHON_DIR=$BRG_BSG_BLADERUNNER_DIR/bsg_replicant/testbenches/python
+COSIM_PYTHON_DIR=$BRG_BSG_BLADERUNNER_DIR/bsg_replicant/testbenches/pytorch
 COSIM_PYTHON_EXE=$COSIM_PYTHON_DIR/test_loader
 
 if [[ ! -f $COSIM_PYTHON_EXE ]]
@@ -14,10 +14,8 @@ fi
 # Absolute path to first command line argument is the script
 # to execute
 SCRIPT_PATH=$(realpath $1)
-SCRIPT_DIR=$(dirname $SCRIPT_PATH)
-SCRIPT=`echo "$(basename $SCRIPT_PATH)" | cut -d'.' -f1`
 
-COSIM_CMD=($COSIM_PYTHON_EXE +ntb_random_seed_automatic +c_args=\"$SCRIPT_DIR/\ $SCRIPT\")
+COSIM_CMD=($COSIM_PYTHON_EXE +ntb_random_seed_automatic +c_args=\"$SCRIPT_PATH\")
 COSIM_CMD=("${COSIM_CMD[@]}" "|" grep -v \": instantiating\")
 
 # COSIM_PYTHON_EXE is the VCS executable in bladerunner.
