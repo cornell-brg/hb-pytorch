d447df915c535d058e2bd0496783c239f9ceab35
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-04-25 22:37:58 -0700

    undef temporary macro

--- c10/hammerblade/HammerBladeDevice.h
+++ c10/hammerblade/HammerBladeDevice.h
@@ -11,8 +11,6 @@
 
 #include <bsg_manycore_cuda.h>
 
-#define PATH(x) pstr(x)
-#define pstr(x) #x
 
 namespace c10 {
 namespace hammerblade {
@@ -21,6 +19,10 @@ extern hb_mc_dimension_t _hb_tg_dim;
 extern hb_mc_dimension_t _hb_grid_dim;
 extern hb_mc_device_t _hb_device;
 
+#define PATH(x) pstr(x)
+#define pstr(x) #x
 static char _bin_path[] = PATH(HB_KERNEL_PATH); // path to device kernel binary
+#undef pstr
+#undef PATH
 
 }} // namespace c10::hammerblade
