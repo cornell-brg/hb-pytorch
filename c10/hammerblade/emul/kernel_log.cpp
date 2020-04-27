d132c75ed7a894403465a097f18c406acf1b4d38
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-04-26 19:58:44 -0700

    Moved header to new include directory

--- c10/hammerblade/emul/kernel_log.cpp
+++ c10/hammerblade/emul/kernel_log.cpp
@@ -1,13 +1,7 @@
 #include <kernel_common.hpp>
 #include <kernel_log.h>
 #include <iostream>
-
-// A popular C++ library for json pasrsing and
-// serialization. Inlcuded as a header only library.
-//
-// Source: https://github.com/nlohmann/json
-// Release: https://github.com/nlohmann/json/releases/tag/v3.7.3
-#include <json.hpp>
+#include <nlohmann/json.hpp>
 
 using json = nlohmann::json;
 
