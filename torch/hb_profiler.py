8a927824b67c9c13da627944f52b72f61ca3e3b6
lc873 <lc873@cornell.edu>
2020-05-07 15:06:10 -0400

    [ExecutionTime] create a tree, which is more structed

--- torch/hb_profiler.py
+++ torch/hb_profiler.py
@@ -55,6 +55,105 @@ class ProfilerTopLvlFuncRecord(ProfilerRecord):
         func = "aten::" + func
         self.func = func
 
+# --------- ExecutionTime Stack Tree -------
+
+class exec_time_Node:
+
+    def __init__(self, func, time, fancy_func=False):
+        self.func = func
+        self.time = float(time)
+        self.children = []
+        self.percentage = -1
+        if fancy_func:
+            func = self.func
+            func = func.split("(")[0]
+            func = func.split("::")[-1]
+            func = "aten::" + func
+            self.func = func
+
+    def add_child(self, child):
+        assert isinstance(child, exec_time_Node)
+        self.children.append(child)
+
+    def __str__(self):
+        return "Node(" + self.func + " : " + str(self.time) + ")"
+
+
+def exec_time_preprocess(data):
+    processed = []
+    data = data.splitlines()
+    roi = ()
+    for d in data:
+        stack, time = d.split(";")
+        if stack == "time_in_roi":
+            roi = (stack, time)
+        else:
+            stack = stack.split("<|>")
+            processed.append((stack, time))
+    return processed, roi
+
+# recursively construct a stack tree
+# the idea is to find len(1) stacks, and construct a node
+# anything between two len(1) stacks, or the end, should be
+# recursively handled and added as len(1) stack's children
+def exec_time_construct_tree_impl(data, parent, fancy_func=False):
+    global_idx = 0
+    while global_idx < len(data):
+        stack, time = data[global_idx]
+        assert len(stack) == 1
+        node = exec_time_Node(stack[0], time, fancy_func)
+        parent.add_child(node)
+        lower_level = []
+        global_idx += 1
+        while global_idx < len(data):
+            stack, time = data[global_idx]
+            if len(stack) == 1:
+                break
+            # stack pop front
+            lower_level.append((stack[1:], time))
+            global_idx += 1
+        exec_time_construct_tree_impl(lower_level, node)
+
+# find other time in ROI
+def exec_time_add_other(root):
+    agg_total = 0.0
+    for kid in root.children:
+        agg_total += kid.time
+    root.children.append(exec_time_Node("other", root.time - agg_total))
+
+# append percentage of ROI to each node
+def exec_time_calc_percentage(root, roi_time=None):
+    if roi_time is None:
+        roi_time = root.time
+    root.percentage = root.time / roi_time * 100.0
+    for kid in root.children:
+        exec_time_calc_percentage(kid, roi_time)
+
+# preorder traversal
+def exec_time_print_tree(root, lvl=0, output=None):
+    if output is None:
+        output = []
+    fancy_str = ""
+    for i in range(lvl):
+        fancy_str += "  "
+    fancy_str += "|- "
+    fancy_str += str(root)
+    output.append(fancy_str)
+    for kid in root.children:
+        exec_time_print_tree(kid, lvl + 1, output)
+    return "\n".join(output)
+
+# wrap everything, return a tree
+def exec_time_tree(fancy_func=False):
+    data = torch._C._hb_profiler_exec_time_raw_stack()
+    data, roi = exec_time_preprocess(data)
+    root = exec_time_Node(roi[0], roi[1])
+    exec_time_construct_tree_impl(data, root, fancy_func)
+    exec_time_add_other(root)
+    exec_time_calc_percentage(root)
+    return root
+
+# --------- torch.hb_profiler APIs ---------
 
 def enable():
     profiler_status.is_in_ROI = True
@@ -99,32 +198,13 @@ def unimpl_print():
     except AttributeError:
         print("PyTorch is not built with profiling")
 
-def _process_raw_data(raw_data=None):
-    if raw_data is None:
-        raw_data = torch._C._hb_profiler_exec_time_fancy_table()
-    raw_entries = raw_data.splitlines()
-    time_in_roi = ProfilerROIRecord(raw_entries[0])
-    agg_total = ProfilerAggTotalRecord(raw_entries[-1])
-    other = ProfilerOtherRecord(time_in_roi.time_ms, agg_total.time_ms)
-    entries = []
-
-    for e in raw_entries[1:-1]:
-        entry = ProfilerTopLvlFuncRecord(e)
-        entry.calc_percentage(time_in_roi.time_ms)
-        entries.append(entry)
-    entries.sort(key=lambda e: e.time_ms, reverse=True)
-    entries += [other, time_in_roi]
-
-    return entries
-
 def exec_time_fancy_print(raw_data=None):
     try:
-        entries = _process_raw_data(raw_data)
-
+        root = exec_time_tree(fancy_func=True)
         buffer = ""
-        for e in entries:
+        for e in (root.children + [root]):
             func = e.func
-            time = e.time_ms / 1000.0
+            time = e.time / 1000.0
             percentage = e.percentage
             buffer += ('{func:30}     {time:.2f} {percentage:.1f}%\n'.format(
                 func=func, time=time, percentage=percentage))
@@ -134,8 +214,7 @@ def exec_time_fancy_print(raw_data=None):
 
 def exec_time_latex_table(raw_data=None):
     try:
-        entries = _process_raw_data(raw_data)
-
+        root = exec_time_tree(fancy_func=True)
         buffer = ""
         header = "\\begin{table}[t]\n" \
                  "\\begin{tabular}{lrr}\n" \
@@ -144,10 +223,10 @@ def exec_time_latex_table(raw_data=None):
                  "\\textbf{Kernel} & \\textbf{(s)} & \\textbf{of Total} \\\\ \\midrule\n"
         buffer += header
 
-        for e in entries:
+        for e in (root.children + [root]):
             func = e.func
             func = func.replace("_", "\\_")
-            time = e.time_ms / 1000.0
+            time = e.time / 1000.0
             percentage = e.percentage
             buffer += ('\\textbf{{{func:30}}} &  {time:.2f} & {percentage:.1f}\\% \\\\'.format(
                 func=func, time=time, percentage=percentage))
@@ -163,7 +242,8 @@ def exec_time_latex_table(raw_data=None):
 
 def exec_time_raw_stack():
     try:
-        return torch._C._hb_profiler_exec_time_raw_stack()
+        root = exec_time_tree()
+        return exec_time_print_tree(root)
     except AttributeError:
         print("PyTorch is not built with profiling")
 
