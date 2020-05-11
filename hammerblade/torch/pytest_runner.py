93560d1af5f7e81e37f5ec33a82287291ff78846
lc873 <lc873@cornell.edu>
2020-05-11 11:01:20 -0400

    [pytest_runner] -vs -> -v

--- hammerblade/torch/pytest_runner.py
+++ hammerblade/torch/pytest_runner.py
@@ -6,7 +6,7 @@ Lin Cheng
 """
 
 # pytest commandline options
-pytest_argv = ["-vs"]
+pytest_argv = ["-v"]
 
 # This is a work around of the bug in which sys.argv is not set
 # when running as embededd script
