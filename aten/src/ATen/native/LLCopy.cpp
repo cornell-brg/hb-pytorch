270c3f1bda31270742150a36220d8ff2404c8468
lc873 <lc873@cornell.edu>
2020-05-13 19:52:59 -0400

    [llcopy] fix undefined ref to llcopy when not building for HB

--- aten/src/ATen/native/LLCopy.cpp
+++ aten/src/ATen/native/LLCopy.cpp
@@ -1,13 +1,16 @@
 #include <ATen/ATen.h>
 #include <ATen/Dispatch.h>
 #include <ATen/native/Resize.h>
+#ifdef USE_HB
 #include <ATen/hammerblade/HammerBladeContext.h>
+#endif
 
 namespace at {
 namespace native {
 
 Tensor llcopy_to_hb(const Tensor& self) {
 
+#ifdef USE_HB
   // get low level storage size
   size_t itemsize = self.storage().itemsize();
   int64_t numel = self.storage().numel();
@@ -33,6 +36,12 @@ Tensor llcopy_to_hb(const Tensor& self) {
   c10::hammerblade::DMA_host_to_device(hb_ptr, ptr, storage_size);
 
   return tensor;
+
+#else
+
+  TORCH_CHECK(false, "cannot call llcopy_to_hb if not using HB");
+
+#endif
 
 }
 
