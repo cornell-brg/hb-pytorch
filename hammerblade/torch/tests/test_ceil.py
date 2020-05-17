d2cba727791dfad0f6f3e608a1c348b13bd44dc6
Kofi Amoako Efah <kae87@gorgonzola.cs.cornell.edu>
2020-05-17 17:23:22 -0400

    Ceil Kernel

---
+++ hammerblade/torch/tests/test_ceil.py
@@ -0,0 +1,30 @@
+"""
+Tests on ceil kernel
+05/17/2020 Kofi Efah (kae87)
+"""
+import torch
+
+torch.manual_seed(42)
+
+def _test_torch_ceil_check(x):
+    h = x.hammerblade()
+    ceil_x = x.ceil()
+    ceil_h = h.ceil()
+    assert ceil_h.device == torch.device("hammerblade")
+    assert torch.allclose(ceil_h.cpu(), ceil_x)
+
+def test_torch_ceil_1():
+    x = torch.ones(10)
+    _test_torch_ceil_check(x)
+
+def test_torch_ceil_2():
+    x = torch.randn(10, 10)
+    _test_torch_ceil_check(x)
+
+def test_torch_ceil_3():
+    x = torch.randn(1)
+    _test_torch_ceil_check(x)
+
+def test_torch_ceil_4():
+    x = torch.zeros(10)
+    _test_torch_ceil_check(x)
