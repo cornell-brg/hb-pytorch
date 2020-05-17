dce322b7541d13ca31d26a9a6ae456b6e5a51a7e
Kofi Amoako Efah <kae87@gorgonzola.cs.cornell.edu>
2020-05-17 15:35:42 -0400

    Implemented Ceil kernel

--- hammerblade/torch/kernel/kernel_ceil.cpp
+++ hammerblade/torch/kernel/kernel_ceil.cpp
@@ -1,27 +1,35 @@
 //====================================================================
 // Ceil kernel
-// 04/11/2020 Kofi Amoako Efah (kae87@cornell.edu)
+// 5/17/2020 Kofi Amoako Efah (kae87@cornell.edu)
 //====================================================================
+
 
 #include <kernel_common.hpp>
 #include <cmath>
 
+// We wrap all external-facing C++ kernels with `extern "C"` to
+// prevent name mangling
+
 extern "C" {
 
   __attribute__ ((noinline))  int tensorlib_ceil(
-          bsg_tensor_t* t0_p,
-          bsg_tensor_t* t1_p) {
+          hb_tensor_t* t0_p,
+          hb_tensor_t* t1_p) {
+    auto res = HBTensor<float>(t0_p);
+    auto input = HBTensor<float>(t1_p);     
     // Start profiling
     bsg_cuda_print_stat_kernel_start();
-    brg_tile_elementwise_for(t0_p, t1_p,
-      [&](float a) { //anonymous function for ceiling. Does the ceiling operation for every element in the tile.
-        return ceil(a); // returns ceiling of argument.
+    hb_parallel_foreach(res, input,
+      [&](float a) {
+        return ceil(a);
     });
+    //   End profiling
     bsg_cuda_print_stat_kernel_end();
     return 0;
   }
 
-  HB_EMUL_REG_KERNEL(tensorlib_ceil, bsg_tensor_t*, bsg_tensor_t*)
+  HB_EMUL_REG_KERNEL(tensorlib_ceil, hb_tensor_t*, hb_tensor_t*)
 
 }
 
+ 
--- hammerblade/torch/tests/test_ceil.py
+++ hammerblade/torch/tests/test_ceil.py
@@ -1,30 +1,26 @@
 """
 Unit Test for kernel_ceil
-Written by Kofi Efah(kae87) 4/13/2020
+Written by Kofi Efah(kae87) 5/17/2020
 """
 import torch
-import random
 
 torch.manual_seed(42)
-random.seed(42)
 
-def _ceil_test_helper(x):
+def _test_torch_ceil_check(x):
     h = x.hammerblade()
     ceil_x = x.ceil()
     ceil_h = h.ceil()
     assert ceil_h.device == torch.device("hammerblade")
-    assert torch.equal(ceil_h.cpu(),ceil_x)
+    assert torch.allclose(ceil_h.cpu(), ceil_x)
 
 def test_torch_ceil_1():
-    x = torch.ones(100)
-    _ceil_test_helper(x)
+    x = torch.ones(10)
+    _test_torch_ceil_check(x)
 
 def test_torch_ceil_2():
-    x = torch.randn(2, 9)
-    _ceil_test_helper(x)
+    x = torch.randn(10, 10)
+    _test_torch_ceil_check(x)
 
 def test_torch_ceil_3():
-    x = torch.zeros(100)
-    _ceil_test_helper(x)
-
-
+    x = torch.zeros(10)
+    _test_torch_ceil_check(x)
