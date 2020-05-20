3e8b424e53e60aecf7e027c935e0f84832a8c69a
Kofi Amoako Efah <kae87@gorgonzola.cs.cornell.edu>
2020-05-19 20:12:59 -0400

    Ceil Kernel

---
+++ hammerblade/torch/kernel/kernel_ceil.cpp
@@ -0,0 +1,29 @@
+//====================================================================
+// Ceil kernel
+// 04/13/2020 Yuyi He (yh383@cornell.edu)
+//====================================================================
+
+#include <kernel_common.hpp>
+#include <cmath>
+
+extern "C" {
+
+  __attribute__ ((noinline))  int tensorlib_ceil(
+          hb_tensor_t* t0_p,
+          hb_tensor_t* t1_p) {
+    auto res = HBTensor<float>(t0_p);
+    auto input = HBTensor<float>(t1_p);
+    // Start profiling
+    bsg_cuda_print_stat_kernel_start();
+    hb_parallel_foreach(res, input,
+      [&](float a) {
+        return ceil(a);
+    });
+    //   End profiling
+    bsg_cuda_print_stat_kernel_end();
+    return 0;
+  }
+
+  HB_EMUL_REG_KERNEL(tensorlib_ceil, hb_tensor_t*, hb_tensor_t*)
+
+}
---
+++ hammerblade/torch/tests/test_ceil.py
@@ -0,0 +1,27 @@
+"""
+Tests on Ceil kernel
+04/10/2020 Kofi Efah (kae87)
+"""
+import torch
+
+torch.manual_seed(42)
+
+def _test_torch_ceil_check(x):
+    h = x.hammerblade()
+    ceil_x = x.ceil()
+    ceil_h = h.ceil()
+    assert ceil_h.device == torch.device("hammerblade")
+    assert torch.allclose(ceil_h.cpu(), ceil_x)
+
+def test_torch_ceil_1():
+    x = torch.ones(10)
+    _test_torch_ceil_check(x)
+
+def test_torch_ceil_2():
+    x = torch.randn(10, 10)
+    _test_torch_ceil_check(x)
+
+def test_torch_ceil_3():
+    x = torch.randn(1)
+    _test_torch_ceil_check(x)
+
