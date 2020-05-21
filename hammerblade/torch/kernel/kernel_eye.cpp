2697e1c31fc48316b16658f50abc40913c09ccff
lc873 <lc873@cornell.edu>
2020-05-21 15:38:08 -0400

    [eye] parallelize the eye kernel; downcast long to uint32_t

--- hammerblade/torch/kernel/kernel_eye.cpp
+++ hammerblade/torch/kernel/kernel_eye.cpp
@@ -4,25 +4,25 @@
 extern "C" {
 
   __attribute__ ((noinline)) int tensorlib_eye(
-    hb_tensor_t* output, long* n, long* m) {
+    hb_tensor_t* output, uint32_t* n, uint32_t* m) {
     HBTensor<float> y(output);
-    long N = *n;
-    long M = *m;
-    // Start profiling
+    uint32_t N = *n;
+    uint32_t M = *m;
 
+    // Start profiling
     bsg_cuda_print_stat_kernel_start();
-    if(__bsg_id == 0) {
-      for(long i = 0; i < N; i++) {
-        for(long j = 0; j < M; j++) {
-          if(i == j) {
-            y(i,j) = 1;
-          }
-          else {
-            y(i,j) = 0;
-          }
+
+    hb_parallel_for(N, [&](size_t i) {
+      for(auto j = 0; j < M; j++) {
+        if(i == j) {
+          y(i,j) = 1;
+        }
+        else {
+          y(i,j) = 0;
         }
       }
-    }
+    });
+
     //   End profiling
     bsg_cuda_print_stat_kernel_end();
 
@@ -30,6 +30,6 @@ extern "C" {
     return 0;
   }
 
-  HB_EMUL_REG_KERNEL(tensorlib_eye, hb_tensor_t*, long*, long*);
+  HB_EMUL_REG_KERNEL(tensorlib_eye, hb_tensor_t*, uint32_t*, uint32_t*);
 
 }
