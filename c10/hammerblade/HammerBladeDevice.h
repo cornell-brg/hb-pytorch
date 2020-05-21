ecff72eaae601679909776e9847a5858373dc5be
lc873 <lc873@cornell.edu>
2020-05-21 11:44:51 -0400

    [HammerBladeDevice] remove unused includes

--- c10/hammerblade/HammerBladeDevice.h
+++ c10/hammerblade/HammerBladeDevice.h
@@ -1,13 +1,10 @@
 #pragma once
 
-/* This header hosts static variables that are essential to manage 
+/* This header hosts static variables that are essential to manage
  * HammerBlade devices
  * Feb 3, 2020
  * Lin Cheng
  */
-
-#include <c10/hammerblade/HammerBladeMacros.h>
-#include <c10/core/Device.h>
 
 #include <bsg_manycore_cuda.h>
 
