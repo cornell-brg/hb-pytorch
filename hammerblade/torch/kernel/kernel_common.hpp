ecf769c676e6f7e788d48d78e57ea7b140f5482e
lc873 <lc873@cornell.edu>
2020-05-21 12:11:28 -0400

    [bsg_tiles_X] if using emul, we set bsg_tiles_X to be emul_hb_mesh_dim.x

--- hammerblade/torch/kernel/kernel_common.hpp
+++ hammerblade/torch/kernel/kernel_common.hpp
@@ -8,13 +8,18 @@
 // BSG_TILE_GROUP_X_DIM and BSG_TILE_GROUP_Y_DIM must be defined
 // before bsg_manycore.h and bsg_tile_group_barrier.h are
 // included.
+#ifdef HB_EMUL
+#include <emul_hb_device.h>
+#define BSG_TILE_GROUP_X_DIM emul_hb_mesh_dim.x
+#define BSG_TILE_GROUP_Y_DIM emul_hb_mesh_dim.y
+#else
 #define BSG_TILE_GROUP_X_DIM 1
 #define BSG_TILE_GROUP_Y_DIM 1
+#endif
 #define bsg_tiles_X BSG_TILE_GROUP_X_DIM
 #define bsg_tiles_Y BSG_TILE_GROUP_Y_DIM
 #include "bsg_manycore.h"
 #include "bsg_set_tile_x_y.h"
-// #include "bsg_tile_group_barrier.h"
 #include "hb_tensor.hpp"
 #include <hb_assert.hpp>
 #include <hb_parallel_for.hpp>
@@ -32,8 +37,5 @@
 // building c10/hammerblade/emul
 
 #include <hammerblade_emul.hpp>
-
-// extern bsg_row_barrier r_barrier;
-// extern bsg_col_barrier c_barrier;
 
 #endif
