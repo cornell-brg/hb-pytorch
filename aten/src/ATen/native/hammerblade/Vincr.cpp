a5d6cf4f04dbec89bd1847ade0584a21e705ccbd
lc873 <lc873@cornell.edu>
2020-06-02 09:24:30 -0400

    [tutorial] more comments

--- aten/src/ATen/native/hammerblade/Vincr.cpp
+++ aten/src/ATen/native/hammerblade/Vincr.cpp
@@ -6,8 +6,10 @@ namespace at { namespace native {
 
 Tensor vincr_hb(const Tensor& self) {
 
+  // Create an output tensor that has the same shape as self
   auto result = at::empty_like(self, self.options());
 
+  // Call HB device kernel tensorlib_vincr
   hb_offload_kernel(result, self, "tensorlib_vincr");
 
   return result;
--- hammerblade/torch/kernel/kernel_vincr.cpp
+++ hammerblade/torch/kernel/kernel_vincr.cpp
@@ -10,8 +10,11 @@ extern "C" {
   __attribute__ ((noinline))  int tensorlib_vincr(
           hb_tensor_t* result_p,
           hb_tensor_t* self_p) {
+
+    // Convert low level pointers to Tensor objects
     HBTensor<float> result(result_p);
     HBTensor<float> self(self_p);
+
     // Start profiling
     bsg_cuda_print_stat_kernel_start();
 
@@ -26,11 +29,12 @@ extern "C" {
     //   End profiling
     bsg_cuda_print_stat_kernel_end();
 
-    // Sync 
+    // Sync
     g_barrier.sync();
     return 0;
   }
 
+  // Register the HB kernel with emulation layer
   HB_EMUL_REG_KERNEL(tensorlib_vincr, hb_tensor_t*, hb_tensor_t*)
 
 }
