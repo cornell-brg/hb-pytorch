adce38c4ef4a19e9bd5bb28b3aae2894b2387cb6
lc873 <lc873@cornell.edu>
2020-06-04 13:29:33 -0400

    [scripts] add the actual scripts ...

---
+++ hammerblade/scripts/process_CPU_stack.py
@@ -0,0 +1,39 @@
+"""
+This script takes in a stack and looks for the CPU_log
+We use this script to process the full input performance on CPU
+per ATen Operator
+
+06/04/2020 Lin Cheng (lc873@cornell.edu)
+"""
+
+import sys
+import os
+sys.path.append(os.getcwd())
+import stack_parser
+
+# INPUT:  raw stack of a single ATen Operator
+# OUTPUT: subtree with CPU_log as root
+# OPTIONS: fancy_func -- show operator name as aten::op instead of raw func signature
+
+def parse(raw_stack, fancy_func=False):
+    root = stack_parser.exec_time_tree(raw_stack, fancy_func=fancy_func)
+    # in the per ATen OP context, the tree looks like
+    #            root
+    #        /          \
+    #   aten::op      other
+    assert len(root.children) == 2
+    assert root.children[1].func == "other"
+    aten_op = root.children[0]
+    # look for @CPU_LOG@
+    # there has to be a CPU_log to use this parser
+    # the aten_op tree should look like
+    #            aten::op
+    #      /        |        \
+    #   CPU_log  ******   HB_log
+    cpu_log = None
+    for child in aten_op.children:
+      if child.func == "@CPU_LOG@":
+          cpu_log = child
+          break
+    assert cpu_log is not None
+    return cpu_log
---
+++ hammerblade/scripts/process_HB_stack.py
@@ -0,0 +1,39 @@
+"""
+This script takes in a stack and looks for the HB_log
+We use this script to process the chunk input performance on HammerBlade
+per ATen Operator
+
+06/04/2020 Lin Cheng (lc873@cornell.edu)
+"""
+
+import sys
+import os
+sys.path.append(os.getcwd())
+import stack_parser
+
+# INPUT:  raw stack of a single ATen Operator
+# OUTPUT: subtree with HB_log as root
+# OPTIONS: fancy_func -- show operator name as aten::op instead of raw func signature
+
+def parse(raw_stack, fancy_func=False):
+    root = stack_parser.exec_time_tree(raw_stack, fancy_func=fancy_func)
+    # in the per ATen OP context, the tree looks like
+    #            root
+    #        /          \
+    #   aten::op      other
+    assert len(root.children) == 2
+    assert root.children[1].func == "other"
+    aten_op = root.children[0]
+    # look for @HB_LOG@
+    # there has to be a HB_log to use this parser
+    # the aten_op tree should look like
+    #            aten::op
+    #      /        |        \
+    #   CPU_log  ******   HB_log
+    hb_log = None
+    for child in aten_op.children:
+      if child.func == "@HB_LOG@":
+          hb_log = child
+          break
+    assert hb_log is not None
+    return hb_log
