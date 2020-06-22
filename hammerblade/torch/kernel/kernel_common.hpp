f5eab7f5c578649183884aaa88208d9bd82d3846
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-06-21 21:10:36 -0700

    Fixed memcpy return type

--- hammerblade/torch/kernel/kernel_common.hpp
+++ hammerblade/torch/kernel/kernel_common.hpp
@@ -29,9 +29,9 @@
 #include <hb_tiled_for.hpp>
 #include <hb_hw_patch.hpp>
 
-void hb_memcpy(__remote void* __restrict dest,
-               const __remote void* __restrict src,
-               size_t n);
+__remote void* hb_memcpy(__remote void* __restrict dest,
+                         const __remote void* __restrict src,
+                         size_t n);
 
 //====================================================================
 // HammerBlade kernel emulation
