5036d23959cc68ac9bab3f134105a9d43314f95d
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-06-21 21:04:03 -0700

    memcpy with remote annotations

--- hammerblade/torch/kernel/kernel_common.cpp
+++ hammerblade/torch/kernel/kernel_common.cpp
@@ -1,4 +1,7 @@
 #include <kernel_common.hpp>
+#include <string.h>
+#include <stdint.h>
+#include "../../string/local.h"
 
 // common reduction buffer
 void* g_reduction_buffer;
@@ -10,6 +13,72 @@ bsg_barrier g_barrier;
 bsg_barrier<bsg_tiles_X, bsg_tiles_Y> g_barrier;
 #endif // HB_EMUL
 
+// This is just Newlib's memcpy with __remote annotations
+void hb_memcpy(__remote void* __restrict aa,
+               const __remote void* __restrict bb,
+               size_t n) {
+  #define BODY(a, b, t) { \
+    t tt = *b; \
+    a++, b++; \
+    *(a - 1) = tt; \
+  }
+
+  __remote char *a = (__remote char *)aa;
+  const __remote char *b = (const __remote char *)bb;
+  __remote char *end = a + n;
+  uintptr_t msk = sizeof (long) - 1;
+  if (unlikely ((((uintptr_t)a & msk) != ((uintptr_t)b & msk))
+           || n < sizeof (long)))
+    {
+small:
+      if (__builtin_expect (a < end, 1))
+    while (a < end)
+      BODY (a, b, char);
+      return aa;
+    }
+
+  if (unlikely (((uintptr_t)a & msk) != 0))
+    while ((uintptr_t)a & msk)
+      BODY (a, b, char);
+
+  __remote long *la = (__remote long *)a;
+  const __remote long *lb = (const __remote long *)b;
+  __remote long *lend = (__remote long *)((uintptr_t)end & ~msk);
+
+  if (unlikely (la < (lend - 8)))
+    {
+      while (la < (lend - 8))
+    {
+      long b0 = *lb++;
+      long b1 = *lb++;
+      long b2 = *lb++;
+      long b3 = *lb++;
+      long b4 = *lb++;
+      long b5 = *lb++;
+      long b6 = *lb++;
+      long b7 = *lb++;
+      long b8 = *lb++;
+      *la++ = b0;
+      *la++ = b1;
+      *la++ = b2;
+      *la++ = b3;
+      *la++ = b4;
+      *la++ = b5;
+      *la++ = b6;
+      *la++ = b7;
+      *la++ = b8;
+    }
+    }
+
+  while (la < lend)
+    BODY (la, lb, long);
+
+  a = (__remote char *)la;
+  b = (const __remote char *)lb;
+  if (unlikely (a < end))
+    goto small;
+  return aa;
+}
 
 extern "C" {
 
