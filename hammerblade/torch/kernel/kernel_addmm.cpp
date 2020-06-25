14ec48794d9af6638fea78d9241e3627e9f11895
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-06-24 23:53:56 -0700

    Addmm result partitioned

--- hammerblade/torch/kernel/kernel_addmm.cpp
+++ hammerblade/torch/kernel/kernel_addmm.cpp
@@ -137,7 +137,6 @@ extern "C" {
     // Start profiling
     bsg_cuda_print_stat_kernel_start();
 
-
     // v2: single tile, use blocking
     int r1 = mat1.dim(0);
     int c1 = mat1.dim(1);
@@ -145,74 +144,42 @@ extern "C" {
     int c2 = mat2.dim(1);
     hb_assert(c1 == r2);
 
-    // calculate number of row and col blocks in each matrix
-    int m1_num_blk_per_row = (r1 + BLOCK_DIM - 1) / BLOCK_DIM; // how many blocks in m1 per row
-    int m1_num_blk_per_col = (c1 + BLOCK_DIM - 1) / BLOCK_DIM; // how many blocks in m1 per col
-    int m2_num_blk_per_row = (r2 + BLOCK_DIM - 1) / BLOCK_DIM; // how many blocks in m2 per row
-    int m2_num_blk_per_col = (c2 + BLOCK_DIM - 1) / BLOCK_DIM; // how many blocks in m2 per col
+    //---------------------------------------------------
+    // calculate x dimension start and end for this tile
+    //---------------------------------------------------
+    size_t len_per_tile = r1 / bsg_tiles_X + 1;
+    size_t start_x = len_per_tile * __bsg_x;
+    size_t end_x = start_x + len_per_tile;
+    end_x = (end_x > r1)  ? r1 : end_x;
 
-    // calculate dimensions of the last row and col block in each matrix
-    int m1_last_blk_dim_x = c1 % BLOCK_DIM == 0 ? BLOCK_DIM : c1 % BLOCK_DIM; // x dimension of last block of mat1
-    int m1_last_blk_dim_y = r1 % BLOCK_DIM == 0 ? BLOCK_DIM : r1 % BLOCK_DIM; // y dimension of last block of mat1
-    int m2_last_blk_dim_x = c2 % BLOCK_DIM == 0 ? BLOCK_DIM : c2 % BLOCK_DIM; // x dimension of last block of mat2
-    int m2_last_blk_dim_y = r2 % BLOCK_DIM == 0 ? BLOCK_DIM : r2 % BLOCK_DIM; // y dimension of last block of mat2
+    //---------------------------------------------------
+    // calculate Y dimension start and end for this tile
+    //---------------------------------------------------
+    len_per_tile = c2 / bsg_tiles_Y + 1;
+    size_t start_y = len_per_tile * __bsg_y;
+    size_t end_y = start_y + len_per_tile;
+    end_y = (end_y > c2)  ? c2 : end_y;
 
-    // iterate over result blocks
-    hb_tiled_for(m1_num_blk_per_row * m2_num_blk_per_col, [&](size_t ridx) {
-        int rr = ridx / m2_num_blk_per_col;
-        // rc is index of col block in result matrix
-        int rc = ridx % m2_num_blk_per_col;
-        // calculate current result block dimensions
-        int res_dim_y = rr == m1_num_blk_per_row - 1 ? m1_last_blk_dim_y : BLOCK_DIM;
-        int res_dim_x = rc == m2_num_blk_per_col - 1 ? m2_last_blk_dim_x : BLOCK_DIM;
-        int partial_block = (res_dim_y != BLOCK_DIM) || (res_dim_x != BLOCK_DIM);
+    //-----------------
+    // loop
+    //----------------
+    for(size_t k = 0; k < c1; k++) {
+      for (size_t i = start_x; i < end_x; i++) {
+        for (size_t j = start_y; j < end_y; j++) {
+          if(k == 0)
+            result(i, j) = 0.0;
 
-        // initialize scratchpad result (load beta * self into result)
-
-        // unrolled version
-        float sp_result[res_dim_y * res_dim_x];
-        if (partial_block) { // general case
-            dram_to_sp(sp_result, beta, self, res_dim_y, res_dim_x, rr, rc);
-        } else { // common case: res_dim_y == res_dim_x == BLOCK_SIZE
-            dram_to_sp_simple(sp_result, beta, self, res_dim_y, res_dim_x, rr, rc);
+          result(i, j) += mat1(i, k) * mat2(k, j);
         }
-        // end: unrolled version
+      }
+    }
 
-        // process mat1 and mat2 for this result block
-        // only care about blocks of mat1 in row rr
-        // and blocks of mat2 in col rc
-        for (int mat1x = 0, mat2y = 0; mat1x < m1_num_blk_per_col && mat2y < m2_num_blk_per_row; mat1x++, mat2y++) {
-            // calculate current block dimensions
-            int mid_dim = mat1x == m1_num_blk_per_col - 1 ? m1_last_blk_dim_x : BLOCK_DIM;
-            partial_block = partial_block || (mid_dim != BLOCK_DIM);
-
-            // load mat1 and mat2 into scratchpad
-
-            // unrolled version
-            float sp_mat1[res_dim_y * mid_dim];
-            float sp_mat2[mid_dim * res_dim_x];
-            if (partial_block) { // general case
-                dram_to_sp(sp_mat1, 1.0f, mat1, res_dim_y, mid_dim, rr, mat1x);
-                dram_to_sp(sp_mat2, 1.0f, mat2, mid_dim, res_dim_x, mat2y, rc);
-                compute(sp_result, sp_mat1, sp_mat2, alpha, res_dim_y, res_dim_x, mid_dim);
-            } else { // common case: res_dim_y == res_dim_x == BLOCK_SIZE
-                dram_to_sp_simple(sp_mat1, 1.0f, mat1, res_dim_y, mid_dim, rr, mat1x);
-                dram_to_sp_simple(sp_mat2, 1.0f, mat2, mid_dim, res_dim_x, mat2y, rc);
-                compute_simple(sp_result, sp_mat1, sp_mat2, alpha, res_dim_y, res_dim_x, mid_dim);
-            }
-            // end: unrolled version
-
-        }
-
-        // copy this block back into DRAM
-        for (int i = 0; i < res_dim_y; i++) {
-            for (int j = 0; j < res_dim_x; j++) {
-                // unrolled version
-                result(rr * BLOCK_DIM + i, rc * BLOCK_DIM + j) = sp_result[i * res_dim_x + j];
-                // end: unrolled version
-            }
-        }
-    });
+    for (size_t i = start_x; i < end_x; i++) {
+      for (size_t j = start_y; j < end_y; j++) {
+        result(i, j) = beta * self(i, j) + alpha * result(i, j);
+      }
+    }
+    
     //   End profiling
     bsg_cuda_print_stat_kernel_end();
 
