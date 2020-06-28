52c27616e481acd42b68fb3e2fc62d9e1c1e9ed4
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-06-28 13:21:43 -0700

    Added macros for remote and noalias defintions

--- hammerblade/torch/kernel/kernel_common.hpp
+++ hammerblade/torch/kernel/kernel_common.hpp
@@ -29,6 +29,28 @@
 #include <hb_tiled_for.hpp>
 #include <hb_hw_patch.hpp>
 
+// Pointers to remote locations (non-scratchpad) could be qualified
+// with __remote. Doing so would tell compiler to assign a different
+// address space to the contents of those pointers and latencies of
+// memory accesses would be considered as 20 cycles. For example,
+// `__remote float* foo;` essentially declares foo as `__remote float*`
+// type, and the compiler assumes loads from `foo` to take 20 cycles
+// on average.
+#ifdef __clang__
+#define __remote __attribute__((address_space(1)))
+#else
+#define __remote
+#endif
+
+// This macro is to protect the code from uncertainity with
+// restrict/__restrict/__restrict__. Apparently some Newlib
+// headers define __restrict as nothing, but __restrict__
+// seems to work. So, we can use NOALIAS as our main way to
+// resolve pointer alaising and possibly in future we could
+// have `#ifdef`s here to make sure we use the right one in
+// each circumstance.
+#define NOALIAS __restrict__
+
 __remote void* hb_memcpy(__remote void* NOALIAS dest,
                          const __remote void* NOALIAS src,
                          size_t n);
