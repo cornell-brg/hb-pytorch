4e2ad83d9b5b7e25863bf346b336a3deac4847e0
Andrew Martin Pareles <amp342@gorgonzola.cs.cornell.edu>
2020-06-29 18:03:09 -0400

    done on hammerblade implementation! (now to test)

--- hammerblade/torch/kernel/kernel_sddmm.cpp
+++ hammerblade/torch/kernel/kernel_sddmm.cpp
@@ -1,29 +1,49 @@
 //====================================================================
 // Sampled dense-dense matrix multiply
-// 05/03/2020 Andrew Pareles (amp342@cornell.edu)
+// 06/29/2020 Andrew Pareles (amp342@cornell.edu)
 //====================================================================
+
 
 #include <kernel_common.hpp>
 #include <cmath>
 
 extern "C" {
 
-  __attribute__ ((noinline))  int tensorlib_round(
-          hb_tensor_t* a_p, //sample (sparse)
-          hb_tensor_t* b_p, 
-          hb_tensor_t* c_p, 
-          hb_tensor_t* out_p) { //destination
+
+/** 
+ * For each i,j, if the element a_ij is nonzero,
+ * find (b@c)_ij. The result goes in out_p.
+ * Nonzero entries of a are represented by (col, row).
+*/
+  __attribute__ ((noinline))  int tensorlib_sddmm(
+          hb_tensor_t* out_p, //destination
+          hb_tensor_t* col_p, //cols
+          hb_tensor_t* row_p, //rows
+          hb_tensor_t* b_p, //dense
+          hb_tensor_t* c_p, //dense
+          int* dot_prod_len, // i.e. b.size(0) or c.size(1)
+          int* nnz
+          ) { 
     // Start profiling
     bsg_cuda_print_stat_kernel_start();
 
-  auto a = HBTensor<float>(a_p);
-  auto b = HBTensor<float>(b_p);
-  auto c = HBTensor<float>(c_p);
-  auto res = HBTensor<float>(out_p);
+    auto cols = HBTensor<float>(col_p);
+    auto rows = HBTensor<float>(row_p);
+    auto b = HBTensor<float>(b_p);
+    auto c = HBTensor<float>(c_p);
+    auto res = HBTensor<float>(out_p);
+    auto dp_len = *dot_prod_len;
 
-    hb_tiled_foreach(inp, res,
-      [&](float a) {
-        return rintf(a);
+
+    hb_tiled_for(nnz, [&](int i) {
+      int row = rows(i);
+      int col = cols(i);
+
+      float sum = 0;
+      for (int dot = 0; dot < dp_len; dot++){
+        sum += b(row, dot) * c(dot, col);
+      }
+      res(row, col) = sum;
     });
 
     //   End profiling
@@ -33,6 +53,6 @@ extern "C" {
     return 0;
   }
 
-  HB_EMUL_REG_KERNEL(tensorlib_round, hb_tensor_t*, hb_tensor_t*)
+  HB_EMUL_REG_KERNEL(tensorlib_sddmm, hb_tensor_t*, hb_tensor_t*, hb_tensor_t*, hb_tensor_t*)
 
 }
