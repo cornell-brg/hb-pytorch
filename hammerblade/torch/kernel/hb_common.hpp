74d4be8e4f28e5e5a8aebdfd49d70e48d9bd26db
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-07-06 10:39:57 -0700

    Moved defs to manycore repo so that they can widely useful

--- hammerblade/torch/kernel/hb_common.hpp
+++ hammerblade/torch/kernel/hb_common.hpp
@@ -1,35 +1,6 @@
 #ifndef _HB_COMMON_HPP
 #define _HB_COMMON_HPP
 
-// Pointers to remote locations (non-scratchpad) could be qualified
-// with __remote. Doing so would tell compiler to assign a different
-// address space to the contents of those pointers and latencies of
-// memory accesses would be considered as 20 cycles. For example,
-// `__remote float* foo;` essentially declares foo as `__remote float*`
-// type, and the compiler assumes loads from `foo` to take 20 cycles
-// on average.
-#ifdef __clang__
-#define __remote __attribute__((address_space(1)))
-#else
-#define __remote
-#endif
-
-// This macro is to protect the code from uncertainity with
-// restrict/__restrict/__restrict__. Apparently some Newlib
-// headers define __restrict as nothing, but __restrict__
-// seems to work. So, we can use NOALIAS as our main way to
-// resolve pointer alaising and possibly in future we could
-// have `#ifdef`s here to make sure we use the right one in
-// each circumstance.
-#define NOALIAS __restrict__
-
-#define PRAGMA(x) _Pragma(#x)
-#ifdef __clang__
-#define UNROLL(n) PRAGMA(unroll n)
-#else
-#define UNROLL(n) PRAGMA(GCC unroll n)
-#endif
-
 // =============================================================
 // Workarounds for HB HW Issues
 //
