68d77759a288043aec760623d6dbc130b47801ac
Andrew Martin Pareles <amp342@gorgonzola.cs.cornell.edu>
2020-07-09 16:20:51 -0400

    hopefully this fixes everything...

--- aten/src/ATen/native/sparse/hammerblade/SddmmHammerBlade.cpp
+++ aten/src/ATen/native/sparse/hammerblade/SddmmHammerBlade.cpp
@@ -23,7 +23,7 @@ Tensor sddmm_hb(const SparseTensor& sample, const Tensor& b, const Tensor& c) {
    
   TORCH_CHECK(sample.sparse_dim() == 2, "We do not support hybrid sparse tensor for 'sample' in HammerBlade sddmm!");
   TORCH_CHECK(b.dim() == 2 && c.dim() == 2, "Expected 2D matrixes for 'mat1' and 'mat2', but got ", b.dim(), " and ", c.dim(), " tensors");
-  TORCH_CHECK(b.size(0) == c.size(1), "Matrix multiply dimension mismatch: 'mat1' dim 0 = ", b.size(0), ", 'mat2' dim 1 = ", c.size(1));
+  TORCH_CHECK(b.size(1) == c.size(0), "Matrix multiply dimension mismatch: 'mat1' dim 0 = ", b.size(0), ", 'mat2' dim 1 = ", c.size(1));
   
   IntTensor indices = sample._indices();
   TORCH_CHECK(indices.dtype() == at::kInt, "Indices on HammerBlade should be int32");
@@ -31,9 +31,9 @@ Tensor sddmm_hb(const SparseTensor& sample, const Tensor& b, const Tensor& c) {
   TORCH_CHECK(colIndices.is_hammerblade(), "colIndices show be HammerBlade Tensor");
   IntTensor rowIndices = indices.select(0, 0);
   
-  Tensor result = at::zeros({b.size(1), c.size(0)}, {at::requires_grad().device(at::kHAMMERBLADE).dtype(at::kFloat)});
+  Tensor result = at::zeros({b.size(0), c.size(1)}, {at::requires_grad().device(at::kHAMMERBLADE).dtype(at::kFloat)});
 
-  int64_t dot_prod_len = b.size(0);
+  int64_t dot_prod_len = b.size(1);
   int64_t nnz = sample._nnz();
 
   hb_offload_kernel(result, colIndices, rowIndices, b, c, dot_prod_len, nnz, "tensorlib_sddmm");
