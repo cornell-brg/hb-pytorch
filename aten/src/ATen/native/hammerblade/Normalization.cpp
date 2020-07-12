f4cf0c2115df6bd53534ab7f307833f2917e1402
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-07-12 15:13:56 -0700

    Separate train anf evaluate modes

--- aten/src/ATen/native/hammerblade/Normalization.cpp
+++ aten/src/ATen/native/hammerblade/Normalization.cpp
@@ -5,6 +5,30 @@
 namespace at {
 namespace native {
 
+std::tuple<Tensor,Tensor,Tensor> batch_norm_hb_transform_input(
+    const Tensor& input, const Tensor& weight, const Tensor& bias,
+    const Tensor& save_mean /* optional */,
+    const Tensor& save_invstd /* optional */,
+    const Tensor& running_mean /* optional */,
+    const Tensor& running_var /* optional */,
+    bool train, double eps) {
+  TORCH_CHECK(false, "HB BatchNorm transform input unimplemented");
+  return std::make_tuple(input, save_mean, save_invstd);
+}
+
+std::tuple<Tensor,Tensor> batch_norm_hb_update_stats(
+    const Tensor& input, const Tensor& running_mean, const Tensor& running_var,
+    double momentum, double eps) {
+  TORCH_CHECK(false, "HB BatchNorm update stats unimplemented");
+
+  int64_t n_input = input.size(1);
+  int64_t n = input.numel() / n_input;
+
+  Tensor save_mean = at::empty({n_input}, input.options());
+  Tensor save_var_transform = at::empty({n_input}, input.options());
+  return std::make_tuple(save_mean, save_var_transform);
+}
+
 std::tuple<Tensor, Tensor, Tensor> batch_norm_hb(
     const Tensor& self, const Tensor& weight, const Tensor& bias,
     const Tensor& running_mean, const Tensor& running_var,
@@ -13,20 +37,18 @@ std::tuple<Tensor, Tensor, Tensor> batch_norm_hb(
                {self, weight, bias, running_mean, running_var},
                Backend::HammerBlade);
 
-  TORCH_CHECK(false, "HB BatchNorm kernel called! (unimplemented)");
-
-  //return AT_DISPATCH_FLOAT_TYPE_ONLY(self.scalar_type(), "batch_norm", [&] {
-  //    if (!train) {
-  //      return batch_norm_cpu_transform_input_template<scalar_t>(
-  //          self, weight, bias, {}, {}, running_mean, running_var, train, eps);
-  //    } else {
-  //      auto save_stats = batch_norm_cpu_update_stats_template<scalar_t, InvStd>(
-  //          self, running_mean, running_var, momentum, eps);
-  //      return batch_norm_cpu_transform_input_template<scalar_t>(
-  //          self, weight, bias, std::get<0>(save_stats), std::get<1>(save_stats),
-  //          running_mean, running_var, train, eps);
-  //    }
-  //  });
+  return AT_DISPATCH_FLOAT_TYPE_ONLY(self.scalar_type(), "batch_norm", [&] {
+      if (!train) {
+        return batch_norm_hb_transform_input(
+            self, weight, bias, {}, {}, running_mean, running_var, train, eps);
+      } else {
+        auto save_stats = batch_norm_hb_update_stats(
+            self, running_mean, running_var, momentum, eps);
+        return batch_norm_hb_transform_input(
+            self, weight, bias, std::get<0>(save_stats), std::get<1>(save_stats),
+            running_mean, running_var, train, eps);
+      }
+    });
 }
 
 }} // namespace at::native
