b21e970e71d7dfa7948dcd7e5166853b609d5436
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-07-16 21:14:49 -0700

    BatchNorm2d backward and kernel placeholder

--- aten/src/ATen/native/hammerblade/Normalization.cpp
+++ aten/src/ATen/native/hammerblade/Normalization.cpp
@@ -84,4 +84,49 @@ std::tuple<Tensor, Tensor, Tensor> batch_norm_hb(
     });
 }
 
+std::tuple<Tensor, Tensor, Tensor> batch_norm_backward_hb(
+    const Tensor& grad_out, const Tensor& self, const Tensor& weight,
+    const Tensor& running_mean, const Tensor& running_var,
+    const Tensor& save_mean, const Tensor& save_invstd,
+    bool train, double eps, std::array<bool,3> grad_input_mask) {
+  TORCH_CHECK(self.dim() == 4,
+              "Only 2d BatchNorm is implemented for HB backend.");
+
+  return AT_DISPATCH_FLOAT_TYPE_ONLY(
+      self.scalar_type(), "batch_norm_backward_hb", [&] {
+      Tensor grad_input;
+      Tensor grad_weight;
+      Tensor grad_bias;
+      if (grad_input_mask[0]) {
+        grad_input = at::empty(self.sizes(), self.options());
+      }
+      if (grad_input_mask[1]) {
+        grad_weight = at::empty(weight.sizes(), weight.options());
+      }
+      if (grad_input_mask[2]) {
+        grad_bias = at::empty(weight.sizes(), weight.options());
+      }
+
+      std::vector<eva_t> device_args;
+      std::vector<eva_t> device_ptrs;
+      device_args.push_back(create_device_tensor(grad_input, device_ptrs));
+      device_args.push_back(create_device_tensor(grad_weight, device_ptrs));
+      device_args.push_back(create_device_tensor(grad_bias, device_ptrs));
+      device_args.push_back(create_device_tensor(grad_out, device_ptrs));
+      device_args.push_back(create_device_tensor(self, device_ptrs));
+      device_args.push_back(create_device_tensor(weight, device_ptrs));
+      device_args.push_back(create_device_tensor(save_mean, device_ptrs));
+      device_args.push_back(create_device_tensor(save_invstd, device_ptrs));
+      device_args.push_back(create_device_tensor(running_mean, device_ptrs));
+      device_args.push_back(create_device_tensor(running_var, device_ptrs));
+      device_args.push_back(create_device_scalar((int) train));
+      device_args.push_back(create_device_scalar((float) eps));
+      c10::hammerblade::offload_kernel("tensorlib_batch_norm2d_backward",
+                                       device_args);
+      cleanup_device(device_args, device_ptrs);
+
+      return std::make_tuple(grad_input, grad_weight, grad_bias);
+    });
+}
+
 }} // namespace at::native
