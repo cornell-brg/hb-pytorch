402d410333cfec6848ec71bea5c25ca3fea5e691
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-07-17 10:50:45 -0700

    Weight and bias gradients

--- hammerblade/torch/kernel/kernel_batch_norm.cpp
+++ hammerblade/torch/kernel/kernel_batch_norm.cpp
@@ -245,6 +245,14 @@ extern "C" {
             }
           }
         }
+
+        if(grad_weight.numel()) {
+          grad_weight(c) = dotp * invstd;
+        }
+
+        if(grad_bias.numel()) {
+          grad_bias(c) = sum;
+        }
       }
     }
 
