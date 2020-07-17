8938bce78aa2a1715ef5262ae8310735c27730d7
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-07-17 12:05:40 -0700

    Parallel BatchNorm2d backward kernel

--- hammerblade/torch/kernel/kernel_batch_norm.cpp
+++ hammerblade/torch/kernel/kernel_batch_norm.cpp
@@ -168,91 +168,119 @@ extern "C" {
     uint32_t W = input.get_sizes()[3];
     uint32_t numel = input.numel() / C;
 
-    if(__bsg_id == 0) {
-      for(size_t c = 0; c < C; ++c) {
-        float mean, invstd;
-        if(train) {
-          mean = save_mean(c);
-          invstd = save_invstd(c);
+    for(size_t c = 0; c < C; ++c) {
+      float mean, invstd;
+      if(train) {
+        mean = save_mean(c);
+        invstd = save_invstd(c);
+      } else {
+        mean = running_mean(c);
+        invstd = 1 / sqrt(running_var(c) + eps);
+      }
+
+      float gamma = weight.numel() ? weight(c) : 1.0;
+
+      float* reduction_buffer = (float*) g_reduction_buffer;
+
+      // Compute partial sum and store it in
+      // reduction buffer
+      float partial_sum = 0.0;
+      hb_tiled_for(N * H * W, [&](size_t i) {
+          size_t w = i % W;
+          size_t h = (i / W) % H;
+          size_t n = (i / (W * H)) % N;
+
+          partial_sum += grad_out(n, c, h, w);
+      });
+      reduction_buffer[__bsg_id] = partial_sum;
+      g_barrier.sync();
+
+      // Use tile 0 to compute the mean
+      if(__bsg_id == 0) {
+        float final_sum = 0.0;
+        for(size_t i = 0; i < bsg_tiles_X * bsg_tiles_Y; ++i) {
+          final_sum += reduction_buffer[i];
+        }
+        reduction_buffer[0] = final_sum;
+      }
+      g_barrier.sync();
+
+      // Load final sum from the gloabal buffer
+      float sum = reduction_buffer[0];
+      g_barrier.sync();
+
+      // Compute partial sum and store it in
+      // reduction buffer
+      partial_sum = 0.0;
+      hb_tiled_for(N * H * W, [&](size_t i) {
+          size_t w = i % W;
+          size_t h = (i / W) % H;
+          size_t n = (i / (W * H)) % N;
+
+          partial_sum += (input(n, c, h, w) - mean) * grad_out(n, c, h, w);
+      });
+      reduction_buffer[__bsg_id] = partial_sum;
+      g_barrier.sync();
+
+      // Use tile 0 to compute the mean
+      if(__bsg_id == 0) {
+        float final_sum = 0.0;
+        for(size_t i = 0; i < bsg_tiles_X * bsg_tiles_Y; ++i) {
+          final_sum += reduction_buffer[i];
+        }
+        reduction_buffer[0] = final_sum;
+      }
+      g_barrier.sync();
+
+      // Load final sum from the gloabal buffer
+      float dotp = reduction_buffer[0];
+      g_barrier.sync();
+
+      // Host code can mask each of these gradient computations. Host
+      // masks a gradient computation by offloading an empty tensor for
+      // corresponding gradient tensor. So, device should skip computing
+      // a gradient if corresponding tensor is empty.
+
+      if(grad_input.numel()) {
+        if (train) {
+          // when in training mode
+          // Q(X) = X - E[x] ; i.e. input centered to zero mean
+          // Y = Q(X) / sigma    ; i.e. BN output before weight and bias
+          // dL/dX = (Q(dL/dY) - dot(Y, dL/dY) * Y) / sigma * w
+
+          // projection of gradOutput on to output scaled by std
+          float k = dotp * invstd * invstd / numel;
+          float grad_mean = sum / numel;
+          hb_tiled_for(N * H * W, [&](size_t i) {
+              size_t w = i % W;
+              size_t h = (i / W) % H;
+              size_t n = (i / (W * H)) % N;
+
+              float scaled_var = (input(n, c, h, w) - mean) * k;
+              grad_input(n, c, h, w) = (grad_out(n, c, h, w) - grad_mean 
+                                          - scaled_var) * invstd * gamma;
+          });
         } else {
-          mean = running_mean(c);
-          invstd = 1 / sqrt(running_var(c) + eps);
+          // when in evaluation mode
+          // Q(X) = X - running_mean  ; i.e. input centered to zero mean
+          // Y = Q(X) / running_std    ; i.e. BN output before weight and bias
+          // dL/dX = w / running_std
+          hb_tiled_for(N * H * W, [&](size_t i) {
+              size_t w = i % W;
+              size_t h = (i / W) % H;
+              size_t n = (i / (W * H)) % N;
+
+              grad_input(n, c, h, w) = grad_out(n, c, h, w) * invstd * gamma;
+          });
         }
+      }
 
-        float gamma = weight.numel() ? weight(c) : 1.0;
+      if(grad_weight.numel()) {
+        grad_weight(c) = dotp * invstd;
+      }
 
-        float sum = 0.0;
-        for(size_t n = 0; n < N; ++n) {
-          for(size_t h = 0; h < H; ++h) {
-            for(size_t w = 0; w < W; ++w) {
-              sum += grad_out(n, c, h, w);
-            }
-          }
-        }
-
-        float dotp = 0.0;
-        for(size_t n = 0; n < N; ++n) {
-          for(size_t h = 0; h < H; ++h) {
-            for(size_t w = 0; w < W; ++w) {
-              dotp += (input(n, c, h, w) - mean) * grad_out(n, c, h, w);
-            }
-          }
-        }
-
-        // Host code can mask each of these gradient computations. Host
-        // masks a gradient computation by offloading an empty tensor for
-        // corresponding gradient tensor. So, device should skip computing
-        // a gradient if corresponding tensor is empty.
-
-        if(grad_input.numel()) {
-          if (train) {
-            // when in training mode
-            // Q(X) = X - E[x] ; i.e. input centered to zero mean
-            // Y = Q(X) / sigma    ; i.e. BN output before weight and bias
-            // dL/dX = (Q(dL/dY) - dot(Y, dL/dY) * Y) / sigma * w
-
-            // projection of gradOutput on to output scaled by std
-            float k = dotp * invstd * invstd / numel;
-            for(size_t n = 0; n < N; ++n) {
-              for(size_t h = 0; h < H; ++h) {
-                for(size_t w = 0; w < W; ++w) {
-                  grad_input(n, c, h, w) = (input(n, c, h, w) - mean) * k;
-                }
-              }
-            }
-
-            float grad_mean = sum / numel;
-            for(size_t n = 0; n < N; ++n) {
-              for(size_t h = 0; h < H; ++h) {
-                for(size_t w = 0; w < W; ++w) {
-                  grad_input(n, c, h, w) = (grad_out(n, c, h, w) - grad_mean 
-                                              - grad_input(n, c, h, w))
-                                              * invstd * gamma;
-                }
-              }
-            }
-          } else {
-            // when in evaluation mode
-            // Q(X) = X - running_mean  ; i.e. input centered to zero mean
-            // Y = Q(X) / running_std    ; i.e. BN output before weight and bias
-            // dL/dX = w / running_std
-            for(size_t n = 0; n < N; ++n) {
-              for(size_t h = 0; h < H; ++h) {
-                for(size_t w = 0; w < W; ++w) {
-                  grad_input(n, c, h, w) = grad_out(n, c, h, w) * invstd * gamma;
-                }
-              }
-            }
-          }
-        }
-
-        if(grad_weight.numel()) {
-          grad_weight(c) = dotp * invstd;
-        }
-
-        if(grad_bias.numel()) {
-          grad_bias(c) = sum;
-        }
+      if(grad_bias.numel()) {
+        grad_bias(c) = sum;
       }
     }
 
