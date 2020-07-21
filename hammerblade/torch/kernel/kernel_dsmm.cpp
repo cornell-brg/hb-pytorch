61e231bad623917a6bb5ce357cbe90360e598eb4
Andrew Martin Pareles <amp342@gorgonzola.cs.cornell.edu>
2020-07-21 17:31:34 -0400

    csc functionality (untested)

--- hammerblade/torch/kernel/kernel_dsmm.cpp
+++ hammerblade/torch/kernel/kernel_dsmm.cpp
@@ -11,12 +11,12 @@ extern "C" {
 
 
 /** 
- *  Dense-sparse matrix multiply.
+ *  Dense-(sparse.T) matrix multiply.
 */
-  __attribute__ ((noinline))  int tensorlib_dsmm(
+  __attribute__ ((noinline))  int tensorlib_dstmm(
           hb_tensor_t* out_p, //destination
           hb_tensor_t* a_p, //dense
-          hb_tensor_t* b_cols_p,
+          hb_tensor_t* b_csc_p,
           hb_tensor_t* b_rows_p,
           hb_tensor_t* b_values_p
           ) { 
@@ -25,11 +25,10 @@ extern "C" {
 
     auto res = HBTensor<float>(out_p);
     auto a = HBTensor<float>(a_p);
-    auto b_cols = HBTensor<int>(b_cols_p);
+    auto b_csc = HBTensor<int>(b_csc_p);
     auto b_rows = HBTensor<int>(b_rows_p);
     auto b_vals = HBTensor<float>(b_values_p);
-    
-    auto a_ncols = a.dim(1);
+
     auto a_nrows = res.dim(0);
     auto b_ncols = res.dim(1);
     auto nnz = b_vals.numel();
@@ -44,20 +43,30 @@ extern "C" {
       printf("%i, ", b_rows(i));
     } printf("\n");
 
-    printf("bcols:"); 
-    for (int i = 0;i< b_cols.numel(); i++){
-      printf("%i, ", b_cols(i));
+    printf("b_csc:"); 
+    for (int i = 0;i< b_csc.numel(); i++){
+      printf("%i, ", b_csc(i));
     } printf("\n");
 
-    // for each row of res, do the multiplication
+    // for each row of a, do the multiplication
     //(since this is a very even way to distribute the work)
     float sum;
-    hb_tiled_for(nnz, [&](size_t i) {
-      int b_row = b_rows(i);
-      int b_col = b_cols(i);
-      float b_val = b_vals(i);
-      for (int a_row = 0; a_row < a_nrows; a_row++){
-        res(a_row, b_col) += b_val * a(a_row, b_row);
+    hb_tiled_for(a_nrows, [&](int a_row) {
+      //for each interval [n,n+1] of b_csc (ie each col of b):
+      for (int b_col = 0; b_col < b_ncols; b_col++){
+        sum = 0;
+        //for each nonzero entry in this col of b
+        for (int b_row_idx = b_csc(b_col); b_row_idx < b_csc(b_col+1); b_row_idx++){
+          int b_row = b_rows(b_row_idx);
+          float b_val = b_vals(b_row_idx);
+          printf("arow %i, ", a_row);
+          printf("bcol %i, ", b_col);
+          printf("brow %i, ", b_row);
+          printf("bval %f\n", b_val);
+          sum += b_val * a(a_row, b_row); //a_col = b_row in dot prod
+        }
+        printf("\n");
+        res(a_row, b_col) = sum;
       }
     });
 
@@ -68,6 +77,6 @@ extern "C" {
     return 0;
   }
 
-  HB_EMUL_REG_KERNEL(tensorlib_dsmm, hb_tensor_t*, hb_tensor_t*, hb_tensor_t*, hb_tensor_t*, hb_tensor_t*)
+  HB_EMUL_REG_KERNEL(tensorlib_dstmm, hb_tensor_t*, hb_tensor_t*, hb_tensor_t*, hb_tensor_t*, hb_tensor_t*)
 
 }
