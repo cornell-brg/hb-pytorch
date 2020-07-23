520e8cc7b72cb7f0f09fbdd0a2dd94932386c578
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-07-22 23:01:12 -0700

    Compute flop count for each input

--- hammerblade/torch/ubenchmarks/conv2d.py
+++ hammerblade/torch/ubenchmarks/conv2d.py
@@ -8,6 +8,8 @@ import utils # noqa
 if __name__ == "__main__":
     args = utils.parse_args()
 
+    log = ""
+
     in_channels = 3
     out_channels = 8
     kernel_size = 3
@@ -25,8 +27,8 @@ if __name__ == "__main__":
         torch.rand(64, in_channels, 64, 64, requires_grad=True),
     ]
 
-    utils.benchmark_module(torch.nn.Conv2d, inputs, args.backward,
-                           in_channels, out_channels, kernel_size)
+    #log += utils.benchmark_module(torch.nn.Conv2d, inputs, args.backward,
+    #                              in_channels, out_channels, kernel_size)
 
     in_channels = 3
     out_channels = 16
@@ -36,14 +38,16 @@ if __name__ == "__main__":
         torch.rand(1, in_channels, 32, 32, requires_grad=True),
         torch.rand(1, in_channels, 64, 64, requires_grad=True),
         torch.rand(8, in_channels, 32, 32, requires_grad=True),
-        torch.rand(8, in_channels, 64, 64, requires_grad=True),
-        torch.rand(16, in_channels, 32, 32, requires_grad=True),
-        torch.rand(16, in_channels, 64, 64, requires_grad=True),
-        torch.rand(32, in_channels, 32, 32, requires_grad=True),
-        torch.rand(32, in_channels, 64, 64, requires_grad=True),
-        torch.rand(64, in_channels, 32, 32, requires_grad=True),
-        torch.rand(64, in_channels, 64, 64, requires_grad=True),
+        #torch.rand(8, in_channels, 64, 64, requires_grad=True),
+        #torch.rand(16, in_channels, 32, 32, requires_grad=True),
+        #torch.rand(16, in_channels, 64, 64, requires_grad=True),
+        #torch.rand(32, in_channels, 32, 32, requires_grad=True),
+        #torch.rand(32, in_channels, 64, 64, requires_grad=True),
+        #torch.rand(64, in_channels, 32, 32, requires_grad=True),
+        #torch.rand(64, in_channels, 64, 64, requires_grad=True),
     ]
 
-    utils.benchmark_module(torch.nn.Conv2d, inputs, args.backward,
-                           in_channels, out_channels, kernel_size)
+    log += utils.benchmark_module(torch.nn.Conv2d, inputs, args.backward,
+                                  in_channels, out_channels, kernel_size)
+
+    print(log)
