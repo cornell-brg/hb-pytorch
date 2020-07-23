be486a1845f8631bd802af341556d29d26c633e3
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-07-23 08:51:13 -0700

    Move options to the front and module to the back

--- hammerblade/torch/ubenchmarks/conv2d.py
+++ hammerblade/torch/ubenchmarks/conv2d.py
@@ -4,12 +4,12 @@ import os
 
 sys.path.append(os.path.dirname(__file__))
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'tests'))
-import utils  # noqa
+import ubench  # noqa
 
 if __name__ == "__main__":
-    args = utils.parse_args()
+    args = ubench.parse_args()
 
-    log = utils.header()
+    log = ubench.header()
 
     in_channels = 3
     out_channels = 8
@@ -28,8 +28,8 @@ if __name__ == "__main__":
         torch.rand(64, in_channels, 64, 64, requires_grad=True),
     ]
 
-    log += utils.benchmark_module(torch.nn.Conv2d, inputs, args.backward,
-                                  in_channels, out_channels, kernel_size)
+    log += ubench.benchmark_module(args.backward, inputs, torch.nn.Conv2d,
+                                   in_channels, out_channels, kernel_size)
 
     in_channels = 8
     out_channels = 16
@@ -48,7 +48,7 @@ if __name__ == "__main__":
         torch.rand(64, in_channels, 64, 64, requires_grad=True),
     ]
 
-    log += utils.benchmark_module(torch.nn.Conv2d, inputs, args.backward,
-                                  in_channels, out_channels, kernel_size)
+    log += ubench.benchmark_module(args.backward, inputs, torch.nn.Conv2d,
+                                   in_channels, out_channels, kernel_size)
 
     print(log)
