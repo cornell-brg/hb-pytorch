e2021b255f77a16aae8178f919e63c088c2698eb
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-07-22 21:06:54 -0700

    More inputs

--- hammerblade/torch/ubenchmarks/conv2d.py
+++ hammerblade/torch/ubenchmarks/conv2d.py
@@ -8,7 +8,42 @@ import utils # noqa
 if __name__ == "__main__":
     args = utils.parse_args()
 
-    inputs = torch.rand(32, 3, 32, 32, requires_grad=True)
+    in_channels = 3
+    out_channels = 8
+    kernel_size = 3
+    inputs = [
+        torch.rand(1, in_channels, 8, 8, requires_grad=True),
+        torch.rand(1, in_channels, 32, 32, requires_grad=True),
+        torch.rand(1, in_channels, 64, 64, requires_grad=True),
+        torch.rand(8, in_channels, 32, 32, requires_grad=True),
+        torch.rand(8, in_channels, 64, 64, requires_grad=True),
+        torch.rand(16, in_channels, 32, 32, requires_grad=True),
+        torch.rand(16, in_channels, 64, 64, requires_grad=True),
+        torch.rand(32, in_channels, 32, 32, requires_grad=True),
+        torch.rand(32, in_channels, 64, 64, requires_grad=True),
+        torch.rand(64, in_channels, 32, 32, requires_grad=True),
+        torch.rand(64, in_channels, 64, 64, requires_grad=True),
+    ]
 
-    utils.benchmark_module(torch.nn.Conv2d, [inputs], args.backward,
-                           3, 4, kernel_size=3)
+    utils.benchmark_module(torch.nn.Conv2d, inputs, args.backward,
+                           in_channels, out_channels, kernel_size)
+
+    in_channels = 3
+    out_channels = 16
+    kernel_size = 3
+    inputs = [
+        torch.rand(1, in_channels, 8, 8, requires_grad=True),
+        torch.rand(1, in_channels, 32, 32, requires_grad=True),
+        torch.rand(1, in_channels, 64, 64, requires_grad=True),
+        torch.rand(8, in_channels, 32, 32, requires_grad=True),
+        torch.rand(8, in_channels, 64, 64, requires_grad=True),
+        torch.rand(16, in_channels, 32, 32, requires_grad=True),
+        torch.rand(16, in_channels, 64, 64, requires_grad=True),
+        torch.rand(32, in_channels, 32, 32, requires_grad=True),
+        torch.rand(32, in_channels, 64, 64, requires_grad=True),
+        torch.rand(64, in_channels, 32, 32, requires_grad=True),
+        torch.rand(64, in_channels, 64, 64, requires_grad=True),
+    ]
+
+    utils.benchmark_module(torch.nn.Conv2d, inputs, args.backward,
+                           in_channels, out_channels, kernel_size)
