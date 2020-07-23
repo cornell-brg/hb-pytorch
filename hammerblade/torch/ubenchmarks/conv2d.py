941146b5d141c9ecf3356c1c2c7071304b26a84d
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-07-22 17:19:34 -0700

    ubenchmarking at module level

--- hammerblade/torch/ubenchmarks/conv2d.py
+++ hammerblade/torch/ubenchmarks/conv2d.py
@@ -1,28 +1,14 @@
 import torch
-import torch.nn.functional as F
-import torch.hammerblade.profiler as hbprof
-import argparse
-import utils
 import sys, os
 
+sys.path.append(os.path.dirname(__file__))
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'tests'))
-import hbutils
+import utils # noqa
 
 if __name__ == "__main__":
     args = utils.parse_args()
 
-    inputs = torch.rand(32, 16, 32, 32, requires_grad=True)
-    kernel = torch.rand(32, 16, 3, 3, requires_grad=True)
+    inputs = torch.rand(32, 3, 32, 32, requires_grad=True)
 
-    output_ref = F.conv2d(inputs, kernel)
-
-    if args.hammerblade:
-        inputs = hbutils.init_hb_tensor(inputs)
-        kernel = hbutils.init_hb_tensor(kernel)
-
-    hbprof.enable()
-    output = F.conv2d(inputs, kernel)
-    hbprof.disable()
-    print(hbprof.stats(key=['ExecTime']))
-
-    assert torch.allclose(output_ref, output.cpu())
+    utils.benchmark_module(torch.nn.Conv2d, [inputs], args.backward,
+                           3, 4, kernel_size=3)
