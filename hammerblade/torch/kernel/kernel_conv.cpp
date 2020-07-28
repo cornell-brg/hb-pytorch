725f97be962562230199f96ad109710708cef13a
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-07-28 13:32:12 -0700

    Load weights inline routine

--- hammerblade/torch/kernel/kernel_conv.cpp
+++ hammerblade/torch/kernel/kernel_conv.cpp
@@ -10,6 +10,14 @@
 const static uint32_t KhBufSize = 5;
 const static uint32_t KwBufSize = 5;
 
+inline void load_weights(float wl[KhBufSize][KwBufSize],
+                         __remote float* NOALIAS wr,
+                         uint32_t offset, uint32_t Kh, uint32_t Kw) {
+  for(int i = 0; i < Kh; ++i)
+    for(int j = 0; j < Kw; ++j)
+        wl[i][j] = wr[offset + i * Kw + j];
+}
+
 // We wrap all external-facing C++ kernels with `extern "C"` to
 // prevent name mangling
 
