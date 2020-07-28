21e474a8eb296ae3f2aaedd5328397150495f152
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-07-27 20:36:08 -0700

    Conv2d param struct

--- hammerblade/torch/kernel/kernel_conv.hpp
+++ hammerblade/torch/kernel/kernel_conv.hpp
@@ -3,5 +3,38 @@
 // 27/07/2020 Bandhav Veluri
 //====================================================================
 
-struct ConvParams {
-  
+struct Conv2dParams {
+  uint32_t N;
+  uint32_t Cout
+  uint32_t Hout
+  uint32_t Wout
+  uint32_t Cin;
+  uint32_t Hin;
+  uint32_t Win;
+  uint32_t Kh;
+  uint32_t Kw;
+  uint32_t Sh;
+  uint32_t Sw;
+  uint32_t Ph;
+  uint32_t Pw;
+
+  Conv2dParams(HBTensor<float> x,
+               HBTensor<float> y,
+               HBTensor<float> w,
+               HBVector<float> s,
+               HBVector<float> p) {
+    N = y.dim(0); // number of minibatches
+    Cout = y.dim(1); // number of output channels
+    Hout = y.dim(2);
+    Wout = y.dim(3);
+    Cin = x.dim(1); // number of input channels
+    Hin = x.dim(2);
+    Win = x.dim(3);
+    Kh = w.dim(2);
+    Kw = w.dim(3);
+    Sh = s[0];
+    Sw = s[1];
+    Ph = p[0];
+    Pw = p[1];
+  }
+};
