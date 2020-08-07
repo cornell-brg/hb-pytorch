fef16e8053ff6ba8b6591395df889d4167f7e47b
cw655 <cw655@cornell.edu>
2020-08-07 10:46:33 -0400

    add cat test

---
+++ hammerblade/torch/tests/test_cat.py
@@ -0,0 +1,29 @@
+"""
+tests of simple cat kernel.
+"""
+
+import torch
+import random
+
+def _test_torch_cat(x):
+    x_h = x.hammerblade()
+    y = torch.cat([x, x, x], 0)
+    y_h = torch.cat([x_h, x_h, x_h], 0)
+    assert y_h.device == torch.device("hammerblade")
+    assert torch.allclose(y, y_h.cpu())
+
+def test_cat_1():
+    x = torch.ones(10)
+    _test_torch_cat(x)
+
+def test_cat_2():
+    x = torch.randn(3, 4)
+    _test_torch_cat(x)
+
+def test_cat_3():
+    x = torch.randn(3, 4, 5)
+    _test_torch_cat(x)
+
+def test_cat_4():
+    x = torch.randn(2, 32, 64, 5)
+    _test_torch_cat(x)
