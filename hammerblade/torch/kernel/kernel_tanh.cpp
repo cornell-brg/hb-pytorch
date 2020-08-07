c7b5124ac1a3b2ba027224d942cef6cb6709489d
cw655 <cw655@cornell.edu>
2020-08-06 22:12:46 -0400

    tanh

---
+++ hammerblade/torch/kernel/kernel_tanh.cpp
@@ -0,0 +1,39 @@
+//====================================================================
+// tanh kernel
+//====================================================================
+// Returns a new tensor with the tangent of the elements of input
+// Used in RNN.
+//
+// Authors : Jack Weber
+// Date    : 05/07/2020
+
+#include <kernel_common.hpp>
+#include <cmath>
+
+extern "C" {
+
+//====================================================================
+// tensorlib_tanh
+//====================================================================
+// This is the tanh kernel for tensors with float elements.
+
+__attribute__ ((noinline))
+int tensorlib_tanh( hb_tensor_t* t0_p, hb_tensor_t* t1_p)
+{
+  auto res = HBTensor<float>(t0_p);
+  auto input = HBTensor<float>(t1_p);
+
+  bsg_cuda_print_stat_kernel_start();
+  hb_tiled_foreach(
+    [&](float a) {
+      return tanh(a);
+  },
+  res, input);
+
+  bsg_cuda_print_stat_kernel_end();
+  return 0;
+}
+
+HB_EMUL_REG_KERNEL(tensorlib_tanh, hb_tensor_t*, hb_tensor_t*)
+
+} /* extern C */
---
+++ hammerblade/torch/tests/test_tanh.py
@@ -0,0 +1,31 @@
+"""
+tests for tanh and tanh.out kernels
+Author : Jack Weber
+Date   : 05/26/2020
+"""
+
+
+import math
+import torch
+import torch.nn as nn
+from hypothesis import given, settings
+
+def _test_tanh(x):
+    h = x.hammerblade()
+    assert h is not x
+    out_cpu = torch.tanh(x)
+    out = torch.tanh(h)
+    assert out.is_hammerblade
+    assert torch.allclose(out.cpu(), out_cpu)
+
+def test_tanh_1():
+    a = torch.randn(10)
+    _test_tanh(a)
+
+def test_tanh_2():
+    a = torch.rand(1, 128)
+    _test_tanh(a)
+
+def test_tanh_3():
+    a = torch.rand(16, 32)
+    _test_tanh(a)
