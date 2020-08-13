5c01b60d1e6592a02689eac494fce199479549df
lc873 <lc873@cornell.edu>
2020-08-13 10:40:29 -0400

    [systolic] overload torch.vvadd to be systolic gemm

--- aten/src/ATen/native/hammerblade/Vvadd.cpp
+++ aten/src/ATen/native/hammerblade/Vvadd.cpp
@@ -6,11 +6,14 @@ namespace at { namespace native {
 
 Tensor vvadd_hb(const Tensor& self, const Tensor& other) {
 
-  // Create an output tensor that has the same shape as self
-  auto result = at::empty_like(self, self.options());
+  TORCH_CHECK(self.scalar_type() == ScalarType::Float, "HammerBlade addmm is implemented for Float only");
+  TORCH_CHECK(other.scalar_type() == ScalarType::Float, "HammerBlade addmm is implemented for Float only");
+  TORCH_CHECK(self.dim() == 2 && other.dim() == 2, "2D matrices expected, got ", self.dim(), " and ", other.dim(), " tensors");
+  TORCH_CHECK(self.size(1) == other.size(0), "Argument #3: Expected dim 0 size ", self.size(1), ", got ", other.size(0));
 
-  // Tutorial TODO:
-  // Call HB device kernel tensorlib_vvadd
+  Tensor result = at::empty({self.size(0), other.size(1)}, self.options());
+
+  hb_offload_kernel(result, self, other, "tensorlib_mm");
 
   return result;
 }
