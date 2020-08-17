57e46eea6c98870dd4a03e7add93159a7998cf86
kr397 <kr397@cornell.edu>
2020-08-17 00:29:54 -0400

    [line trace] changes for dummy vvadd

--- hammerblade/torch/kernel/kernel_vvadd.cpp
+++ hammerblade/torch/kernel/kernel_vvadd.cpp
@@ -12,29 +12,15 @@ extern "C" {
           hb_tensor_t* self_p,
           hb_tensor_t* other_p) {
 
-    // Tutorial TODO:
-    // Convert all low level pointers to Tensor objects
-    HBTensor<float> result(result_p);
-    HBTensor<float> self(self_p);
-
-    // Start profiling
-    bsg_cuda_print_stat_kernel_start();
-
-    // Use a single tile only
     if (__bsg_id == 0) {
-      // Tutorial TODO:
-      // add elements from self and other together -- put the result in result
+      for (size_t i = 0; i < result_p->N; i++){
+        ((float*)((intptr_t) result_p->data))[i] = ((float*)((intptr_t)self_p->data))[i] + 
+          ((float*)((intptr_t)other_p->data))[i];
+      }
     }
-
-    //   End profiling
-    bsg_cuda_print_stat_kernel_end();
-
-    // Sync
-    g_barrier.sync();
     return 0;
   }
-
+  
   // Register the HB kernel with emulation layer
   HB_EMUL_REG_KERNEL(tensorlib_vvadd, hb_tensor_t*, hb_tensor_t*, hb_tensor_t*)
-
 }
