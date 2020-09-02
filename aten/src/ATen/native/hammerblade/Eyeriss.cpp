f6ba2b5132729fe55678c4e71f2d20b22bc3843e
lc873 <lc873@cornell.edu>
2020-09-02 15:55:10 -0400

    [Eyeriss] working host code -- mainly copied from hb_conv

--- aten/src/ATen/native/hammerblade/Eyeriss.cpp
+++ aten/src/ATen/native/hammerblade/Eyeriss.cpp
@@ -5,15 +5,158 @@
 namespace at {
 namespace native {
 
-Tensor hb_convolution(
+// ----------------- helper functions -----------------
+
+constexpr int input_batch_size_dim = 0;  // also grad_input
+constexpr int input_channels_dim = 1;
+constexpr int output_batch_size_dim = 0;  // also grad_output
+constexpr int output_channels_dim = 1;
+constexpr int weight_output_channels_dim = 0;
+constexpr int weight_input_channels_dim = 1;
+
+// Often written as 2 + max_dim (extra dims for batch size and channels)
+constexpr int max_dim = 3;
+
+static void check_args(CheckedFrom c, IntArrayRef args, size_t expected_size, const char* arg_name)
+{
+  TORCH_CHECK(args.size() <= expected_size,
+           "Too many ", arg_name, " values (", args.size(), ") supplied, expecting ",
+           expected_size, " (while checking arguments for ", c, ")");
+  TORCH_CHECK(args.size() >= expected_size,
+           "Not enough ", arg_name, " values (", args.size(), ") supplied, expecting ",
+           expected_size, " (while checking arguments for ", c, ")");
+
+  auto num_negative_values = std::count_if(args.begin(), args.end(), [](int x){return x < 0;});
+  if (num_negative_values > 0){
+    std::stringstream ss;
+    ss << arg_name << " should be greater than zero but got (";
+    std::copy(args.begin(), args.end() - 1, std::ostream_iterator<int>(ss,", "));
+    ss << args.back() <<  ")" << " (while checking arguments for " << c << ")";
+    AT_ERROR(ss.str());
+  }
+}
+
+
+static void convolution_shape_check(
+    CheckedFrom c,
+    const TensorGeometryArg& input, const TensorGeometryArg& weight, const TensorGeometryArg& output,
+    IntArrayRef padding, IntArrayRef stride, IntArrayRef dilation, int64_t groups)
+{
+  check_args(c, padding, input->dim() - 2, "padding");
+  check_args(c, stride, padding.size(), "stride");
+  check_args(c, dilation, padding.size(), "dilation");
+
+  // Input
+  checkDimRange(c, input, 3, 6 /* exclusive */);
+  checkSize(c, input, input_channels_dim, weight->size(1) * groups);
+
+  // Weight
+  checkSameDim(c, input, weight);
+
+  // TODO: check that output->size() matches output_sizes
+  // TODO: check that weight matches output->sizes()
+  checkSameDim(c, input, output);
+}
+
+
+static void hb_convolution_arg_check(
+    int64_t output_dims, IntArrayRef dilation, int64_t groups) {
+  // Dimension check
+  TORCH_CHECK(output_dims == 4, "Only 2d convolution supported now.");
+
+  // Dilation check
+  bool dilation_check = true;
+  for(auto d : dilation) {
+    if(d != 1) {
+      TORCH_WARN("dilation[i] = ", d);
+      dilation_check = false;
+      break;
+    }
+  }
+  TORCH_CHECK(dilation_check,
+        "dilation = ", dilation,
+        " is not supported by HB yet.",
+        " Make sure dilation is all ones.");
+
+  // Groups check
+  TORCH_CHECK(groups == 1,
+      "Grouped convolution not supported by HB yet."
+      " Make sure groups = 1.");
+}
+
+
+static std::vector<int64_t> conv_output_size(
+    IntArrayRef input_size, IntArrayRef weight_size,
+    IntArrayRef padding, IntArrayRef stride, IntArrayRef dilation,
+    int64_t groups) {
+  // ASSERT(input_size.size() > 2)
+  // ASSERT(input_size.size() == weight_size.size())
+  auto dim = input_size.size();
+  std::vector<int64_t> output_size(dim);
+  output_size[0] = input_size[input_batch_size_dim];
+  output_size[1] = weight_size[weight_output_channels_dim];
+  for (size_t d = 2; d < dim; ++d) {
+    auto kernel = dilation[d - 2] * (weight_size[d] - 1) + 1;
+    output_size[d] = (input_size[d] + (2 * padding[d - 2])
+                        - kernel) / stride[d - 2] + 1;
+  }
+  return output_size;
+}
+
+// ----------------- end of helper functions -----------------
+
+Tensor eyeriss_convolution_forward(
+    CheckedFrom c,
+    const TensorArg& input, const TensorArg& weight,
+    IntArrayRef padding, IntArrayRef stride, IntArrayRef dilation, 
+    int64_t groups) {
+  checkAllSameType(c, {input, weight});
+  checkAllSameHB(c, {input, weight});
+
+  auto output_t = at::empty(
+                    conv_output_size(input->sizes(), weight->sizes(),
+                                     padding, stride, dilation, groups),
+                    input->options());
+
+  if (output_t.numel() == 0) {
+    return output_t;
+  }
+
+  // Avoid ambiguity of "output" when this is being used as backwards
+  TensorArg output{ output_t, "result", 0 };
+  convolution_shape_check(c, input, weight, output, padding, stride, 
+      dilation, groups);
+
+  Tensor weight_contig = weight->contiguous();
+
+  hb_convolution_arg_check(output->dim(), dilation, groups);
+
+  std::vector<eva_t> device_args;
+  std::vector<eva_t> device_ptrs;
+  device_args.push_back(create_device_tensor(output_t, device_ptrs));
+  device_args.push_back(create_device_tensor(*input, device_ptrs));
+  device_args.push_back(create_device_tensor(*weight, device_ptrs));
+  device_args.push_back(create_device_vector(padding, true, device_ptrs));
+  device_args.push_back(create_device_vector(stride, true, device_ptrs));
+
+  c10::hammerblade::offload_kernel(
+      "tensorlib_eyeriss", device_args);
+  cleanup_device(device_args, device_ptrs);
+
+  return *output;
+}
+
+Tensor eyeriss_convolution(
     const Tensor& input_t, const Tensor& weight_t, const Tensor& bias_t,
-    IntArrayRef padding, IntArrayRef stride, 
+    IntArrayRef padding, IntArrayRef stride,
     IntArrayRef dilation, int64_t groups) {
   TensorArg input  { input_t,  "input",  1 },
             weight { weight_t, "weight", 2 },
             bias   { bias_t,   "bias",   3 };
-  CheckedFrom c = "hb_convolution";
-  return at::empty({1}, input_t->options());
+  CheckedFrom c = "eyeriss_convolution";
+  auto output_t = eyeriss_convolution_forward(
+      c, input, weight, padding, stride, dilation, groups);
+  return output_t;
 }
 
 }} // at::native
