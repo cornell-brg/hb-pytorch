7f0baa02336a1d2464f344c730958819659b16c5
lc873 <lc873@cornell.edu>
2020-10-02 12:20:55 -0400

    [tanh_backward] now it compiles

--- aten/src/ATen/native/hammerblade/TanhBackward.cpp
+++ aten/src/ATen/native/hammerblade/TanhBackward.cpp
@@ -5,15 +5,12 @@
 #include <ATen/native/hammerblade/Offload.h>
 
 namespace at { namespace native {
-namespace {
 
-void tanh_backward_hb(const Tensor & grad_output, const Tensor & output) {
+Tensor tanh_backward_hb(const Tensor & grad_output, const Tensor & output) {
    // AT_DISPATCH_FLOAT_TYPE_ONLY(grad_output.dtype(), output.dtype(), "tanh_backward_hb", [&]() {
         hb_offload_kernel(grad_output, output, "tensorlib_tanh_backward");
      //   });
 }
-
-} // anonymous namespace
 
 //REGISTER_HAMMERBLADE_DISPATCH(tanh_backward_stub, &tanh_backward_kernel_hb);
 //
