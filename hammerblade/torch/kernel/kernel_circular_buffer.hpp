e7ca96c21eabb751805bd38ae34e34d86ef26dc0
lc873 <lc873@cornell.edu>
2020-10-16 11:00:06 -0400

    [circular buffer] we dont need this->

--- hammerblade/torch/kernel/kernel_circular_buffer.hpp
+++ hammerblade/torch/kernel/kernel_circular_buffer.hpp
@@ -31,28 +31,28 @@ namespace CircularBuffer{
       FIFO<T, N, DEPTH>(unsigned int prv_y, unsigned int prv_x, unsigned int nxt_y, unsigned int nxt_x){
         // I know this is super confusing ...
         // But trust me -- this is what it should look like
-        this->occupancy_prv_r = reinterpret_cast<volatile unsigned int*>(bsg_tile_group_remote_pointer(prv_x,prv_y,this->occupancy_nxt));
-        this->occupancy_nxt_r = reinterpret_cast<volatile unsigned int*>(bsg_tile_group_remote_pointer(nxt_x,nxt_y,this->occupancy));
-        this->buffer_remote = reinterpret_cast<T*>(bsg_tile_group_remote_pointer(nxt_x,nxt_y,this->buffer));
+        occupancy_prv_r = reinterpret_cast<volatile unsigned int*>(bsg_tile_group_remote_pointer(prv_x,prv_y,occupancy_nxt));
+        occupancy_nxt_r = reinterpret_cast<volatile unsigned int*>(bsg_tile_group_remote_pointer(nxt_x,nxt_y,occupancy));
+        buffer_remote = reinterpret_cast<T*>(bsg_tile_group_remote_pointer(nxt_x,nxt_y,buffer));
       }
 
       __attribute__((always_inline))
       T *obtain_wr_ptr(){
         // bsg_print_hexadecimal(0xFACEB00C);
-        volatile unsigned int *o = &(this->occupancy_nxt[this->occ_idx_nxt]);
+        volatile unsigned int *o = &(occupancy_nxt[occ_idx_nxt]);
         bsg_wait_local(reinterpret_cast<int *> (const_cast<unsigned int*> (o)), 0);
         // bsg_print_hexadecimal(0xFACEB00D);
-        return this->buffer_remote + this->occ_idx_nxt * N;
+        return buffer_remote + occ_idx_nxt * N;
       }
 
       __attribute__((always_inline))
       int finish_wr_ptr(){
-        volatile unsigned int *o   = &(this->occupancy_nxt[this->occ_idx_nxt]);
-        volatile unsigned int *o_r = this->occupancy_nxt_r + this->occ_idx_nxt;
+        volatile unsigned int *o   = &(occupancy_nxt[occ_idx_nxt]);
+        volatile unsigned int *o_r = occupancy_nxt_r + occ_idx_nxt;
         asm volatile("": : :"memory");
         *o   = 1;
         *o_r = 1;
-        this->occ_idx_nxt = (this->occ_idx_nxt + 1) % DEPTH;
+        occ_idx_nxt = (occ_idx_nxt + 1) % DEPTH;
         // bsg_print_hexadecimal(0xBEEFBEEF);
         return 0;
       }
@@ -60,27 +60,27 @@ namespace CircularBuffer{
       __attribute__((always_inline))
       T *obtain_rd_ptr(){
         // bsg_print_hexadecimal(0xFACE);
-        volatile unsigned int *o = &(this->occupancy[this->occ_idx]);
+        volatile unsigned int *o = &(occupancy[occ_idx]);
         bsg_wait_local(reinterpret_cast<int *> (const_cast<unsigned int*> (o)), 1);
         // bsg_print_hexadecimal(0xFACF);
-        return this->buffer + this->occ_idx * N;
+        return buffer + occ_idx * N;
       }
 
       __attribute__((always_inline))
       int finish_rd_ptr(){
-        volatile unsigned int *o   = &(this->occupancy[this->occ_idx]);
-        volatile unsigned int *o_r = this->occupancy_prv_r + this->occ_idx;
+        volatile unsigned int *o   = &(occupancy[occ_idx]);
+        volatile unsigned int *o_r = occupancy_prv_r + occ_idx;
         asm volatile("": : :"memory");
         *o   = 0;
         *o_r = 0;
-        this->occ_idx = (this->occ_idx + 1) % DEPTH;
+        occ_idx = (occ_idx + 1) % DEPTH;
         // bsg_print_hexadecimal(0xBEEF);
         return 0;
       }
 
       __attribute__((always_inline))
       T* get_buffer(){
-        return this->buffer;
+        return buffer;
       }
   };
 
