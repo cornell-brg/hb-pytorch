e89e709199dc8133b9f37759b466d790c6c21f6d
lc873 <lc873@cornell.edu>
2020-10-22 19:59:50 -0400

    [embedding] per word locks

--- hammerblade/torch/kernel/kernel_embedding_backward.cpp
+++ hammerblade/torch/kernel/kernel_embedding_backward.cpp
@@ -11,13 +11,15 @@ extern "C" {
           hb_tensor_t* grad_weight_p,
           hb_tensor_t* grad_p,
           hb_tensor_t* index_p,
+          hb_tensor_t* locks_p,
           int32_t* padding_idx_p,
           int32_t* num_weights_p,
           int32_t* numel_p) {
 
     HBTensor<float> grad_weight(grad_weight_p);
     HBTensor<float> grad(grad_p);
-    HBTensor<int32_t> index(index_p);
+    HBTensor<int64_t> index(index_p);
+    HBTensor<int32_t> locks(locks_p);
     int32_t padding_idx = *padding_idx_p;
     int32_t num_weights = *num_weights_p;
     int32_t numel = *numel_p;
@@ -27,28 +29,36 @@ extern "C" {
     // indices_numel = N
     // num_weights = K
 
-    bsg_attr_remote int32_t* index_data = (bsg_attr_remote int32_t*)index.data_ptr();
     bsg_attr_remote float* grad_weight_data = (bsg_attr_remote float*)grad_weight.data_ptr();
     bsg_attr_remote float* grad_data = (bsg_attr_remote float*)grad.data_ptr();
-    uint32_t grad_weight_s0 = grad_weight.get_strides()[0];
-    uint32_t grad_s0 = grad.get_strides()[0];
-    uint32_t grad_s1 = grad.get_strides()[1];
+    bsg_attr_remote int* locks_data = (bsg_attr_remote int*)locks.data_ptr();
+    const bsg_attr_remote int64_t* index_data = (bsg_attr_remote int64_t*)index.data_ptr();
+    const uint32_t grad_weight_s0 = grad_weight.get_strides()[0];
+    const uint32_t grad_s0 = grad.get_strides()[0];
+    const uint32_t grad_s1 = grad.get_strides()[1];
 
     bsg_cuda_print_stat_kernel_start();
     bsg_saif_start();
 
     for (size_t idx = bsg_id; idx < indices_numel; idx += (BSG_TILE_GROUP_X_DIM * BSG_TILE_GROUP_Y_DIM)) {
       if (idx < indices_numel) {
-        int32_t offset = *(index_data + idx);
+        const int32_t offset = (int32_t)*(index_data + idx);
         if (offset != padding_idx) {
           bsg_attr_remote float* dst = grad_weight_data + offset * grad_weight_s0;
           bsg_attr_remote float* src = grad_data + idx * grad_s0;
+          bsg_print_hexadecimal(0xbeefbeef);
+          int* lock_addr = locks_data + offset;
+          // acquire the lock
+          while(bsg_amoswap_aq(lock_addr, 1)!=0);
+          bsg_print_hexadecimal(0xbeef0000);
           bsg_unroll(16)
           for (size_t i = 0; i < numel; i++) {
             *dst += *src;
             dst++;
             src += grad_s1;
           }
+          bsg_amoswap_aq(lock_addr, 0);
+          bsg_print_hexadecimal(0xfaceface);
         }
       }
     }
