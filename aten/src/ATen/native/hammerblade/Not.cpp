44abe459c66c0769b1072bd539c03d79a1f158a9
cw655 <cw655@cornell.edu>
2020-10-22 14:07:11 -0400

    not

--- aten/src/ATen/native/hammerblade/Not.cpp
+++ aten/src/ATen/native/hammerblade/Not.cpp
@@ -1,18 +1,20 @@
-#include <cmath>
 #include <ATen/Dispatch.h>
 #include <ATen/hammerblade/HammerBladeContext.h>
 #include <ATen/native/TensorIterator.h>
-#include <ATen/native/ReduceOps.h>
+#include <ATen/native/UnaryOps.h>
 #include <ATen/native/hammerblade/Offload.h>
 
 namespace at { namespace native {
 
-Tensor not_kernel_hb(const Tensor& self) {
-  // TORCH_CHECK(self.scalar_type() == ScalarType::Int || self.scalar_type() == ScalarType::Bool, "HammerBlade or is implemented for Int and Bool only");
-  // TORCH_CHECK(other.scalar_type() == ScalarType::Int || other.scalar_type() == ScalarType::Bool, "HammerBlade or is implemented for Int and Bool only");
-  Tensor result = at::empty_like(self, self.options());
-  hb_offload_kernel(result, self, "tensorlib_not");
-  return result;
+namespace {
+
+void not_kernel_hb(TensorIterator& iter) {
+  AT_DISPATCH_INTS_ONLY(iter.dtype(), "not_hb", [&]() {
+    std::cout << "host code dispatching";
+    offload_op_unary(iter, "tensorlib_not");
+  });
 }
 
+} // anonymous namespace
+REGISTER_HAMMERBLADE_DISPATCH(bitwise_not_stub, &not_kernel_hb);
 }} // namespace at::native
--- hammerblade/torch/kernel/kernel_not.cpp
+++ hammerblade/torch/kernel/kernel_not.cpp
@@ -4,22 +4,24 @@
 //
 // Authors : Janice Wei
 // Date    : 10/08/2020
-
 #include <kernel_common.hpp>
 #include <cstdint>
+#include <cmath>
 
 extern "C" {
 
   __attribute__ ((noinline))  int tensorlib_not(
           hb_tensor_t* t0_p,
           hb_tensor_t* t1_p) {
+    std::cout << "device code start";
     auto res = HBTensor<int>(t0_p);
     auto input = HBTensor<int>(t1_p);
-
+    std::cout << "bsg_cuda_start";
     bsg_cuda_print_stat_kernel_start();
 
     hb_tiled_foreach(
       [](int a) {
+        std::cout << ~a;
         return ~a;
       },
       res, input);
