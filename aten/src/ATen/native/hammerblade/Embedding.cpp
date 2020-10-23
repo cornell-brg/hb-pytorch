a359bf3451dd3b47eee2c2ef7367a4174ccfd921
lc873 <lc873@cornell.edu>
2020-10-22 22:40:33 -0400

    [embedding back] makes embedding back less confusing

--- aten/src/ATen/native/hammerblade/Embedding.cpp
+++ aten/src/ATen/native/hammerblade/Embedding.cpp
@@ -8,7 +8,7 @@ namespace at {
 namespace native {
 
 Tensor embedding_dense_backward_hb(
-    const Tensor & grad_, const Tensor & indices, int64_t num_weights,
+    const Tensor & grad, const Tensor & indices, int64_t num_weights,
     int64_t padding_idx, bool scale_grad_by_freq) {
 
   TORCH_CHECK( !scale_grad_by_freq, "scale_grad_by_freq not yet supported on HB");
@@ -18,7 +18,6 @@ Tensor embedding_dense_backward_hb(
   checkScalarType("embedding_backward", indices_arg, kLong);
 
   int64_t numel = indices.numel();
-  auto indices_contig = indices;
   auto grad_weight = at::zeros({num_weights, grad_.size(-1)}, grad_.options());
   auto locks = at::zeros({10678}, grad_.options());
 
@@ -26,7 +25,7 @@ Tensor embedding_dense_backward_hb(
   int32_t num_weights_i32 = safe_downcast<int32_t, int64_t>(num_weights);
   int32_t output_numel_i32 = safe_downcast<int32_t, int64_t>(grad_.size(-1));
 
-  hb_offload_kernel(grad_weight, grad_, indices_contig, locks, padding_idx_i32,
+  hb_offload_kernel(grad_weight, grad, indices, locks, padding_idx_i32,
                     num_weights_i32, output_numel_i32,
                     "tensorlib_embedding_backward");
 
