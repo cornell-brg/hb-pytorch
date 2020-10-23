40d24f13478e1725ca15dc783ed8906b299e98de
lc873 <lc873@cornell.edu>
2020-10-22 22:37:17 -0400

    [embedding back] hacky? maybe ... removed the contiguous part

--- aten/src/ATen/native/hammerblade/Embedding.cpp
+++ aten/src/ATen/native/hammerblade/Embedding.cpp
@@ -12,13 +12,13 @@ Tensor embedding_dense_backward_hb(
     int64_t padding_idx, bool scale_grad_by_freq) {
 
   TORCH_CHECK( !scale_grad_by_freq, "scale_grad_by_freq not yet supported on HB");
+  TORCH_CHECK( indices.is_contiguous(), "assuming indices to be contiguous" );
 
   auto indices_arg = TensorArg(indices, "indices", 2);
   checkScalarType("embedding_backward", indices_arg, kLong);
 
   int64_t numel = indices.numel();
   auto indices_contig = indices;
-  auto grad = grad_.view({numel, grad_.size(-1)});
   auto grad_weight = at::zeros({num_weights, grad_.size(-1)}, grad_.options());
   auto locks = at::zeros({10678}, grad_.options());
 
@@ -26,7 +26,7 @@ Tensor embedding_dense_backward_hb(
   int32_t num_weights_i32 = safe_downcast<int32_t, int64_t>(num_weights);
   int32_t output_numel_i32 = safe_downcast<int32_t, int64_t>(grad_.size(-1));
 
-  hb_offload_kernel(grad_weight, grad, indices_contig, locks, padding_idx_i32,
+  hb_offload_kernel(grad_weight, grad_, indices_contig, locks, padding_idx_i32,
                     num_weights_i32, output_numel_i32,
                     "tensorlib_embedding_backward");
 
--- hammerblade/torch/kernel/kernel_embedding_backward.cpp
+++ hammerblade/torch/kernel/kernel_embedding_backward.cpp
@@ -36,6 +36,9 @@ extern "C" {
     const uint32_t grad_weight_s0 = grad_weight.get_strides()[0];
     const uint32_t grad_s0 = grad.get_strides()[0];
     const uint32_t grad_s1 = grad.get_strides()[1];
+    const uint32_t grad_s2 = grad.get_strides()[2];
+    const uint32_t batch_size = index.dim(0);
+    const uint32_t embeddings_per_batch = indices_numel / batch_size;
 
     bsg_cuda_print_stat_kernel_start();
     bsg_saif_start();
@@ -44,21 +47,23 @@ extern "C" {
       if (idx < indices_numel) {
         const int32_t offset = (int32_t)*(index_data + idx);
         if (offset != padding_idx) {
+          const int32_t batch_id = idx / embeddings_per_batch;
+          const int32_t embedding_id = idx % embeddings_per_batch;
           bsg_attr_remote float* dst = grad_weight_data + offset * grad_weight_s0;
-          bsg_attr_remote float* src = grad_data + idx * grad_s0;
-          bsg_print_hexadecimal(0xbeefbeef);
+          bsg_attr_remote float* src = grad_data + batch_id * grad_s0 + embedding_id * grad_s1;
+          //bsg_print_hexadecimal(0xbeefbeef);
           int* lock_addr = locks_data + offset;
           // acquire the lock
           while(bsg_amoswap_aq(lock_addr, 1)!=0);
-          bsg_print_hexadecimal(0xbeef0000);
+          //bsg_print_hexadecimal(0xbeef0000);
           bsg_unroll(16)
           for (size_t i = 0; i < numel; i++) {
             *dst += *src;
             dst++;
-            src += grad_s1;
+            src += grad_s2;
           }
           bsg_amoswap_rl(lock_addr, 0);
-          bsg_print_hexadecimal(0xfaceface);
+          //bsg_print_hexadecimal(0xfaceface);
         }
       }
     }
