79b7ce312629849b4afeb47b52cd4d206255ba72
Zhongyuan Zhao <zz546@zhang-21.ece.cornell.edu>
2020-10-27 12:04:34 -0400

    clean up all the temp files

--- hammerblade/scripts/tests/lgc-ista-newblade-c2sropt-tag/lgc_ista_hb_10/run.sh
+++
@@ -1,1 +0,0 @@
-(cd lgc_ista_hb_10; pycosim /scratch/users/zz546/hb-pytorch/hammerblade/torch/tests/profiler/test_lgc_ista_spmv_profile_route.py > out.std 2>&1)
--- hammerblade/scripts/tests/lgc-ista-newblade-c2sropt-tag/lgc_ista_xeon_10_2/run.sh
+++
@@ -1,1 +0,0 @@
-(cd lgc_ista_xeon_10_2; python /scratch/users/zz546/hb-pytorch/hammerblade/torch/tests/profiler/test_lgc_ista_spmv_profile_route.py > out.std 2>&1)
