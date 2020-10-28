ed2da50526d72802a194317804385c8855576ca4
lc873 <lc873@cornell.edu>
2020-10-28 17:59:16 -0400

    [maxpool] now each tile do one row at a time

--- hammerblade/torch/kernel/kernel_pooling.cpp
+++ hammerblade/torch/kernel/kernel_pooling.cpp
@@ -45,7 +45,7 @@ extern "C" {
     hb_assert(STRIDE_Y == Sh);
     hb_assert(STRIDE_X == Sw);
 
-    size_t num_blocks = Hout * Wout * C * N;
+    size_t num_blocks = Hout * C * N;
 
     bsg_attr_remote float* input_base = (float*)x.data_ptr();
     bsg_attr_remote float* output_base = (float*)y.data_ptr();
@@ -61,51 +61,57 @@ extern "C" {
       if (idx < num_blocks) {
 
         // calculate which output we need to produce -- there is no reuse so ...
-        size_t image_id = idx / (Hout * Wout * C);
-        size_t tmp = idx % (Hout * Wout * C);
-        size_t channel_id = tmp / (Hout * Wout);
-        tmp = tmp % (Hout * Wout);
-        size_t yy = tmp / Wout;
-        size_t xx = tmp % Wout;
+        size_t image_id = idx / (Hout * C);
+        size_t tmp = idx % (Hout * C);
+        size_t channel_id = tmp / Hout;
+        size_t yy = tmp % Hout;
 
         // calculate where to find the input data and where to write the output
-        size_t index_offset = yy * STRIDE_Y * input_strides[2] + xx * STRIDE_X * input_strides[3];
+        size_t index_offset = yy * STRIDE_Y * input_strides[2];
         size_t offset = image_id * input_strides[0] + channel_id * input_strides[1] + index_offset;
         size_t output_offset = image_id * output_strides[0] + channel_id * output_strides[1]
-                               + yy * output_strides[2] + xx * output_strides[3];
+                               + yy * output_strides[2];
 
         bsg_attr_remote float* input_ptr = input_base + offset;
         bsg_attr_remote float* output_ptr = output_base + output_offset;
         bsg_attr_remote int* ind_ptr = ind_base + output_offset;
 
-        // fully unroll as we know kernel is 2x2
-        float max = std::numeric_limits<float>::lowest();
-        int index;
-        register float tmp0 = *(input_ptr + 0);
-        register float tmp1 = *(input_ptr + 1);
-        register float tmp2 = *(input_ptr + input_strides[2] + 0);
-        register float tmp3 = *(input_ptr + input_strides[2] + 1);
-        asm volatile("": : :"memory");
+        bsg_unroll(6)
+        for (size_t xx = 0; xx < Wout; xx++) {
+          // fully unroll as we know kernel is 2x2
+          float max = std::numeric_limits<float>::lowest();
+          int index;
+          register float tmp0 = *(input_ptr + 0);
+          register float tmp1 = *(input_ptr + 1);
+          register float tmp2 = *(input_ptr + input_strides[2] + 0);
+          register float tmp3 = *(input_ptr + input_strides[2] + 1);
+          asm volatile("": : :"memory");
 
-        if (tmp0 > max) {
-          max = tmp0;
-          index = index_offset;
-        }
-        if (tmp1 > max) {
-          max = tmp1;
-          index = index_offset + 1;
-        }
-        if (tmp2 > max) {
-          max = tmp2;
-          index = index_offset + input_strides[2];
-        }
-        if (tmp3 > max) {
-          max = tmp3;
-          index = index_offset + input_strides[2] + 1;
-        }
+          if (tmp0 > max) {
+            max = tmp0;
+            index = index_offset;
+          }
+          if (tmp1 > max) {
+            max = tmp1;
+            index = index_offset + 1;
+          }
+          if (tmp2 > max) {
+            max = tmp2;
+            index = index_offset + input_strides[2];
+          }
+          if (tmp3 > max) {
+            max = tmp3;
+            index = index_offset + input_strides[2] + 1;
+          }
 
-        *output_ptr = max;
-        *ind_ptr = index;
+          *output_ptr = max;
+          *ind_ptr = index;
+
+          ind_ptr++;
+          output_ptr++;
+          input_ptr += Sw;
+          index_offset += Sw;
+        }
       }
     }
 
@@ -142,32 +148,75 @@ extern "C" {
     auto Ph = *padH;
     auto Pw = *padW;
 
+    hb_assert(KERNEL_DIM == Kh);
+    hb_assert(KERNEL_DIM == Kw);
+    hb_assert(STRIDE_Y == Sh);
+    hb_assert(STRIDE_X == Sw);
+
+    size_t num_blocks = Hout * C * N;
+
+    bsg_attr_remote float* input_base = (float*)x.data_ptr();
+    bsg_attr_remote float* output_base = (float*)y.data_ptr();
+    bsg_attr_remote int* ind_base = (int*)ind.data_ptr();
+    const uint32_t* input_strides = x.get_strides();
+    const uint32_t* output_strides = y.get_strides();
+
     // Start profiling
     bsg_cuda_print_stat_kernel_start();
 
-    hb_tiled_foreach([]() {return 0.0;}, x);
-    g_barrier.sync();
+    // main loop
+    for (size_t idx = bsg_id; idx < num_blocks; idx += (BSG_TILE_GROUP_X_DIM * BSG_TILE_GROUP_Y_DIM)) {
+      if (idx < num_blocks) {
 
-    hb_tiled_for(bsg_tiles_X * bsg_tiles_Y,
-                 [&](size_t n, size_t c, size_t xh, size_t xw) {
-      for(uint32_t kh = 0; kh < Kh; ++kh)
-        for(uint32_t kw = 0; kw < Kw; ++kw) {
-          uint32_t rel_h = xh - kh + Ph;
-          uint32_t rel_w = xw - kw + Pw;
+        // calculate which output we need to produce -- each block is a row in grad_output
+        size_t image_id = idx / (Hout * C);
+        size_t tmp = idx % (Hout * C);
+        size_t channel_id = tmp / Hout;
+        size_t yy = tmp % Hout;
 
-          if((rel_h % Sh != 0) || (rel_w % Sw != 0))
-            continue;
+        // calculate where to find the input data and where to write the output
+        size_t index_offset = yy * STRIDE_Y * input_strides[2];
+        size_t offset = image_id * input_strides[0] + channel_id * input_strides[1] + index_offset;
+        size_t output_offset = image_id * output_strides[0] + channel_id * output_strides[1]
+                               + yy * output_strides[2];
 
-          uint32_t yh = rel_h / Sh;
-          uint32_t yw = rel_w / Sw;
+        bsg_attr_remote float* input_ptr = input_base + offset;
+        bsg_attr_remote float* output_ptr = output_base + output_offset;
+        bsg_attr_remote int* ind_ptr = ind_base + output_offset;
 
-          if(yh >= 0 && yh < Hout && yw >= 0 && yw < Wout
-             && xh == ind(n, c, yh, yw) / Win
-             && xw == ind(n, c, yh, yw) % Win) {
-            x(n, c, xh, xw) += y(n, c, yh, yw);
-          } // else 0
+        bsg_unroll(6)
+        for (size_t xx = 0; xx < Wout; xx++) {
+          int index = *ind_ptr;
+          float grad = *output_ptr;
+
+          // fully unroll as we know kernel is 2x2
+          register float tmp0 = 0;
+          register float tmp1 = 0;
+          register float tmp2 = 0;
+          register float tmp3 = 0;
+
+          if (index == index_offset) {
+            tmp0 = grad;
+          } else if (index == index_offset + 1) {
+            tmp1 = grad;
+          } else if (index == index_offset + input_strides[2]) {
+            tmp2 = grad;
+          } else {
+            tmp3 = grad;
+          }
+
+          *(input_ptr + 0) = tmp0;
+          *(input_ptr + 1) = tmp1;
+          *(input_ptr + input_strides[2] + 0) = tmp2;
+          *(input_ptr + input_strides[2] + 1) = tmp3;
+
+          ind_ptr++;
+          output_ptr++;
+          input_ptr += Sw;
+          index_offset += Sw;
         }
-    }, N, C, Hin, Win);
+      }
+    }
 
     // End profiling
     bsg_cuda_print_stat_kernel_end();
