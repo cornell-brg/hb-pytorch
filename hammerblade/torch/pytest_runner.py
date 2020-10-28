e3d4ad34db3fc8c60f8c41f215481f4459169c83
Zhongyuan Zhao <zz546@zhang-21.ece.cornell.edu>
2020-10-28 18:45:13 -0400

    remove sinkhorn temp files and riscv kernel temp objective files

--- hammerblade/torch/pytest_runner.py
+++ hammerblade/torch/pytest_runner.py
@@ -22,7 +22,7 @@ import pytest
 current_path = str(pathlib.Path(__file__).parent.absolute())
 
 # add current and tests paths
-sys.path.append(current_path + "/tests/temp/")
+sys.path.append(current_path + "/tests/")
 sys.path.append(current_path)
 
 # construct target list
@@ -31,12 +31,12 @@ targets = []
 # Get test list from the command line if provided
 if len(sys.argv) > 1:
     for t in sys.argv[1:]:
-        targets.append(current_path + "/tests/temp/" + t)
+        targets.append(current_path + "/tests/" + t)
 else:
     # collect pytest files. somehow it can't do so automatically
     # inside COSIM
     import glob
-    targets = glob.glob(current_path + "/tests/temp/test_*.py")
+    targets = glob.glob(current_path + "/tests/test_*.py")
 
 targets.sort()
 
