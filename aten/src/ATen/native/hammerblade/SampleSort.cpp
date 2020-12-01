11cd3d2cb53972ef784ee607382a152794f4fa20
Neil Adit <neiladit@gmail.com>
2020-12-01 02:05:18 -0500

    adds Sample sort host code to dispatch kernel on device

---
+++ aten/src/ATen/native/hammerblade/SampleSort.cpp
@@ -0,0 +1,24 @@
+#include <ATen/Dispatch.h>
+#include <ATen/hammerblade/HammerBladeContext.h>
+#include <ATen/native/TensorIterator.h>
+#include <ATen/native/hammerblade/Offload.h>
+
+namespace at {
+namespace native {
+
+Tensor sample_sort_hb(const Tensor &inp, const int64_t nproc, const int64_t sr) {
+    
+    auto sorted_ = at::zeros(inp.sizes(),inp.options());
+    auto sample_keys = at::zeros(nproc*sr,inp.options());
+    auto sorted_keys = at::zeros(nproc*sr,inp.options());
+    auto splitters = at::zeros(nproc,inp.options());
+    auto thread_buckets = at::zeros(nproc,inp.options());
+    int32_t nproc_i32 = safe_downcast<int32_t, int64_t>(nproc);
+    int32_t sr_i32 = safe_downcast<int32_t, int64_t>(sr);
+    
+    hb_offload_kernel(sorted_,inp,sample_keys,sorted_keys,splitters,thread_buckets,nproc_i32,sr_i32,"tensorlib_samplesort");
+
+    return sorted_;
+}
+
+}} // namespace at::native
