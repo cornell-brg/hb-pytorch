b7fc39c29f7c6bef76fcf4df28294f52d7d23396
lc873 <lc873@cornell.edu>
2021-01-06 16:28:40 -0500

    [upsampling] backward 1 thread

--- aten/src/ATen/native/hammerblade/UpSampleNearest1d.cpp
+++ aten/src/ATen/native/hammerblade/UpSampleNearest1d.cpp
@@ -7,92 +7,6 @@
 namespace at { namespace native {
 namespace {
 
-/*
-template <typename scalar_t>
-static void upsample_nearest1d_out_frame(
-    scalar_t* odata,
-    scalar_t* idata,
-    int64_t input_width,
-    int64_t output_width,
-    int64_t nbatch,
-    int64_t channels) {
-  const float scale = (float)input_width / (float)output_width;
-  channels = channels * nbatch;
-
-  // special case: just copy
-  if (input_width == output_width) {
-    for (int64_t w2 = 0; w2 < output_width; ++w2) {
-      const int64_t w1 = w2;
-      const scalar_t* pos1 = &idata[w1];
-      scalar_t* pos2 = &odata[w2];
-
-      for (int64_t c = 0; c < channels; ++c) {
-        pos2[0] = pos1[0];
-        pos1 += input_width;
-        pos2 += output_width;
-      }
-    }
-    return;
-  }
-
-  for (int64_t w2 = 0; w2 < output_width; ++w2) {
-    const scalar_t src_x =
-        nearest_neighbor_compute_source_index(scale, w2, input_width);
-    const int64_t w1 = src_x;
-    const scalar_t* pos1 = &idata[w1];
-    scalar_t* pos2 = &odata[w2];
-
-    for (int64_t c = 0; c < channels; ++c) {
-      pos2[0] = pos1[0];
-      pos1 += input_width;
-      pos2 += output_width;
-    }
-  }
-}
-*/
-
-template <typename scalar_t>
-static void upsample_nearest1d_backward_out_frame(
-    scalar_t* odata,
-    scalar_t* idata,
-    int64_t input_width,
-    int64_t output_width,
-    int64_t nbatch,
-    int64_t channels) {
-  const float scale = (float)input_width / (float)output_width;
-  channels = channels * nbatch;
-
-  // special case: same-size matching grids
-  if (input_width == output_width) {
-    for (int64_t w2 = 0; w2 < output_width; ++w2) {
-      const int64_t w1 = w2;
-      scalar_t* pos1 = &idata[w1];
-      const scalar_t* pos2 = &odata[w2];
-
-      for (int64_t c = 0; c < channels; ++c) {
-        pos1[0] += pos2[0];
-        pos1 += input_width;
-        pos2 += output_width;
-      }
-    }
-    return;
-  }
-
-  for (int64_t w2 = 0; w2 < output_width; ++w2) {
-    const int64_t w1 =
-        nearest_neighbor_compute_source_index(scale, w2, input_width);
-
-    scalar_t* pos1 = &idata[w1];
-    const scalar_t* pos2 = &odata[w2];
-
-    for (int64_t c = 0; c < channels; ++c) {
-      pos1[0] += pos2[0];
-      pos1 += input_width;
-      pos2 += output_width;
-    }
-  }
-}
-
 static void upsample_nearest1d_out_hb_template(
     Tensor& output,
     const Tensor& input_,
@@ -150,11 +64,10 @@ static void upsample_nearest1d_backward_out_hb_template(
       "It is expected input_size equals to 3, but got size ",
       input_size.size());
 
-  int64_t output_width = output_size[0];
-
-  int64_t nbatch = input_size[0];
-  int64_t channels = input_size[1];
-  int64_t input_width = input_size[2];
+  int32_t output_width = safe_downcast<int32_t, int64_t>(output_size[0]);
+  int32_t nbatch       = safe_downcast<int32_t, int64_t>(input_size[0]);
+  int32_t channels     = safe_downcast<int32_t, int64_t>(input_size[1]);
+  int32_t input_width  = safe_downcast<int32_t, int64_t>(input_size[2]);
 
   upsample_1d_shape_check(
       Tensor(),
@@ -171,16 +84,14 @@ static void upsample_nearest1d_backward_out_hb_template(
 
   AT_DISPATCH_FLOAT_TYPE_ONLY(
       grad_output.scalar_type(), "upsample_nearest1d_backward", [&] {
-        scalar_t* idata = grad_input.data_ptr<scalar_t>();
-        scalar_t* odata = grad_output.data_ptr<scalar_t>();
-
-        upsample_nearest1d_backward_out_frame<scalar_t>(
-            odata,
-            idata,
+        hb_offload_kernel(
+            grad_output,
+            grad_input,
             input_width,
             output_width,
             nbatch,
-            channels);
+            channels,
+            "tensorlib_upsample_nearest1d_back");
       });
 }
 } // namespace
--- hammerblade/torch/kernel/kernel_upsample_nearest1d.cpp
+++ hammerblade/torch/kernel/kernel_upsample_nearest1d.cpp
@@ -75,4 +75,62 @@ int tensorlib_upsample_nearest1d(
 HB_EMUL_REG_KERNEL(tensorlib_upsample_nearest1d, hb_tensor_t*, hb_tensor_t*,
                    int32_t*, int32_t*, int32_t*, int32_t*)
 
+__attribute__ ((noinline))
+int tensorlib_upsample_nearest1d_back(
+    hb_tensor_t* output_p,
+    hb_tensor_t* input_p,
+    int32_t*     input_width_p,
+    int32_t*     output_width_p,
+    int32_t*     nbatch_p,
+    int32_t*     channels_p ) {
+
+  HBTensor<float> output(output_p);
+  HBTensor<float> input(input_p);
+  int32_t         input_width = *input_width_p;
+  int32_t         output_width = *output_width_p;
+  int32_t         nbatch = *nbatch_p;
+  int32_t         channels = *channels_p;
+
+  float* idata = (float*)input.data_ptr();
+  float* odata = (float*)output.data_ptr();
+
+  const float scale = (float)input_width / (float)output_width;
+  channels = channels * nbatch;
+
+  // special case: same-size matching grids
+  if (input_width == output_width) {
+    for (int32_t w2 = 0; w2 < output_width; ++w2) {
+      const int32_t w1 = w2;
+      float* pos1 = &idata[w1];
+      const float* pos2 = &odata[w2];
+
+      for (int32_t c = 0; c < channels; ++c) {
+        pos1[0] += pos2[0];
+        pos1 += input_width;
+        pos2 += output_width;
+      }
+    }
+    return 0;
+  }
+
+  for (int32_t w2 = 0; w2 < output_width; ++w2) {
+    const int32_t w1 =
+        nearest_neighbor_compute_source_index(scale, w2, input_width);
+
+    float* pos1 = &idata[w1];
+    const float* pos2 = &odata[w2];
+
+    for (int32_t c = 0; c < channels; ++c) {
+      pos1[0] += pos2[0];
+      pos1 += input_width;
+      pos2 += output_width;
+    }
+  }
+
+  return 0;
+}
+
+HB_EMUL_REG_KERNEL(tensorlib_upsample_nearest1d_back, hb_tensor_t*, hb_tensor_t*,
+                   int32_t*, int32_t*, int32_t*, int32_t*)
+
 }
