4e2842f9e32a4c7c4105f1b0a565d0ca72c21263
lc873 <lc873@cornell.edu>
2021-01-06 16:17:39 -0500

    [upsampling1d] single thread forward

---
+++ hammerblade/torch/kernel/kernel_upsample_nearest1d.cpp
@@ -0,0 +1,78 @@
+//========================================================================
+// Upsampling 1D with nearest algo
+//========================================================================
+// Authors  : Lin Cheng
+// Date     : 01/06/2021
+
+#include <kernel_common.hpp>
+
+static inline int32_t nearest_neighbor_compute_source_index(
+    const float scale,
+    int32_t dst_index,
+    int32_t input_size) {
+  const int32_t src_index =
+      std::min(static_cast<int32_t>(floorf(dst_index * scale)), input_size - 1);
+  return src_index;
+}
+
+extern "C" {
+
+__attribute__ ((noinline))
+int tensorlib_upsample_nearest1d(
+    hb_tensor_t* output_p,
+    hb_tensor_t* input_p,
+    int32_t*     input_width_p,
+    int32_t*     output_width_p,
+    int32_t*     nbatch_p,
+    int32_t*     channels_p ) {
+
+  HBTensor<float> output(output_p);
+  HBTensor<float> input(input_p);
+  int32_t         input_width = *input_width_p;
+  int32_t         output_width = *output_width_p;
+  int32_t         nbatch = *nbatch_p;
+  int32_t         channels = *channels_p;
+
+  float* idata = (float*)input.data_ptr();
+  float* odata = (float*)output.data_ptr();
+
+  const float scale = (float)input_width / (float)output_width;
+  channels = channels * nbatch;
+
+  // special case: just copy
+  if (input_width == output_width) {
+    for (int32_t w2 = 0; w2 < output_width; ++w2) {
+      const int32_t w1 = w2;
+      const float* pos1 = &idata[w1];
+      float* pos2 = &odata[w2];
+
+      for (int32_t c = 0; c < channels; ++c) {
+        pos2[0] = pos1[0];
+        pos1 += input_width;
+        pos2 += output_width;
+      }
+    }
+    return 0;
+  }
+
+  for (int64_t w2 = 0; w2 < output_width; ++w2) {
+    const int32_t w1 =
+        nearest_neighbor_compute_source_index(scale, w2, input_width);
+    const float* pos1 = &idata[w1];
+    float* pos2 = &odata[w2];
+
+    for (int64_t c = 0; c < channels; ++c) {
+      pos2[0] = pos1[0];
+      pos1 += input_width;
+      pos2 += output_width;
+    }
+  }
+
+  return 0;
+
+}
+
+HB_EMUL_REG_KERNEL(tensorlib_upsample_nearest1d, hb_tensor_t*, hb_tensor_t*,
+                   int32_t*, int32_t*, int32_t*, int32_t*)
+
+}
---
+++ hammerblade/torch/tests/test_upsample.py
@@ -0,0 +1,64 @@
+"""
+Tests on torch.nn.Upsample and its backward
+01/06/2021 Lin Cheng
+Currently only 1D is supported
+Currently only nearest neighbor is supported
+Upsample1D takes in 3D tensors -- (N,Channel,Elem)
+"""
+
+import torch
+import torch.nn as nn
+import random
+import hbutils
+import pytest
+
+torch.manual_seed(42)
+random.seed(42)
+
+def _test_upsample1d_nearest(x, scale_factor):
+  x = x.clone().detach().requires_grad_(True)
+  x_hb = hbutils.init_hb_tensor(x)
+  assert x is not x_hb
+
+  m    = nn.Upsample(scale_factor=scale_factor, mode='nearest')
+  m_hb = nn.Upsample(scale_factor=scale_factor, mode='nearest')
+
+  y    = m(x)
+  y_hb = m_hb(x_hb)
+
+  assert torch.equal(y, y_hb.cpu())
+
+  grad    = y.clone().detach()
+  grad_hb = y_hb.clone().detach()
+  y.backward(grad)
+  y_hb.backward(grad_hb)
+
+  assert x.grad is not None
+  assert x_hb.grad is not None
+  assert x.grad is not x_hb.grad
+  assert torch.allclose(x.grad, x_hb.grad.cpu())
+
+def test_upsample1d_nearest1():
+  x = torch.ones(1,1,1)
+  scale_factor = 2
+  _test_upsample1d_nearest(x, scale_factor)
+
+def test_upsample1d_nearest2():
+  x = torch.ones(1,1,10)
+  scale_factor = 2
+  _test_upsample1d_nearest(x, scale_factor)
+
+def test_upsample1d_nearest3():
+  x = torch.rand(1,1,10)
+  scale_factor = 3
+  _test_upsample1d_nearest(x, scale_factor)
+
+def test_upsample1d_nearest4():
+  x = torch.rand(2,3,10)
+  scale_factor = 2
+  _test_upsample1d_nearest(x, scale_factor)
+
+def test_upsample1d_nearest5():
+  x = torch.rand(2,3,10)
+  scale_factor = 3
+  _test_upsample1d_nearest(x, scale_factor)
