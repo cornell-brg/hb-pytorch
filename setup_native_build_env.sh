d26058ccc159c2418a092a17c3e74856c035db38
lc873 <lc873@cornell.edu>
2021-02-10 08:36:36 -0500

    [native] link mkl

--- setup_native_build_env.sh
+++ setup_native_build_env.sh
@@ -3,6 +3,13 @@ echo "  Setting up PyTorch building environment ... for native evaluation!"
 echo "  Make sure you enabled devtoolset-8!"
 echo ""
 
+# setup path to MKL
+export MKL_ROOT=/work/global/lc873/work/sdh/venv_baseline/
+export MKL_INCLUDE=$MKL_ROOT/include
+export MKL_LIBRARY=$MKL_ROOT/lib
+export CMAKE_INCLUDE_PATH=$MKL_INCLUDE:$CMAKE_INCLUDE_PATH
+export CMAKE_LIBRARY_PATH=$MKL_LIBRARY:$CMAKE_LIBRARY_PATH
+
 # setup pytorch building options
 export BUILD_TEST=0
 export USE_HB=0
