3e385ebf91321ab31181b1e3df06b1d409e318c4
Zhongyuan Zhao <zz546@zhang-21.ece.cornell.edu>
2021-02-12 11:48:18 -0500

    fix spmm issue

--- aten/src/ATen/native/hammerblade/Cat.cpp
+++ aten/src/ATen/native/hammerblade/Cat.cpp
@@ -1,3 +1,9 @@
+//======================================================================
+//   _cat_hb extension from Lin Cheng and Janice Wei
+//   support cat on arbitrary dimension of 1-3D tensors
+//   Zhongyuan Zhao 02/10/2021 zz546@cornell.edu
+//======================================================================
+
 #include <ATen/ATen.h>
 #include <ATen/hammerblade/HammerBladeContext.h>
 #include <ATen/native/hammerblade/Offload.h>
@@ -6,7 +12,11 @@ namespace at { namespace native {
 
 Tensor _cat_hb(TensorList tensors, int64_t dim) {
   TORCH_CHECK(tensors.size() > 0, "_cat_hb: cannot concatenate empty tensor list");
-  TORCH_CHECK(dim == 0, "this simple cat only takes dim=0");
+  int64_t dims = tensors[0].dim();
+  for(int i = 1; i < tensors.size(); i++) {
+    TORCH_CHECK(tensors[i].dim() == dims, "The number of dimensions of input tensors should be equal");
+  }
+  TORCH_CHECK(dim < dims, "Invalid dimension parameter");
   TORCH_CHECK(tensors[0].dim() <= 3, "this simple cat only takes up to 3-dimension tensors");
   // convert TensorList length to uint32
   uint32_t length_u32 = safe_downcast<uint32_t, size_t>(tensors.size());
@@ -21,21 +31,24 @@ Tensor _cat_hb(TensorList tensors, int64_t dim) {
   scalars.push_back(create_device_scalar(length_u32));
   scalars.push_back(create_device_scalar(dim_i32));
 
-  auto ndim = tensors[0].dim();
+  //Compute the size of the output tensor
+   
+  
+//  auto ndim = tensors[0].dim();
   uint32_t space = 0;
   for (size_t i = 0; i < length_u32; i++) {
-    TORCH_CHECK(tensors[i].dim() == ndim, "tensors have different dimensions");
+    TORCH_CHECK(tensors[i].dim() == dims, "tensors have different dimensions");
     space += tensors[i].size(0);
   }
 
   Tensor result;
-  if (ndim == 1) {
+  if (dims == 1) {
     result = at::empty({space}, tensors[0].options());
   }
-  else if (ndim == 2) {
+  else if (dims == 2) {
     result = at::empty({space, tensors[0].size(1)}, tensors[0].options());
   }
-  else if (ndim == 3) {
+  else if (dims == 3) {
     result = at::empty({space, tensors[0].size(1), tensors[0].size(2)}, tensors[0].options());
   }
 
