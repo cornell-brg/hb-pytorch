537ce570390db1a9c99c7c09b03434689571f0bb
Zhongyuan Zhao <zz546@zhang-21.ece.cornell.edu>
2021-02-13 18:11:08 -0500

    Add extension of _cat kernel

--- aten/src/ATen/native/hammerblade/Cat.cpp
+++ aten/src/ATen/native/hammerblade/Cat.cpp
@@ -1,6 +1,6 @@
 //======================================================================
 //   _cat_hb extension from Lin Cheng and Janice Wei
-//   support cat on arbitrary dimension of 1-3D tensors
+//   support cat on arbitrary dimension of arbitrary dimension tensors
 //   Zhongyuan Zhao 02/10/2021 zz546@cornell.edu
 //======================================================================
 
@@ -12,7 +12,8 @@ namespace at { namespace native {
 
 Tensor _cat_hb(TensorList tensors, int64_t dim) {
   TORCH_CHECK(tensors.size() > 0, "_cat_hb: cannot concatenate empty tensor list");
-  int64_t dims = tensors[0].dim();
+  Tensor input0 = tensors[0];
+  int64_t dims = input0.dim();
   for(int i = 1; i < tensors.size(); i++) {
     TORCH_CHECK(tensors[i].dim() == dims, "The number of dimensions of input tensors should be equal");
   }
@@ -32,25 +33,24 @@ Tensor _cat_hb(TensorList tensors, int64_t dim) {
   scalars.push_back(create_device_scalar(dim_i32));
 
   //Compute the size of the output tensor
-   
+  int64_t output_cat_dim = 0;
+  std::vector<int64_t> out_size(dims);
   
-//  auto ndim = tensors[0].dim();
-  uint32_t space = 0;
   for (size_t i = 0; i < length_u32; i++) {
     TORCH_CHECK(tensors[i].dim() == dims, "tensors have different dimensions");
-    space += tensors[i].size(0);
+    output_cat_dim += tensors[i].size(dim);
+  }
+  for(int d = 0; d < dims; d++) {
+    if(d == dim) {
+      out_size[d] = output_cat_dim;
+    }
+    else {
+      out_size[d] = input0.size(d);
+    }
   }
 
   Tensor result;
-  if (dims == 1) {
-    result = at::empty({space}, tensors[0].options());
-  }
-  else if (dims == 2) {
-    result = at::empty({space, tensors[0].size(1)}, tensors[0].options());
-  }
-  else if (dims == 3) {
-    result = at::empty({space, tensors[0].size(1), tensors[0].size(2)}, tensors[0].options());
-  }
+  result = at::empty(out_size, input0.options());
 
   tensor_args.push_back(result);
   // offload call
