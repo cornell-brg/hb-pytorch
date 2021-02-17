84743d9ce96710251f928c4f883044cfedd4dad6
Neil Adit <neiladit@gmail.com>
2021-02-17 17:17:37 -0500

    adding hammerblade route to cdist_stub

--- aten/src/ATen/native/hammerblade/Cdist.cpp
+++ aten/src/ATen/native/hammerblade/Cdist.cpp
@@ -4,22 +4,21 @@
 #include <ATen/native/hammerblade/Offload.h>
 
 namespace at { namespace native {
-namespace {
+void cdist_kernel_hb(Tensor& result, const Tensor& x1, const Tensor& x2, double p) {
 
-Tensor cdist_hb(const Tensor& x1, const Tensor& x2, double p, c10::optional<int64_t> compute_mode) {
-
+    //only meant for Euclidean distance
     TORCH_CHECK(x1.is_hammerblade(), "cdist hb: expected 'x1' to be a HammerBlade tensor");
     TORCH_CHECK(x2.is_hammerblade(), "cdist hb: expected 'x2' to be a HammerBlade tensor");
 
     TORCH_CHECK(x1.dim() == 2 && x2.dim() == 2, "2D matrices expected, got ", x1.dim(), " and ", x2.dim(), " tensors");
     TORCH_CHECK(x1.size(1) == x2.size(1), "2nd dimension, observations should be equal! Got ", x1.size(1), " and ", x2.size(1));
 
-    Tensor result = at::empty({x1.size(0), x2.size(0)}, x1.options());
+    // Tensor result = at::empty({x1.size(0), x2.size(0)}, x1.options());
 
     hb_offload_kernel(result, x1, x2, "tensorlib_cdist");
-    return result;
+    // return result;
 }
 
-} // anonymous namespace
+REGISTER_HAMMERBLADE_DISPATCH(cdist_stub, &cdist_kernel_hb);
 
 }} // namespace at::native
