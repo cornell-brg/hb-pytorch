e7660065ff13c5923a1504b427fbd1aa5cd689a0
lc873 <lc873@cornell.edu>
2021-03-22 11:15:43 -0400

    [saif] no longer need to define bsg_saif_start/end ourselves

--- hammerblade/torch/kernel/kernel_common.hpp
+++ hammerblade/torch/kernel/kernel_common.hpp
@@ -22,9 +22,6 @@
 #define BSG_TILE_GROUP_Y_DIM bsg_global_Y
 #define bsg_tiles_X BSG_TILE_GROUP_X_DIM
 #define bsg_tiles_Y BSG_TILE_GROUP_Y_DIM
-//Macros for triggering saif generation
-#define bsg_saif_start() asm volatile ("addi zero,zero,1")
-#define bsg_saif_end() asm volatile ("addi zero,zero,2")
 #endif // HB_EMUL
 // imaginary __bsg_pod_id and BSG_POD_DIM
 #define __bsg_pod_id 0
