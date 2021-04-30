55032b76fa524569c6404aa1809af5029672be8d
zz546 <zz546@cornell.edu>
2021-04-30 14:45:19 -0400

    resolve all the conflicts

--- aten/src/ATen/native/hammerblade/Embedding.cpp
+++ aten/src/ATen/native/hammerblade/Embedding.cpp
@@ -7,10 +7,6 @@
 namespace at {
 namespace native {
 
-<<<<<<< HEAD
-Tensor embedding_dense_backward_hb(
-    const Tensor & grad_, const Tensor & indices, int64_t num_weights,
-=======
 // The idea here is to combine embedding with sum -- do reduction on the fly so we dont
 // have to deal with strange memory layout in sum
 Tensor embedding_hb(const Tensor & weight, const Tensor & indices,
@@ -61,24 +57,23 @@ Tensor embedding_dense_backward_hb(
 
 Tensor embedding_dense_backward_hb_recsys(
     const Tensor & grad, const Tensor & indices, int64_t num_weights,
->>>>>>> d8d63e689c... [hack] now embedding hack of recsys is a stand alone op
     int64_t padding_idx, bool scale_grad_by_freq) {
 
   TORCH_CHECK( !scale_grad_by_freq, "scale_grad_by_freq not yet supported on HB");
+  TORCH_CHECK( indices.is_contiguous(), "assuming indices to be contiguous" );
 
   auto indices_arg = TensorArg(indices, "indices", 2);
   checkScalarType("embedding_backward", indices_arg, kLong);
 
   int64_t numel = indices.numel();
-  auto indices_contig = indices.to(at::kInt).contiguous();
-  auto grad = grad_.contiguous().view({numel, grad_.size(-1)}).contiguous();
-  auto grad_weight = at::zeros({num_weights, grad_.size(-1)}, grad_.options());
+  auto grad_weight = at::zeros({num_weights, grad.size(-1)}, grad.options());
+  auto locks = at::zeros({10678}, grad.options());
 
   int32_t padding_idx_i32 = safe_downcast<int32_t, int64_t>(padding_idx);
   int32_t num_weights_i32 = safe_downcast<int32_t, int64_t>(num_weights);
-  int32_t output_numel_i32 = safe_downcast<int32_t, int64_t>(grad_.size(-1));
+  int32_t output_numel_i32 = safe_downcast<int32_t, int64_t>(grad.size(-1));
 
-  hb_offload_kernel(grad_weight, grad, indices_contig, padding_idx_i32,
+  hb_offload_kernel(grad_weight, grad, indices, locks, padding_idx_i32,
                     num_weights_i32, output_numel_i32,
                     "tensorlib_embedding_backward_recsys");
 
--- hammerblade/torch/kernel/kernel_embedding_backward.cpp
+++ hammerblade/torch/kernel/kernel_embedding_backward.cpp
@@ -7,75 +7,59 @@
 
 extern "C" {
 
-  __attribute__ ((noinline))  int tensorlib_embedding_backward_recsys(
+
+__attribute__ ((noinline))  int tensorlib_embedding_backward_recsys(
           hb_tensor_t* grad_weight_p,
           hb_tensor_t* grad_p,
           hb_tensor_t* index_p,
+          hb_tensor_t* locks_p,
           int32_t* padding_idx_p,
           int32_t* num_weights_p,
           int32_t* numel_p) {
 
     HBTensor<float> grad_weight(grad_weight_p);
     HBTensor<float> grad(grad_p);
-    HBTensor<int32_t> index(index_p);
-    int32_t padding_idx = *padding_idx_p;
-    int32_t num_weights = *num_weights_p;
-    int32_t numel = *numel_p;
+    HBTensor<int64_t> index(index_p);
+    HBTensor<int32_t> locks(locks_p);
+    const int32_t padding_idx = *padding_idx_p;
+    const int32_t num_weights = *num_weights_p;
+    const int32_t numel = *numel_p;
+    const uint32_t indices_numel = index.numel();
+
     bsg_attr_remote float* grad_weight_data = (bsg_attr_remote float*)grad_weight.data_ptr();
     bsg_attr_remote float* grad_data = (bsg_attr_remote float*)grad.data_ptr();
-    bsg_attr_remote int32_t* index_data = (bsg_attr_remote int32_t*)index.data_ptr();
+    bsg_attr_remote int* locks_data = (bsg_attr_remote int*)locks.data_ptr();
+    const bsg_attr_remote int64_t* index_data = (bsg_attr_remote int64_t*)index.data_ptr();
+    const uint32_t grad_weight_s0 = grad_weight.get_strides()[0];
+    const uint32_t grad_s0 = grad.get_strides()[0];
+    // const uint32_t grad_s1 = grad.get_strides()[1]; we don't actually use it ... since we know we will multiple 0 with it
+    const uint32_t grad_s2 = grad.get_strides()[2];
+    const uint32_t batch_size = index.dim(0);
+    const uint32_t embeddings_per_batch = indices_numel / batch_size;
 
     bsg_cuda_print_stat_kernel_start();
+    bsg_saif_start();
 
-    uint32_t indices_numel = index.numel();
-    // parallelize over vocabulary
-    // hb_tiled_range(num_weights, [&](size_t start, size_t end) {
-    //   for (uint32_t i = 0; i < indices_numel; i++) {
-    //     if (index_data[i] != padding_idx) {
-    //       int32_t k = index_data[i];
-    //       if (k >= start && k < end) {
-    //         float scale = 1.0;
-    //         float* dst = grad_weight_data + k * grad_weight.get_strides()[0];
-    //         float* src = grad_data + i * grad.get_strides()[0];
-    //         for (size_t j=0; j<numel; j++) {
-    //           *dst += *src * scale;
-    //           dst++;
-    //           src++;
-    //         }
-    //       }
-    //     }
-    //   }
-    // });
-
-    // which chunk of vocabulary we should focus on
-    size_t len_per_pod  = num_weights / BSG_POD_DIM + 1;
-    size_t pod_start    = len_per_pod * __bsg_pod_id;
-    size_t pod_end      = pod_start + len_per_pod;
-    pod_end = (pod_end > num_weights) ? num_weights : pod_end;
-
-    size_t len_per_tile = numel / (bsg_tiles_X * bsg_tiles_Y) + 1;
-    size_t start        = len_per_tile * __bsg_id;
-    size_t end          = start + len_per_tile;
-    end = (end > numel) ? numel : end;
-
-    // add is handled by the entire pod
-    for (uint32_t i = 0; i < indices_numel; i++) {
-      if (index_data[i] != padding_idx) {
-        int32_t k = index_data[i];
-        if (k >= pod_start && k < pod_end) {
-          float scale = 1.0;
-          // add is handled by the entire pod
-          bsg_attr_remote float* dst = grad_weight_data + k * grad_weight.get_strides()[0] + start * grad_weight.get_strides()[1];
-          bsg_attr_remote float* src = grad_data + i * grad.get_strides()[0] + start * grad.get_strides()[1];
-          for (size_t j=start; j<end; j++) {
-            *dst += *src * scale;
+    for (size_t idx = bsg_id; idx < indices_numel; idx += (BSG_TILE_GROUP_X_DIM * BSG_TILE_GROUP_Y_DIM)) {
+      if (idx < indices_numel) {
+        const int32_t offset = (int32_t)*(index_data + idx);
+        if (offset != padding_idx) {
+          const int32_t batch_id = idx / embeddings_per_batch;
+          bsg_attr_remote float* dst = grad_weight_data + offset * grad_weight_s0;
+          bsg_attr_remote float* src = grad_data + batch_id * grad_s0;
+          int* lock_addr = locks_data + offset;
+          while(bsg_amoswap_aq(lock_addr, 1)!=0);
+          bsg_unroll(16)
+          for (size_t i = 0; i < numel; i++) {
+            *dst += *src;
             dst++;
-            src++;
+            src += grad_s2;
           }
+          bsg_amoswap_rl(lock_addr, 0);
         }
       }
     }
-
+    bsg_saif_end();
     bsg_cuda_print_stat_kernel_end();
 
     g_barrier.sync();
