f659ee5b0549b3e54af9dcc4c3f50ae44932f63e
Niklas Schmelzle <jms854@cornell.edu>
2021-08-05 11:44:50 -0400

    Extended pytest for hb sigmoid_backward kernel with random input testcase

--- hammerblade/torch/tests/test_sigmoid_backward.py
+++ hammerblade/torch/tests/test_sigmoid_backward.py
@@ -5,36 +5,47 @@ Tests on sigmoid_backward
 import torch
 import torch.nn as nn
 
-def test_sigmoid_backward_scalar():
+from random import randrange
+import numpy as np
+
+def _test_sigmoid_backward(inp, grad_out=[1.]):
     sigmoid = nn.Sigmoid()
 
-    inp = torch.tensor([0.0], requires_grad=True)
-    inp_h = torch.tensor([0.0], device="hammerblade", requires_grad=True)
+    inp_ten = torch.tensor(inp, requires_grad=True)
+    inp_h_ten = torch.tensor(inp, device="hammerblade", requires_grad=True)
 
-    out = sigmoid(inp)
-    out_h = sigmoid(inp_h)
+    out_ten = sigmoid(inp_ten)
+    out_h_ten = sigmoid(inp_h_ten)
 
-    out.backward()
-    out_h.backward()
+    grad_output_ten = torch.tensor(grad_out)
+    grad_output_h_ten = grad_output_ten.hammerblade()
 
-    assert out_h.device == torch.device("hammerblade")
-    assert torch.allclose(inp.grad, inp_h.grad.cpu())
+    out_ten.backward(grad_output_ten)
+    out_h_ten.backward(grad_output_h_ten)
+
+    assert out_h_ten.device == torch.device("hammerblade")
+    assert torch.allclose(inp_ten.grad, inp_h_ten.grad.cpu())
+
+
+def test_sigmoid_backward_scalar():
+    _test_sigmoid_backward([0.0])
 
 def test_sigmoid_backward_vector():
-    sigmoid = nn.Sigmoid()
+    _test_sigmoid_backward([0.5, 0.6, 0.7], [0.7, 0.8, 0.9])
 
-    inp = torch.tensor([0.5, 0.6, 0.7], requires_grad=True)
-    inp_h = torch.tensor([0.5, 0.6, 0.7], device="hammerblade", requires_grad=True)
+def test_sigmoid_backward_random():
+    x_size = randrange(50)
+    y_size = randrange(10)
 
-    out = sigmoid(inp)
-    out_h = sigmoid(inp_h)
+    inp = [1.0]
+    grad_out = [1.0]
+    if y_size == 1:
+        inp = np.random.rand(x_size).tolist()
+        grad_out = np.random.rand(x_size).tolist()
+    else:
+        inp = np.random.rand(y_size, x_size).tolist()
+        grad_out = np.random.rand(y_size, x_size).tolist()
 
-    grad_output = torch.tensor([0.7, 0.8, 0.9])
-    grad_output_h = grad_output.hammerblade()
-    out.backward(grad_output)
-    out_h.backward(grad_output_h)
-
-    assert out_h.device == torch.device("hammerblade")
-    assert torch.allclose(inp.grad, inp_h.grad.cpu())
+    _test_sigmoid_backward(inp, grad_out)
 
 # TODO: Hypothesis?
