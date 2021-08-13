78069df7886a849feb38e8bfa1533c6f077d0e5a
Niklas Schmelzle <jms854@cornell.edu>
2021-08-13 16:57:43 -0400

    Added tests for mse_loss with target requiring gradient
    (pow kernel is required in this case)

--- hammerblade/torch/tests/test_mse_loss.py
+++ hammerblade/torch/tests/test_mse_loss.py
@@ -27,12 +27,12 @@ def _test_mse_loss(inp_list, target_list, target_requires_grad=False):
     assert torch.allclose(output, outputh.cpu())
 
 
-# target without gradient
+# target does not require gradient
 def test_mse_loss1():
-    _test_mse_loss([2.0], [2.5])
+    _test_mse_loss([2.0], [2.5], False)
 
 def test_mse_loss2():
-    _test_mse_loss([1.0, 7.0, -3.0], [2.0, 5.0, -1.0])
+    _test_mse_loss([1.0, 7.0, -3.0], [2.0, 5.0, -1.0], False)
 
 def test_mse_loss3():
     x_size = randrange(50)
@@ -47,4 +47,28 @@ def test_mse_loss3():
         inp = np.random.rand(y_size, x_size).tolist()
         target = np.random.rand(y_size, x_size).tolist()
 
-    _test_mse_loss(inp, target)
+    _test_mse_loss(inp, target, False)
+
+
+# target requires gradient
+# pow (tensor-scalar) kernel is also utilized
+def test_mse_loss_grad1():
+    _test_mse_loss([2.0], [2.5], True)
+
+def test_mse_loss_grad2():
+    _test_mse_loss([1.0, 7.0, -3.0], [2.0, 5.0, -1.0], True)
+
+def test_mse_loss_grad3():
+    x_size = randrange(50)
+    y_size = randrange(10)
+
+    inp = [1.0]
+    target = [1.0]
+    if y_size == 1:
+        inp = np.random.rand(x_size).tolist()
+        target = np.random.rand(x_size).tolist()
+    else:
+        inp = np.random.rand(y_size, x_size).tolist()
+        target = np.random.rand(y_size, x_size).tolist()
+
+    _test_mse_loss(inp, target, True)
