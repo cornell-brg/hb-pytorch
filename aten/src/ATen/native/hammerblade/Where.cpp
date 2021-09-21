fac1d104e770c13c49b3ac6894367ddf5e5618c6
Niklas Schmelzle <jms854@cornell.edu>
2021-09-21 17:47:58 -0400

    Added where operator implementation for hammerblade

---
+++ aten/src/ATen/native/hammerblade/Where.cpp
@@ -0,0 +1,28 @@
+#include <ATen/Dispatch.h>
+#include <ATen/hammerblade/HammerBladeContext.h>
+#include <ATen/native/TensorIterator.h>
+#include <ATen/native/hammerblade/Offload.h>
+
+namespace at { namespace native {
+
+Tensor _s_where_hb(const Tensor& condition, const Tensor& self, const Tensor& other) {
+    TORCH_CHECK(self.dtype() == other.dtype(), "expected scalar type ", self.dtype(), " but found ", other.dtype());
+    Tensor result = at::empty(self.sizes(), self.options());
+
+    auto iter = at::TensorIterator();
+    iter.set_check_mem_overlap(true);
+    iter.add_output(result);
+    iter.add_input(condition);
+    iter.add_input(self);
+    iter.add_input(other);
+    iter.dont_compute_common_dtype();
+    iter.build();
+
+    AT_DISPATCH_FLOAT_TYPE_ONLY(iter.dtype(), "where_hb", [&](){
+        offload_op_ternary(iter, "tensorlib_where");
+    });
+
+    return result;
+}
+
+}}
