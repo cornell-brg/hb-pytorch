d091617e8b6123335bc7088d7ebc013b54af1ff9
Niklas Schmelzle <jms854@cornell.edu>
2021-11-08 15:08:55 -0500

    First version of index_add hammerblade implementation
    Similar to CUDA implementation for large indeces.

--- aten/src/ATen/native/hammerblade/Indexing.cpp
+++ aten/src/ATen/native/hammerblade/Indexing.cpp
@@ -59,5 +59,110 @@ void index_kernel_hb(TensorIterator& iter, IntArrayRef index_size, IntArrayRef i
 REGISTER_HAMMERBLADE_DISPATCH(index_stub, &index_kernel_hb);
 
 }
+
+
+// NOTE: Function updates addDim
+Tensor collapseDims(Tensor t, int64_t &addDim) {
+    // collapse dimensions smaller than addDim
+    std::vector<int64_t> new_shape;
+    if (addDim != 0) {
+        int64_t new_size = 1;
+        for (int i = 0; i < addDim; ++i) {
+            new_size *= t.size(i);
+        }
+        new_shape.push_back(new_size);
+    }
+
+    // keep size of addDim
+    new_shape.push_back(t.size(addDim));
+
+    // collapse dimensions bigger than addDim
+    if (addDim != t.dim() - 1) {
+        int64_t new_size = 1;
+        for (int i = addDim+1; i < t.dim(); ++i) {
+            new_size *= t.size(i);
+        }
+        new_shape.push_back(new_size);
+    }
+
+    Tensor t_collapsed = t.view(IntArrayRef(new_shape));
+
+    if (addDim != 0)
+        addDim = 1;
+    std::cout << "addDim: " << addDim << std::endl;
+    std::cout << "new_shape: " << new_shape << std::endl;
+
+    /*
+    std::cout  << "addDim: " << addDim << std::endl;
+    for (int i = 0; i < t_collapsed.dim(); ++i) {
+        std::cout << "size[" << i << "]: " << t_collapsed.size(i) << std::endl;
+        std::cout << "stride[" << i << "]: " << t_collapsed.stride(i) << std::endl;
+    }
+    */
+
+    return t_collapsed;
+}
+
+Tensor& index_add_hb_(Tensor &self, int64_t dim, const Tensor &index, const Tensor &source) {
+    auto  numel = index.numel();
+    TORCH_CHECK_INDEX(index.dim() <= 1, "index_add_(): Index is supposed to be a vector");
+    TORCH_CHECK(index.scalar_type() == ScalarType::Long, "index_add_(): Expected dtype int64 for index");
+    TORCH_CHECK(self.scalar_type() == source.scalar_type(),
+                "index_add_(): self and source must have the same scalar type");
+    TORCH_CHECK(dim == 0 || dim < source.dim(),
+                "index_add_(): Indexing dim ", dim, " is out of bounds of tensor");
+    TORCH_CHECK(numel == (source.dim() == 0 ? 1 : source.size(dim)),
+                "index_add_(): Number of indices should be equal to self.size(dim)");
+    // TODO: check all dimension sizes besides dim dimension
+    // TODO: check dim equal of self and source
+
+
+    int64_t dst_add_dim = dim;
+    int64_t src_add_dim = dim;
+    // TODO: dst_add_dim and src_add_dim have the same size?
+    Tensor dst_c = collapseDims(self, dst_add_dim);
+    Tensor src_c = collapseDims(source, src_add_dim);
+
+
+    // TODO: check sliceSize != 0
+    // TODO: simplify sliceSize calculation?
+    int64_t sliceSize = 1;
+    for (int i = 0; i < dst_c.dim(); ++i) {
+        if (i != dst_add_dim) {
+            sliceSize *= dst_c.size(i);
+        }
+    }
+    int64_t nbrIndices = index.numel();
+    // TODO: Just last dim?
+    bool indexShouldBeMajor = false;
+    for (int i = 0; i < dst_c.dim(); ++i) {
+        if (i != dst_add_dim && dst_c.size(i) > 1 && dst_c.stride(i) < dst_c.stride(dst_add_dim))
+            indexShouldBeMajor = true;
+    }
+    if (indexShouldBeMajor)
+        std::cout << "indexShouldBeMajor" << std::endl;
+    else
+        std::cout << "elementsInSliceMajor" << std::endl;
+
+    std::vector<eva_t>  device_args;
+    std::vector<eva_t>  device_ptrs;
+    device_args.push_back(create_device_tensor(dst_c, device_ptrs));
+    device_args.push_back(create_device_tensor(src_c, device_ptrs));
+    device_args.push_back(create_device_tensor(index, device_ptrs));
+    device_args.push_back(create_device_scalar((int64_t) dst_add_dim));
+    device_args.push_back(create_device_scalar((int64_t) sliceSize));
+    device_args.push_back(create_device_scalar((int64_t) nbrIndices));
+    device_args.push_back(create_device_scalar((bool) indexShouldBeMajor));
+
+    // TODO: numIndices <= 16
+    // TODO: Implement small indices
+
+    c10::hammerblade::offload_kernel("tensorlib_index_add_large_index", device_args);
+
+    cleanup_device(device_args, device_ptrs);
+
+    return self;
+}
+
 }}
 
---
+++ hammerblade/torch/tests/test_index_add.py
@@ -0,0 +1,147 @@
+import torch
+
+def _test_index_add(dim, t1, t2, index):
+    t1_h = t1.hammerblade()
+    t2_h = t2.hammerblade()
+    index_h = index.hammerblade()
+
+    out = t1.index_add(dim, index, t2)
+    out_h = t1_h.index_add(dim, index_h, t2_h)
+    print(out)
+    print(out_h)
+    abs_diff = torch.abs(out - out_h.cpu())
+    nonzero = torch.nonzero(abs_diff)
+    print(nonzero)
+    print("#nonzero: ", nonzero.size())
+    print("sum abs difference: ", torch.sum(abs_diff))
+    assert out_h.device == torch.device("hammerblade")
+    assert torch.allclose(out, out_h.cpu())
+
+
+def test_index_add_1():
+    dim = 0
+    t1 = torch.ones(5, 3)
+    t2 = torch.tensor([[1, 2, 3], [4, 5, 6], [7, 8, 9]], dtype=torch.float)
+    index = torch.tensor([3, 2, 1])
+    _test_index_add(dim, t1, t2, index)
+
+def test_index_add_2():
+    dim = 1
+    t1 = torch.ones(5, 3)
+    t2 = torch.tensor([[1, 2, 3], [4, 5, 6], [7, 8, 9], [0, 0, 0], [0, 0, 0]], dtype=torch.float)
+    index = torch.tensor([2, 0, 1])
+    _test_index_add(dim, t1, t2, index)
+
+def test_index_add_3():
+    dim = 0
+    t1 = torch.tensor([[[[1, 1], [2, 2], [3, 3]], [[11, 11], [22, 22], [33, 33]]],
+                       [[[4, 4], [5, 5], [6, 6]], [[44, 44], [55, 55], [66, 66]]],
+                       [[[7, 7], [8, 8], [9, 9]], [[77, 77], [88, 88], [99, 99]]]], dtype=torch.float)
+    print(t1)
+    print("size: ", t1.size()) # 3, 2, 3, 2
+    print("strides: ", t1.stride())
+
+    t2 = torch.rand([2, 2, 3, 2])
+    print(t2)
+    index = torch.tensor([1, 0])
+    print(index)
+    _test_index_add(dim, t1, t2, index)
+
+def test_index_add_4():
+    dim = 1
+    t1 = torch.tensor([[[[1, 1], [2, 2], [3, 3]], [[11, 11], [22, 22], [33, 33]]],
+                       [[[4, 4], [5, 5], [6, 6]], [[44, 44], [55, 55], [66, 66]]],
+                       [[[7, 7], [8, 8], [9, 9]], [[77, 77], [88, 88], [99, 99]]]], dtype=torch.float)
+    print(t1)
+    print("size: ", t1.size()) # 3, 2, 3, 2
+    print("strides: ", t1.stride())
+
+    t2 = torch.rand([3, 1, 3, 2])
+    print(t2)
+    index = torch.tensor([0])
+    print(index)
+    _test_index_add(dim, t1, t2, index)
+
+def test_index_add_5():
+    dim = 2
+    t1 = torch.tensor([[[[1, 1], [2, 2], [3, 3]], [[11, 11], [22, 22], [33, 33]]],
+                       [[[4, 4], [5, 5], [6, 6]], [[44, 44], [55, 55], [66, 66]]],
+                       [[[7, 7], [8, 8], [9, 9]], [[77, 77], [88, 88], [99, 99]]]], dtype=torch.float)
+    print(t1)
+    print("size: ", t1.size()) # 3, 2, 3, 2
+    print("strides: ", t1.stride())
+
+    t2 = torch.rand([3, 2, 5, 2])
+    print(t2)
+    index = torch.tensor([0, 0, 1, 2, 2])
+    print(index)
+    _test_index_add(dim, t1, t2, index)
+
+def test_index_add_6():
+    dim = 3
+    t1 = torch.tensor([[[[1, 1], [2, 2], [3, 3]], [[11, 11], [22, 22], [33, 33]]],
+                       [[[4, 4], [5, 5], [6, 6]], [[44, 44], [55, 55], [66, 66]]],
+                       [[[7, 7], [8, 8], [9, 9]], [[77, 77], [88, 88], [99, 99]]]], dtype=torch.float)
+    print(t1)
+    print("size: ", t1.size()) # 3, 2, 3, 2
+    print("strides: ", t1.stride())
+
+    t2 = torch.rand([3, 2, 3, 1])
+    print(t2)
+    index = torch.tensor([0])
+    print(index)
+    _test_index_add(dim, t1, t2, index)
+
+def test_index_add_7():
+    dim = 4
+    t1 = torch.rand([11, 3, 7, 5, 20, 2])
+    t2 = torch.rand([11, 3, 7, 5, 10, 2])
+    index = torch.tensor([0, 2, 4, 6, 8, 10, 12, 14, 16, 18])
+    _test_index_add(dim, t1, t2, index)
+
+def test_index_add_8():
+    dim = 4
+    t1 = torch.rand([11, 3, 1, 5, 20, 2])
+    t2 = torch.rand([11, 3, 1, 5, 10, 2])
+    index = torch.tensor([0, 2, 4, 6, 8, 10, 12, 14, 16, 18])
+    _test_index_add(dim, t1, t2, index)
+
+def test_index_add_9():
+    dim = 2
+    t1 = torch.rand([350, 36, 1500])
+    t2 = torch.rand([350, 36, 140])
+    index = torch.ones(140, dtype=torch.int64)
+    print(index)
+    _test_index_add(dim, t1, t2, index)
+
+def test_index_add_10():
+    dim = 2
+    t1 = torch.rand([350, 36, 1500])
+    t2 = torch.rand([350, 36, 140])
+    index = torch.randint(0, 1500, (140,))
+    print(index)
+    _test_index_add(dim, t1, t2, index)
+
+def test_index_add_11():
+    dim = 2
+    t1 = torch.rand([350, 36, 1500])
+    t2 = torch.rand([350, 36, 140])
+    index = torch.arange(0, 140, dtype=torch.int64)
+    print(index)
+    _test_index_add(dim, t1, t2, index)
+
+def test_index_add_12():
+    dim = 1
+    count = 100
+    t1 = torch.ones([45, 10])
+    t2 = torch.ones([45, count])
+    index = torch.zeros(count, dtype=torch.int64)
+    _test_index_add(dim, t1, t2, index)
+
+def test_index_add_13():
+    dim = 0
+    count = 10000
+    t1 = torch.ones([10, 2])
+    t2 = torch.ones([count, 2])
+    index = torch.zeros(count, dtype=torch.int64)
+    _test_index_add(dim, t1, t2, index)
