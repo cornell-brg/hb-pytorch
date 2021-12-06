984852c9df42c14b12573f5b13073d0a76e9f293
Niklas Schmelzle <jms854@cornell.edu>
2021-12-06 11:28:14 -0500

    Simplified tests to improve cosimulation time

--- hammerblade/torch/tests/test_index_add.py
+++ hammerblade/torch/tests/test_index_add.py
@@ -7,8 +7,8 @@ def _test_index_add(dim, t1, t2, index):
 
     out_h = t1_h.index_add(dim, index_h, t2_h)
     out = t1.index_add(dim, index, t2)
-    print(out)
-    print(out_h)
+    #print(out)
+    #print(out_h)
     #abs_diff = torch.abs(out - out_h.cpu())
     #nonzero = torch.nonzero(abs_diff)
     #print(nonzero)
@@ -24,6 +24,7 @@ def test_index_add_1():
     t2 = torch.tensor([[1, 2, 3], [4, 5, 6], [7, 8, 9]], dtype=torch.float)
     index = torch.tensor([3, 2, 1])
     _test_index_add(dim, t1, t2, index)
+    print("*** test_index_add_1 done!")
 
 def test_index_add_2():
     dim = 1
@@ -31,7 +32,9 @@ def test_index_add_2():
     t2 = torch.tensor([[1, 2, 3], [4, 5, 6], [7, 8, 9], [0, 0, 0], [0, 0, 0]], dtype=torch.float)
     index = torch.tensor([2, 0, 1])
     _test_index_add(dim, t1, t2, index)
+    print("*** test_index_add_2 done!")
 
+# test index_add operation for different dimmensions
 def test_index_add_3():
     dim = 0
     t1 = torch.tensor([[[[1, 1], [2, 2], [3, 3]], [[11, 11], [22, 22], [33, 33]]],
@@ -46,7 +49,9 @@ def test_index_add_3():
     index = torch.tensor([1, 0])
     print(index)
     _test_index_add(dim, t1, t2, index)
+    print("*** test_index_add_3 done!")
 
+# test index_add operation for different dimmensions
 def test_index_add_4():
     dim = 1
     t1 = torch.tensor([[[[1, 1], [2, 2], [3, 3]], [[11, 11], [22, 22], [33, 33]]],
@@ -61,7 +66,9 @@ def test_index_add_4():
     index = torch.tensor([0])
     print(index)
     _test_index_add(dim, t1, t2, index)
+    print("*** test_index_add_4 done!")
 
+# test index_add operation for different dimmensions
 def test_index_add_5():
     dim = 2
     t1 = torch.tensor([[[[1, 1], [2, 2], [3, 3]], [[11, 11], [22, 22], [33, 33]]],
@@ -76,7 +83,9 @@ def test_index_add_5():
     index = torch.tensor([0, 0, 1, 2, 2])
     print(index)
     _test_index_add(dim, t1, t2, index)
+    print("*** test_index_add_5 done!")
 
+# test index_add operation for different dimmensions
 def test_index_add_6():
     dim = 3
     t1 = torch.tensor([[[[1, 1], [2, 2], [3, 3]], [[11, 11], [22, 22], [33, 33]]],
@@ -91,85 +100,68 @@ def test_index_add_6():
     index = torch.tensor([0])
     print(index)
     _test_index_add(dim, t1, t2, index)
+    print("*** test_index_add_6 done!")
 
+# check with 1 dimensional layer inside
 def test_index_add_7():
-    dim = 4
-    t1 = torch.rand([11, 3, 7, 5, 20, 2])
-    t2 = torch.rand([11, 3, 7, 5, 10, 2])
-    index = torch.tensor([0, 2, 4, 6, 8, 10, 12, 14, 16, 18])
-    _test_index_add(dim, t1, t2, index)
-
-def test_index_add_8():
-    dim = 4
-    t1 = torch.rand([11, 3, 1, 5, 20, 2])
-    t2 = torch.rand([11, 3, 1, 5, 10, 2])
-    index = torch.tensor([0, 2, 4, 6, 8, 10, 12, 14, 16, 18])
-    _test_index_add(dim, t1, t2, index)
-
-def test_index_add_9():
-    dim = 2
-    t1 = torch.rand([350, 36, 1500])
-    t2 = torch.rand([350, 36, 140])
-    index = torch.ones(140, dtype=torch.int64)
-    print(index)
-    _test_index_add(dim, t1, t2, index)
-
-def test_index_add_10():
-    dim = 2
-    t1 = torch.rand([350, 36, 1500])
-    t2 = torch.rand([350, 36, 140])
-    index = torch.randint(0, 1500, (140,))
-    print(index)
-    _test_index_add(dim, t1, t2, index)
-
-def test_index_add_11():
-    dim = 2
-    t1 = torch.rand([350, 36, 1500])
-    t2 = torch.rand([350, 36, 140])
-    index = torch.arange(0, 140, dtype=torch.int64)
-    print(index)
-    _test_index_add(dim, t1, t2, index)
-
-def test_index_add_12():
     dim = 1
-    count = 100
-    t1 = torch.ones([45, 10])
-    t2 = torch.ones([45, count])
-    index = torch.zeros(count, dtype=torch.int64)
+    t1 = torch.rand([14, 7, 1, 20])
+    t2 = torch.rand([14, 6, 1, 20])
+    index = torch.tensor([1, 6, 2, 5, 3, 4])
     _test_index_add(dim, t1, t2, index)
+    print("*** test_index_add_7 done!")
 
-def test_index_add_13():
+# test write to same memory
+def test_index_add_8():
     dim = 0
-    count = 10000
+    count = 499
     t1 = torch.ones([10, 2])
     t2 = torch.ones([count, 2])
     index = torch.zeros(count, dtype=torch.int64)
     _test_index_add(dim, t1, t2, index)
+    print("*** test_index_add_8 done!")
 
-def test_index_add_14():
+# check 1D input
+def test_index_add_9():
     dim = 0
     t1 = torch.zeros(5)
     t2 = torch.rand(10)
     index = torch.tensor([0, 0, 3, 3, 3, 0, 4, 4, 4, 4], dtype=torch.int64)
     _test_index_add(dim, t1, t2, index)
+    print("*** test_index_add_9 done!")
 
-def test_index_add_15():
+# check 0D input
+def test_index_add_10():
     dim = 0
     t1 = torch.tensor([42.])
     t2 = torch.rand([5])
     index = torch.tensor([0, 0, 0, 0, 0], dtype=torch.int64)
     _test_index_add(dim, t1, t2, index)
+    print("*** test_index_add_10 done!")
 
-def test_index_add_16():
+# check 1 dimensional layers on outside
+def test_index_add_11():
     dim = 2
     t1 = torch.rand([1, 7, 11, 1])
     t2 = torch.rand([1, 7, 7, 1])
     index = torch.tensor([0, 1, 2, 3, 4, 5, 6], dtype=torch.int64)
     _test_index_add(dim, t1, t2, index)
+    print("*** test_index_add_11 done!")
 
-def test_index_add_17():
+# check 1 dimensional layers on outside
+def test_index_add_12():
     dim = 1
     t1 = torch.rand([1, 11, 1])
     t2 = torch.rand([1, 7, 1])
     index = torch.tensor([0, 1, 2, 3, 4, 5, 6], dtype=torch.int64)
     _test_index_add(dim, t1, t2, index)
+    print("*** test_index_add_12 done!")
+
+# check with slices greater than number of cores
+def test_index_add_13():
+    dim = 1
+    t1 = torch.rand([15, 5, 10])
+    t2 = torch.rand([15, 18, 10])
+    index = torch.tensor([0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2], dtype=torch.int64)
+    _test_index_add(dim, t1, t2, index)
+    print("*** test_index_add_13 done!")
